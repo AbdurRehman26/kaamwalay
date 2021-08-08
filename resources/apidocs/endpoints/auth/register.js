fe8f286425ec8f5ffbb0272599cf1ea907adca6e
Muhammad Nauman <naumancs@gmail.com>
2021-08-08 18:53:35 +0500

    added apidoc

---
+++ resources/apidocs/endpoints/auth/register.js
@@ -0,0 +1,40 @@
+/**
+ * @api {post} /register Register User
+ * @apiName Register User
+ * @apiGroup Authentication
+ *
+ * @apiUse header_main
+ *
+ * @apiParam { String } first_name First Name of the User
+ * @apiParam { String } last_name Last Name of the User
+ * @apiParam { String } email Unique Email of the User
+ * @apiParam { String } username Unique Username of the User
+ * @apiParam { String } password Password of the User
+ * @apiParam { String } password_confirmation Confirm password of the User
+ * @apiParam { String } [phone] Optional phone number of the User
+ *
+ * @apiSuccess {Array} data containing token and user object
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *      {
+ *          "data": [
+ *              "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTYyODA3ODcxMCwiZXhwIjoxNjU5NjE0NzEwLCJuYmYiOjE2MjgwNzg3MTAsImp0aSI6ImtJdW9MdU1NNlNzckhkN3QiLCJzdWIiOjIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.z0OsnqkKlcLYp9V7zkjvOCT1rBhLvfXlfWxHG_lYuRc"
+ *              "user": {
+ *                  "id": 18,
+ *                  "first_name": "Jane",
+ *                  "last_name": "Doe",
+ *                  "email": "test@test.test",
+ *                  "username": "test",
+ *                  "phone": null,
+ *                  "stripe_id": "cus_K0651IOZjCy9Wa",
+ *                  "roles": [
+ *                      {
+ *                          "id": 2,
+ *                          "name": "customer"
+ *                      }
+ *                  ]
+ *              }
+ *          ]
+ *      }
+ */

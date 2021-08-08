2eda28d9c06d3a1707fe1eaec8def3389b5861a6
Muhammad Nauman <naumancs@gmail.com>
2021-08-09 02:39:58 +0500

    docs updated

--- resources/apidocs/endpoints/auth/me.js
+++ resources/apidocs/endpoints/auth/me.js
@@ -11,20 +11,22 @@
  * @apiSuccessExample Success-Response:
  *     HTTP/1.1 200 OK
  *      {
- *          "user": {
- *              "id": 18,
- *              "first_name": "Jane",
- *              "last_name": "Doe",
- *              "email": "test@test.test",
- *              "username": "test",
- *              "phone": null,
- *              "stripe_id": "cus_K0651IOZjCy9Wa",
- *              "roles": [
- *                  {
- *                      "id": 2,
- *                      "name": "customer"
- *                  }
- *              ]
+ *          "data": {
+ *              "user": {
+ *                  "id": 18,
+ *                  "first_name": "Jane",
+ *                  "last_name": "Doe",
+ *                  "email": "test@test.test",
+ *                  "username": "test",
+ *                  "phone": null,
+ *                  "stripe_id": "cus_K0651IOZjCy9Wa",
+ *                  "roles": [
+ *                      {
+ *                          "id": 2,
+ *                          "name": "customer"
+ *                      }
+ *                  ]
+ *              }
  *          }
  *      }
  */

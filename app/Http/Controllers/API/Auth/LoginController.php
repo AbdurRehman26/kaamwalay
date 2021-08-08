784094eb98182152bfe7a0e0126bdb8926fb8f7e
muhammad-nauman <muhammad-nauman@users.noreply.github.com>
2021-08-08 07:09:29 +0000

    Fix styling

--- app/Http/Controllers/API/Auth/LoginController.php
+++ app/Http/Controllers/API/Auth/LoginController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\API\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\Auth\LoginRequest;
-use App\Http\Resources\API\Customer\User\UserResource;
 use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 

4e5be60698f8af5a4e62a29ed8cb7e2fda9d6056
Luis Molina <lm.molinab@gmail.com>
2021-08-08 10:22:46 -0500

    added scout extensive package from algolia and included configuration for cards index

--- app/Models/CardProduct.php
+++ app/Models/CardProduct.php
@@ -9,7 +9,7 @@ use Laravel\Scout\Searchable;
 class CardProduct extends Model
 {
     use HasFactory;
-        use Searchable;
+    use Searchable;
 
     /**
      * The attributes that are mass assignable.
@@ -29,25 +29,55 @@ class CardProduct extends Model
         'category_id',
     ];
 
-   /**
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+
+    protected $casts = [
+        'id' => 'integer',
+    ];
+
+    /**
      * Get the name of the index associated with the model.
      *
      * @return string
      */
     public function searchableAs()
     {
-        return 'local_card_products';
+        return config('app.env'). '_' . $this->getTable();
     }
 
     /**
-     * The attributes that should be cast to native types.
+     * Get the indexable data array for the model.
      *
-     * @var array
+     * @return array
      */
-    protected $casts = [
-        'id' => 'integer',
-    ];
+    public function toSearchableArray()
+    {
+        $array = [
+            "id" => $this->id,
+            "name" => $this->name,
+            "category_id" => $this->category_id,
+            "category_name" => $this->category->name,
+            "set_id" => $this->set_id,
+            "set_name" => $this->set->name,
+            "serie_id" => $this->set->serie_id,
+            "serie_name" => $this->set->serie->name,
+            "release_date" => $this->set->release_date,
+            "release_year" => $this->set->release_year,
+            "rarity" => $this->rarity,
+            "card_number" => $this->card_number,
+            "card_number_order" => $this->card_number_order,
+            "image_path" => $this->image_path,
+            "image_bucket_path" => $this->image_bucket_path,
+            "card_url" => $this->card_url,
+        ];
 
+        return $array;
+    }
+    
     public function set(){
         return $this->belongsTo(Set::class);
     }

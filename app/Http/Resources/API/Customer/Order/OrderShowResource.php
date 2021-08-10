b1684fe7e9d12b00f79bef12bb40102c376f41fd
Kazmi <sydabdrehman@gmail.com>
2021-08-11 01:39:24 +0500

    adding order show resource values

--- app/Http/Resources/API/Customer/Order/OrderShowResource.php
+++ app/Http/Resources/API/Customer/Order/OrderShowResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\API\Customer\Order;
 
+use App\Http\Resources\API\Customer\Order\PaymentPlan\PaymentPlanResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderShowResource extends JsonResource
@@ -19,9 +20,9 @@ class OrderShowResource extends JsonResource
             'order_number' => $this->order_number,
             'shipping_method' => $this->shippingMethod->name,
             'number_of_cards' => $this->orderItems->count(),
-            'service_level' => $this->paymentPlan,
             'created_at' => $this->created_at,
             'total_declared_value' => $this->grand_total,
+            'service_level' => new PaymentPlanResource($this->paymentPlan),
             'order_address' => new OrderAddressResource($this->orderAddress),
 
         ];

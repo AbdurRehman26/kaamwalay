ab43e6ba8d7f20e8c73e055f9489cdfd18c6b591
Muhammad Nauman <naumancs@gmail.com>
2021-08-11 00:48:11 +0500

    added test to handle response from AGS

--- tests/Feature/API/Customer/Auth/LoginTest.php
+++ tests/Feature/API/Customer/Auth/LoginTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\API\Customer\Auth;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
@@ -86,5 +87,31 @@ class LoginTest extends TestCase
         $response->assertJsonStructure(['data' => ['user']]);
         $response->assertJsonPath('data.user.email', $user->email);
         $response->assertJsonPath('data.user.id', $user->id);
+    }
+
+    /** @test @group auth */
+    public function ags_user_can_login()
+    {
+        if(!config('services.ags.is_platform_enabled')) {
+            $this->markTestSkipped('AGS platform is not enabled.');
+        }
+        $testEmail = 'test@test.test';
+        Http::fake([
+            config('services.ags.base_url') . '/login/' => Http::response([
+                'access_token' => 'token',
+                'user' => [
+                    'username' => 'test',
+                    'email' => $testEmail,
+                ]
+            ], 200),
+        ]);
+        $response = $this->postJson('api/auth/login', [
+            'email' => $testEmail,
+            'password' => 'Asdasd1',
+        ]);
+        $user = User::firstOrFail();
+        $response->assertStatus(200);
+        $response->assertJsonStructure([ 'access_token', 'type', 'expiry' ]);
+        $this->assertSame($testEmail, $user->email);
     }
 }
--- tests/Unit/API/Services/AGSServiceTest.php
+++ tests/Unit/API/Services/AGSServiceTest.php
@@ -14,8 +14,9 @@ class AGSServiceTest extends TestCase
     public function base_url_is_defined_when_services_is_enabled()
     {
         $ags = new AGS();
-        if ($ags->isEnabled()) {
-            $this->assertTrue(! ! config('services.ags.base_url'));
+        if (!$ags->isEnabled()) {
+            $this->markTestSkipped('AGS platform is not enabled.');
         }
+        $this->assertTrue(! ! config('services.ags.base_url'));
     }
 }

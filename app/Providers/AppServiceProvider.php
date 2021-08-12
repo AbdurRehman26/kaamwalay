220f1e8960c3828423711eb3326384f7457e2060
Muhammad Nauman <naumancs@gmail.com>
2021-08-13 02:26:58 +0500

    added route for spa

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        ResetPassword::createUrlUsing(function ($notifiable, $token) {
-            return config('app.url') . '/password/reset?token=' . $token . '&email=' . $notifiable->getEmailForPasswordReset();
-        });
+        //
     }
 }

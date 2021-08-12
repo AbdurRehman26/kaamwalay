b105d03f028122836ab009526dbd28fedad83735
Nabeel Ahmed <thisisnabeelahmed@gmail.com>
2021-08-12 08:27:45 +0500

    Order APIs docs

--- resources/apidocs/endpoints/customer/orders/list.js
+++ resources/apidocs/endpoints/customer/orders/list.js
@@ -6,66 +6,88 @@
  * @apiUse header_main
  * @apiUse authorization
  *
- * @apiSuccess {Array} data Orders List
+ * @apiSuccess {Object} data Orders data
  *
  * @apiSuccessExample Success-Response:
  *     HTTP/1.1 200 OK
-         {
-            "data": [
-                {
-                    "id": 3,
-                    "order_number": "b67ae96f-ff60-393e-b363-23844f311a18",
-                    "created_at": {
-                        "date": "2021-08-11 15:40:24.000000",
-                        "timezone_type": 3,
-                        "timezone": "UTC"
-                    },
-                    "arrived_at": {
-                        "date": "2015-07-09 00:00:00.000000",
-                        "timezone_type": 3,
-                        "timezone": "UTC"
-                    },
-                    "payment_plan": {
-                        "id": 10,
-                        "price": 2958,
-                        "max_protection_amount": 2108,
-                        "turnaround": "24 Day Turnaround"
-                    },
-                    "number_of_cards": 2553,
-                    "status": "Elenora"
-                }
-            ],
-            "links": {
-                "first": "http://localhost/api/customer/orders?page=1",
-                "last": "http://localhost/api/customer/orders?page=1",
-                "prev": null,
-                "next": null
-            },
-            "meta": {
-                "current_page": 1,
-                "from": 1,
-                "last_page": 1,
-                "links": [
-                    {
-                        "url": null,
-                        "label": "&laquo; Previous",
-                        "active": false
-                    },
-                    {
-                        "url": "http://localhost/api/customer/orders?page=1",
-                        "label": "1",
-                        "active": true
-                    },
-                    {
-                        "url": null,
-                        "label": "Next &raquo;",
-                        "active": false
-                    }
-                ],
-                "path": "http://localhost/api/customer/orders",
-                "per_page": 15,
-                "to": 5,
-                "total": 5
-            }
-        }
+ *        {
+ *            "data": [
+ *                {
+ *                    "id": 29,
+ *                    "order_number": "RG000000029",
+ *                    "created_at": {
+ *                        "date": "2021-08-12 02:22:46.000000",
+ *                        "timezone_type": 3,
+ *                        "timezone": "UTC"
+ *                    },
+ *                    "arrived_at": {
+ *                        "date": "2021-08-12 02:42:56.957751",
+ *                        "timezone_type": 3,
+ *                        "timezone": "UTC"
+ *                    },
+ *                    "payment_plan": {
+ *                        "id": 1,
+ *                        "price": 20,
+ *                        "max_protection_amount": 500,
+ *                        "turnaround": "28-30 Day"
+ *                    },
+ *                    "number_of_cards": 11,
+ *                    "status": "Pending Payment"
+ *                },
+ *                {
+ *                    "id": 28,
+ *                    "order_number": "RG000000028",
+ *                    "created_at": {
+ *                        "date": "2021-08-11 23:10:30.000000",
+ *                        "timezone_type": 3,
+ *                        "timezone": "UTC"
+ *                    },
+ *                    "arrived_at": {
+ *                        "date": "2021-08-12 02:42:56.961054",
+ *                        "timezone_type": 3,
+ *                        "timezone": "UTC"
+ *                    },
+ *                    "payment_plan": {
+ *                        "id": 1,
+ *                        "price": 20,
+ *                        "max_protection_amount": 500,
+ *                        "turnaround": "28-30 Day"
+ *                    },
+ *                    "number_of_cards": 11,
+ *                    "status": "Pending Payment"
+ *                },
+ *            ],
+ *            "links": {
+ *                "first": "http://robograding.test/api/customer/orders?page=1",
+ *                "last": "http://robograding.test/api/customer/orders?page=1",
+ *                "prev": null,
+ *                "next": null
+ *            },
+ *            "meta": {
+ *                "current_page": 1,
+ *                "from": 1,
+ *                "last_page": 1,
+ *                "links": [
+ *                    {
+ *                        "url": null,
+ *                        "label": "&laquo; Previous",
+ *                        "active": false
+ *                    },
+ *                    {
+ *                        "url": "http://robograding.test/api/customer/orders?page=1",
+ *                        "label": "1",
+ *                        "active": true
+ *                    },
+ *                    {
+ *                        "url": null,
+ *                        "label": "Next &raquo;",
+ *                        "active": false
+ *                    }
+ *                ],
+ *                "path": "http://robograding.test/api/customer/orders",
+ *                "per_page": 15,
+ *                "to": 15,
+ *                "total": 15
+ *            }
+ *        }
  */
--- resources/apidocs/endpoints/customer/orders/show.js
+++ resources/apidocs/endpoints/customer/orders/show.js
@@ -1,101 +1,128 @@
 /**
- * @api {get} /customer/orders List orders
- * @apiName List Orders
+ * @api {get} /customer/orders/1 Show Order
+ * @apiName Show Order
  * @apiGroup Customer-Orders
  *
  * @apiUse header_main
  * @apiUse authorization
  *
- * @apiSuccess {Object} data Order data
+ * @apiParam {Integer} id Order unique ID
+ *
+ * @apiSuccess {Object} data Order object
  *
  * @apiSuccessExample Success-Response:
  *     HTTP/1.1 200 OK
-         {
-            "data": {
-                "id": 1,
-                "order_number": "b9f7df2e-3168-3b96-ad08-7ef64467f0d9",
-                "number_of_cards": 4488,
-                "total_declared_value": 32147696.09,
-                "grand_total": 7205.14,
-                "shipping_fee": 425.13,
-                "created_at": {
-                    "date": "2021-08-11 15:40:23.000000",
-                    "timezone_type": 3,
-                    "timezone": "UTC"
-                },
-                "customer": {
-                    "id": 2,
-                    "first_name": "Rosemary",
-                    "last_name": "Rosemary",
-                    "email": "anastacio85@example.com",
-                    "username": "amara.senger",
-                    "phone": null,
-                    "stripe_id": null,
-                    "roles": [
-                        {
-                            "id": 2,
-                            "name": "customer"
-                        }
-                    ]
-                },
-                "shipping_method": {
-                    "id": 2,
-                    "code": "sequi",
-                    "name": "Gavin"
-                },
-                "payment_plan": {
-                    "id": 8,
-                    "price": 4823,
-                    "max_protection_amount": 8049,
-                    "turnaround": "22 Day Turnaround"
-                },
-                "shipping_address": {
-                    "id": 1,
-                    "first_name": "Herta",
-                    "last_name": "Dickinson",
-                    "address": "New Dax",
-                    "state": "maxime",
-                    "zip": "76066-6044",
-                    "phone": "+1-714-224-8249",
-                    "flat": "3",
-                    "country": {
-                        "id": 12,
-                        "code": "PW",
-                        "name": "Guernsey"
-                    }
-                },
-                "billing_address": {
-                    "id": 2,
-                    "first_name": "Ronaldo",
-                    "last_name": "Crona",
-                    "address": "South Hershelshire",
-                    "state": "facilis",
-                    "zip": "15250",
-                    "phone": "+1-785-698-0042",
-                    "flat": "7",
-                    "country": {
-                        "id": 13,
-                        "code": "CY",
-                        "name": "Bhutan"
-                    }
-                },
-                "order_items": [
-                    {
-                        "id": 1,
-                        "quantity": 4488,
-                        "declared_value_per_unit": 4902455.33,
-                        "card_product": {
-                            "id": 101,
-                            "name": "Kylee",
-                            "card_category_name": "Israel",
-                            "card_set_name": "Nola",
-                            "card_series_name": "Mireille",
-                            "release_year": 1982,
-                            "card_number_order": "589",
-                            "image_path": "https://den-cards.pokellector.com/305/Bellsprout.SWSH05.1.37528.png"
-                        }
-                    }
-                ]
-            }
-        }
+ *        {
+ *            "data": {
+ *                "id": 29,
+ *                "order_number": "RG000000029",
+ *                "number_of_cards": 11,
+ *                "total_declared_value": 5500,
+ *                "grand_total": 263,
+ *                "shipping_fee": 43,
+ *                "created_at": {
+ *                    "date": "2021-08-12 02:22:46.000000",
+ *                    "timezone_type": 3,
+ *                    "timezone": "UTC"
+ *                },
+ *                "customer": {
+ *                    "id": 16,
+ *                    "first_name": "Gisselle",
+ *                    "last_name": "Homenick",
+ *                    "email": "terrance.schmidt@example.net",
+ *                    "username": "satterfield.toy",
+ *                    "phone": null,
+ *                    "stripe_id": "cus_K1Sq1o4TaJvMHX",
+ *                    "roles": [
+ *                        {
+ *                            "id": 2,
+ *                            "name": "customer"
+ *                        }
+ *                    ]
+ *                },
+ *                "shipping_method": {
+ *                    "id": 1,
+ *                    "code": "insured_shipping",
+ *                    "name": "Insured Shipping"
+ *                },
+ *                "payment_plan": {
+ *                    "id": 1,
+ *                    "price": 20,
+ *                    "max_protection_amount": 500,
+ *                    "turnaround": "28-30 Day"
+ *                },
+ *                "shipping_address": {
+ *                    "id": 57,
+ *                    "first_name": "Test",
+ *                    "last_name": "Test",
+ *                    "address": "Test address",
+ *                    "city": "Test",
+ *                    "state": "AB",
+ *                    "zip": "123A",
+ *                    "phone": "1312310913",
+ *                    "flat": "43",
+ *                    "country": {
+ *                        "id": 1,
+ *                        "code": "US",
+ *                        "name": "United States"
+ *                    }
+ *                },
+ *                "billing_address": {
+ *                    "id": 58,
+ *                    "first_name": "Test",
+ *                    "last_name": "Test",
+ *                    "address": "Test address",
+ *                    "city": "Test",
+ *                    "state": "AB",
+ *                    "zip": "123A",
+ *                    "phone": "1312310913",
+ *                    "flat": "43",
+ *                    "country": {
+ *                        "id": 1,
+ *                        "code": "US",
+ *                        "name": "United States"
+ *                    }
+ *                },
+ *                "order_payment": {
+ *                    "card": {
+ *                        "brand": "visa",
+ *                        "exp_month": 4,
+ *                        "exp_year": 2024,
+ *                        "last4": "4242"
+ *                    }
+ *                },
+ *                "order_items": [
+ *                    {
+ *                        "id": 42,
+ *                        "quantity": 1,
+ *                        "declared_value_per_unit": 500,
+ *                        "card_product": {
+ *                            "id": 1,
+ *                            "name": "Bellsprout",
+ *                            "card_category_name": "Pokemon",
+ *                            "card_set_name": "Battle Styles",
+ *                            "card_series_name": "Sword & Shield Series",
+ *                            "release_year": 2021,
+ *                            "card_number_order": "001",
+ *                            "image_path": "https://den-cards.pokellector.com/305/Bellsprout.SWSH05.1.37528.png"
+ *                        }
+ *                    },
+ *                    {
+ *                        "id": 43,
+ *                        "quantity": 10,
+ *                        "declared_value_per_unit": 500,
+ *                        "card_product": {
+ *                            "id": 2,
+ *                            "name": "Weepinbell",
+ *                            "card_category_name": "Pokemon",
+ *                            "card_set_name": "Battle Styles",
+ *                            "card_series_name": "Sword & Shield Series",
+ *                            "release_year": 2021,
+ *                            "card_number_order": "002",
+ *                            "image_path": "https://den-cards.pokellector.com/305/Weepinbell.SWSH05.2.37529.png"
+ *                        }
+ *                    }
+ *                ]
+ *            }
+ *        }
  */

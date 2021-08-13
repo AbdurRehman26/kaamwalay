4916a0c0a66b097f37208a3e3e6caa3d57100067
Muhammad Nauman <naumancs@gmail.com>
2021-08-14 02:50:16 +0500

    added stdClass to array conversion

--- app/Services/Payment/Providers/PaypalService.php
+++ app/Services/Payment/Providers/PaypalService.php
@@ -50,7 +50,7 @@ class PaypalService implements PaymentProviderServiceInterface
 
             return [
                 'request' => $requestData,
-                'response' => $response->result,
+                'response' => get_object_vars($response->result),
                 'payment_provider_reference_id' => $response->result->id,
             ];
         } catch (HttpException $e) {
@@ -65,7 +65,7 @@ class PaypalService implements PaymentProviderServiceInterface
         try {
             $response = $this->client->execute($orderRequest);
 
-            return $this->validateOrderIsPaid($order, $response->result);
+            return $this->validateOrderIsPaid($order, get_object_vars($response->result));
         } catch (HttpException $e) {
             return false;
         }

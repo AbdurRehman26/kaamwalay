d81a2da9d0dd653730578ab228ab6ecf7fe2aff7
Dumitrana Alinus <alin.designstudio@gmail.com>
2021-08-14 08:24:22 +0300

    fix: type

--- resources/ts/shared/hooks/useConfiguration.ts
+++ resources/ts/shared/hooks/useConfiguration.ts
@@ -4,7 +4,7 @@ import { useMemo } from 'react';
 import { ConfigurationEntity } from '../entities/ConfigurationEntity';
 import { useSharedSelector } from './useSharedDispatch';
 
-export function useConfiguration() {
+export function useConfiguration(): ConfigurationEntity {
     const config = useSharedSelector((state) => state.configuration.data);
     return useMemo(() => plainToClass(ConfigurationEntity, config), [config]);
 }

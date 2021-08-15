c5d0f7633554f62a909bb415d00ed75903e00a29
thenabeel <thenabeel@users.noreply.github.com>
2021-08-15 09:56:18 +0000

    Fix styling

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -18,7 +18,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
         parent::boot();
 
         // Horizon::routeSmsNotificationsTo('15556667777');
-         Horizon::routeMailNotificationsTo('nabeel@wooter.com');
+        Horizon::routeMailNotificationsTo('nabeel@wooter.com');
         // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
 
         // Horizon::night();

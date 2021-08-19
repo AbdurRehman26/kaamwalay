240e528e7ff24696d741248a8b2a6712aa49312a
Kazmi <sydabdrehman@gmail.com>
2021-08-19 20:19:23 +0500

    dd removed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,8 +8,6 @@ class HomeController extends Controller
 {
     public function getView(): RedirectResponse
     {
-        dd(\Carbon\Carbon::now()->subDays(1)->format('Y-m-d'));
-
         return redirect()->route('dashboard.main', ['path' => '/']);
     }
 }

1e092a978b4f04bf22b7c9f80375db8537c7139d
Dumitrana Alinus <alin.designstudio@gmail.com>
2021-08-24 15:15:51 +0300

    docs(atoms): added docs for the atoms

--- resources/ts/landings/utils/createAtom.ts
+++ resources/ts/landings/utils/createAtom.ts
@@ -1,6 +1,22 @@
 import { ElementType } from 'react';
 import { Atom } from '../classes/Atom';
 
+/**
+ * Short handler for creating atoms quick and easy
+ * @example
+ * ```
+ * // Creating an atom from component.
+ * const DrawerNavigationAtom = createAtom(
+ *     lazy(() => import('../components/DrawerNavigation')),
+ *     'drawer-navigation',
+ * );
+ *
+ * // Creating an atom from atoms
+ * const LayoutAtom = createAtom([AuthControlsAtom, DrawerNavigationAtom]);
+ * ```
+ * @param from
+ * @param to
+ */
 export function createAtom(from: ElementType | Atom[], to?: string | HTMLElement) {
     const atom = new Atom().from(from);
 

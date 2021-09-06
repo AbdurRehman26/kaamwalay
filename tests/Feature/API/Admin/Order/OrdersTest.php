084c5b228059ba3b67c7eab29ddeb79b6ed2bcf6
Muhammad Nauman <naumancs@gmail.com>
2021-09-07 00:34:57 +0500

    added chaining to all the test.

--- tests/Feature/API/Admin/Order/OrdersTest.php
+++ tests/Feature/API/Admin/Order/OrdersTest.php
@@ -53,123 +53,107 @@ beforeEach(function () {
 });
 
 it('returns orders list for admin', function () {
-    $response = $this->getJson('/api/admin/orders');
-    $response->assertOk();
-    $response->assertJsonStructure([
-        'data' => [
-            [
-                'id',
-                'order_number',
-                'arrived',
-                'status',
-                'created_at',
+    $this->getJson('/api/admin/orders')
+        ->assertOk()
+        ->assertJsonStructure([
+            'data' => [
+                [
+                    'id',
+                    'order_number',
+                    'arrived',
+                    'status',
+                    'created_at',
+                ],
             ],
-        ],
-    ]);
+        ]);
 })->group('admin', 'admin_orders');
 
 it('returns order details', function () {
-    $response = $this->getJson('api/admin/orders/1');
-
-    $response->assertOk();
-    $response->assertJsonStructure([
-        'data' => [
-            'id',
-            'order_number',
-            'status',
-            'created_at',
-            'customer',
-            'order_items',
-        ],
-    ]);
+    $this->getJson('api/admin/orders/1')
+        ->assertOk()
+        ->assertJsonStructure([
+            'data' => [
+                'id',
+                'order_number',
+                'status',
+                'created_at',
+                'customer',
+                'order_items',
+            ],
+        ]);
 })->group('admin', 'admin_orders');
 
 test('orders throws error for roles other than admin', function () {
-    $this->actingAs(User::factory()->withRole(config('permission.roles.customer'))->create());
-    $response = $this->getJson('api/admin/orders/1');
-
-    $response->assertForbidden();
+    $this->actingAs(User::factory()->withRole(config('permission.roles.customer'))->create())
+        ->getJson('api/admin/orders/1')
+        ->assertForbidden();
 })->group('admin', 'admin_orders');
 
 test('order details throws error for roles other than admin', function () {
-    $this->actingAs(User::factory()->withRole(config('permission.roles.customer'))->create());
-    $response = $this->getJson('api/admin/orders/1');
-
-    $response->assertForbidden();
+    $this->actingAs(User::factory()->withRole(config('permission.roles.customer'))->create())
+        ->getJson('api/admin/orders/1')
+        ->assertForbidden();
 })->group('admin', 'admin_orders');
 
 it('filters orders by order number', function () {
-    $response = $this->getJson('/api/admin/orders?filter[order_number]=' . $this->orders[0]->order_number);
-
-    $response->assertOk();
-    $response->assertJsonCount(1, ['data']);
-    $response->assertJsonFragment([
-        'order_number' => $this->orders[0]->order_number,
-    ]);
-    $response->assertJsonMissing([
-        'order_number' => $this->orders[1]->order_number,
-    ]);
+    $this->getJson('/api/admin/orders?filter[order_number]=' . $this->orders[0]->order_number)
+        ->assertOk()
+        ->assertJsonCount(1, ['data'])
+        ->assertJsonFragment([
+            'order_number' => $this->orders[0]->order_number,
+        ]);
 })->group('admin', 'admin_orders');
 
 it('filters orders by id', function () {
-    $response = $this->getJson('/api/admin/orders?filter[id]=' . $this->orders[0]->id);
-
-    $response->assertOk();
-    $response->assertJsonCount(1, ['data']);
-    $response->assertJsonFragment([
-        'id' => $this->orders[0]->id,
-    ]);
-    $response->assertJsonMissing([
-        'id' => $this->orders[1]->id,
-    ]);
+    $this->getJson('/api/admin/orders?filter[id]=' . $this->orders[0]->id)
+        ->assertOk()
+        ->assertJsonCount(1, ['data'])
+        ->assertJsonFragment([
+            'id' => $this->orders[0]->id,
+        ]);
 })->group('admin', 'admin_orders');
 
 it('returns only placed orders', function () {
-    $response = $this->getJson('/api/admin/orders?filter[status_code]=placed');
-
-    $response->assertOk();
-    $response->assertJsonFragment([
-        'status' => OrderStatus::find(2)->name,
-    ]);
-    $response->assertJsonMissing([
-        'status' => OrderStatus::find(1)->name,
-    ]);
+    $this->getJson('/api/admin/orders?filter[status_code]=placed')
+        ->assertOk()
+        ->assertJsonFragment([
+            'status' => OrderStatus::find(2)->name,
+        ])
+        ->assertJsonMissing([
+            'status' => OrderStatus::find(1)->name,
+        ]);
 })->group('admin', 'admin_orders');
 
 it('returns only reviewed orders', function () {
-    $response = $this->getJson('/api/admin/orders?filter[status_code]=reviewed');
-
-    $response->assertOk();
-    $response->assertJsonCount(1, ['data']);
-    $response->assertJsonFragment([
-        'status' => OrderStatus::find(7)->name,
-    ]);
+    $this->getJson('/api/admin/orders?filter[status_code]=reviewed')
+        ->assertOk()
+        ->assertJsonCount(1, ['data'])
+        ->assertJsonFragment([
+            'status' => OrderStatus::find(7)->name,
+        ]);
 })->group('admin', 'admin_orders');
 
 it('returns only graded orders', function () {
-    $response = $this->getJson('/api/admin/orders?filter[status_code]=graded');
-
-    $response->assertOk();
-    $response->assertJsonCount(1, ['data']);
-    $response->assertJsonFragment([
-        'status' => OrderStatus::find(4)->name,
-    ]);
+    $this->getJson('/api/admin/orders?filter[status_code]=graded')
+        ->assertOk()
+        ->assertJsonCount(1, ['data'])
+        ->assertJsonFragment([
+            'status' => OrderStatus::find(4)->name,
+        ]);
 })->group('admin', 'admin_orders');
 
 it('returns only shipped orders', function () {
-    $response = $this->getJson('/api/admin/orders?filter[status_code]=shipped');
-
-    $response->assertOk();
-    $response->assertJsonCount(1, ['data']);
-    $response->assertJsonFragment([
-        'status' => OrderStatus::find(5)->name,
-    ]);
+    $this->getJson('/api/admin/orders?filter[status_code]=shipped')
+        ->assertOk()
+        ->assertJsonCount(1, ['data'])
+        ->assertJsonFragment([
+            'status' => OrderStatus::find(5)->name,
+        ]);
 })->group('admin', 'admin_orders');
 
 it('returns orders order by asc grand_total', function () {
-    $response = $this->getJson('/api/admin/orders?sort=grand_total');
-
-    $response->assertOk();
+    $response = $this->getJson('/api/admin/orders?sort=grand_total')
+        ->assertOk();
     $this->assertEquals(
         Order::orderBy('grand_total')->pluck('id')->toArray(),
         collect($response->getData()->data)->pluck('id')->toArray()
@@ -177,9 +161,8 @@ it('returns orders order by asc grand_total', function () {
 })->group('admin', 'admin_orders');
 
 it('returns orders order by desc grand_total', function () {
-    $response = $this->getJson('/api/admin/orders?sort=-grand_total');
-
-    $response->assertOk();
+    $response = $this->getJson('/api/admin/orders?sort=-grand_total')
+        ->assertOk();
     $this->assertEquals(
         Order::orderBy('grand_total', 'DESC')->pluck('id')->toArray(),
         collect($response->getData()->data)->pluck('id')->toArray()

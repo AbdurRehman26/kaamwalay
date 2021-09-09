52c9356816c2f438087d6cfbb9c6630a6fcee93b
Muhammad Nauman <naumancs@gmail.com>
2021-09-09 14:59:31 +0500

    updated resource

--- app/Http/Resources/API/Customer/Order/OrderItem/OrderItemResource.php
+++ app/Http/Resources/API/Customer/Order/OrderItem/OrderItemResource.php
@@ -15,7 +15,7 @@ class OrderItemResource extends JsonResource
             'declared_value_per_unit' => $this->declared_value_per_unit,
             'card_product' => new CardProductResource($this->cardProduct),
             'status' => new ItemStatusResource($this->itemStatuses()->latest()->first()),
-            'certificate_number' => optional($this->userCard->userCardCertificate)->number,
+            'certificate_number' => $this->userCard?->userCardCertificate?->number,
         ];
     }
 }

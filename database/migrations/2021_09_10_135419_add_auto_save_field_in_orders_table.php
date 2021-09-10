2bb884f1fdc2be99936a9c6369893f3cacb65b95
Luis Molina <lm.molinab@gmail.com>
2021-09-10 10:37:44 -0500

    add dropcolumn in autosave migration

--- database/migrations/2021_09_10_135419_add_auto_save_field_in_orders_table.php
+++ database/migrations/2021_09_10_135419_add_auto_save_field_in_orders_table.php
@@ -26,7 +26,7 @@ class AddAutoSaveFieldInOrdersTable extends Migration
     public function down()
     {
         Schema::table('orders', function (Blueprint $table) {
-            //
+            $table->dropColumn('auto_saved_at');
         });
     }
 }

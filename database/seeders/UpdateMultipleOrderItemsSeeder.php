8162974599d52207bcfc2b02a0db64db89a6fc51
Luis Molina <lm.molinab@gmail.com>
2021-09-13 12:32:34 -0500

    added seeder

---
+++ database/seeders/UpdateMultipleOrderItemsSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\OrderItem;
+use Illuminate\Database\Seeder;
+
+class UpdateMultipleOrderItemsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $currentDateTime = new \Datetime();
+        $items = OrderItem::where('quantity', '>', 1)->get();
+
+        foreach ($items as $item) {
+            $quantity = $item->quantity;
+            for ($i = 0; $i < $quantity - 1; $i++) {
+                $newItem = $item->replicate();
+                $newItem->quantity = 1;
+                $newItem->created_at = $currentDateTime;
+                $newItem->save();
+            }
+
+            $item->quantity = 1;
+            $item->save();
+        }
+    }
+}

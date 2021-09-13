81a97c4b15016ffa84a07279157c382da877dbaa
Muhammad Nauman <naumancs@gmail.com>
2021-09-14 01:28:31 +0500

    added tests

--- app/Services/Admin/CardGradingService.php
+++ app/Services/Admin/CardGradingService.php
@@ -2,8 +2,6 @@
 
 namespace App\Services\Admin;
 
-use Illuminate\Support\Arr;
-
 class CardGradingService
 {
     public function defaultValues(string $node): array
@@ -38,38 +36,6 @@ class CardGradingService
         ];
     }
 
-
-    public function calculateOverallValues(array | string $frontValues): array
-    {
-        if (is_string($frontValues)) {
-            $frontValues = json_decode($frontValues, associative: true);
-        }
-
-        return [
-            'center' => $this->getAverage(Arr::get($frontValues, 'front.center'), Arr::get($frontValues, 'back.center')),
-            'surface' => $this->getAverage(Arr::get($frontValues, 'front.surface'), Arr::get($frontValues, 'back.surface')),
-            'edge' => $this->getAverage(Arr::get($frontValues, 'front.edge'), Arr::get($frontValues, 'back.edge')),
-            'corner' => $this->getAverage(Arr::get($frontValues, 'front.corner'), Arr::get($frontValues, 'back.corner')),
-        ];
-    }
-
-    public function calculateOverallAverage(array | string $overAllValues): float
-    {
-        if (is_string($overAllValues)) {
-            $overAllValues = json_decode($overAllValues, associative: true);
-        }
-
-        return $this->getAverage($overAllValues['center'], $overAllValues['surface'], $overAllValues['edge'], $overAllValues['corner']);
-    }
-
-    protected function getAverage(...$values): float
-    {
-        if (count($values) === 0) {
-            return 0.0;
-        }
-
-        return number_format((float) (array_sum($values) / count($values)), 1);
-    }
 
     public function validateIfHumanGradesAreCompleted(array $humanGrades): bool
     {

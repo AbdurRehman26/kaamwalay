403dd019f1cd65fca39531449616aa5be036eb22
Muhammad Nauman <naumancs@gmail.com>
2021-09-15 01:44:27 +0500

    added test

--- app/Http/Requests/API/Admin/Order/Grades/UserCardGradeRequest.php
+++ app/Http/Requests/API/Admin/Order/Grades/UserCardGradeRequest.php
@@ -25,16 +25,12 @@ class UserCardGradeRequest extends FormRequest
     {
         return [
             'human_grade_values' => ['required', 'array'],
-            'human_grade_values.front' => ['required_without_all:human_grade_values.back', 'array', 'min:1'],
-            'human_grade_values.front.center' => [
-                'numeric',
-                'max:10',
-                'min:0',
-            ],
+            'human_grade_values.front' => ['required', 'array'],
+            'human_grade_values.front.center' => ['numeric','max:10','min:0',],
             'human_grade_values.front.surface' => ['numeric', 'max:10', 'min:0'],
             'human_grade_values.front.corner' => ['numeric', 'max:10', 'min:0'],
             'human_grade_values.front.edge' => ['numeric', 'max:10', 'min:0'],
-            'human_grade_values.back' => ['required_without_all:human_grade_values.front', 'array', 'min:1'],
+            'human_grade_values.back' => ['required', 'array'],
             'human_grade_values.back.center' => ['numeric', 'max:10', 'min:0'],
             'human_grade_values.back.surface' => ['numeric', 'max:10', 'min:0'],
             'human_grade_values.back.corner' => ['numeric', 'max:10', 'min:0'],
---
+++ tests/Feature/API/Admin/Order/UserCardTest.php
@@ -0,0 +1,80 @@
+<?php
+
+use App\Models\User;
+use App\Models\UserCardCertificate;
+use Database\Seeders\RolesSeeder;
+use Illuminate\Support\Facades\Http;
+
+beforeEach(function () {
+    $this->userCardCertificate = UserCardCertificate::factory()->create();
+    $this->userCard = $this->userCardCertificate->userCard;
+    $this->order = $this->userCard->orderItem->order;
+    $this->seed(RolesSeeder::class);
+    $this->actingAs(User::factory()->withRole(config('permission.roles.admin'))->create());
+});
+
+uses()->group('admin', 'grading');
+
+it('stores the human grades and does not update data on AGS', function () {
+    Http::fake();
+    $this->putJson('/api/admin/orders/' . $this->order->id . '/cards/' . $this->userCard->id . '/grades', [
+        'human_grade_values' => [
+            'front' => [
+                'center' => 2.50,
+                'surface' => 0,
+                'edge' => 0,
+                'corner' => 0,
+            ],
+            'back' => [
+                'center' => 0,
+                'surface' => 0,
+                'edge' => 0,
+                'corner' => 0,
+            ],
+        ],
+    ])
+    ->assertOk();
+
+    Http::assertNothingSent();
+});
+
+it('stores the human grades and update data on AGS', function () {
+    Http::fake([
+        'https://ags.api/v2/robograding/certificates/?certificate_id=' . $this->userCardCertificate->number =>
+            Http::response(json_decode(file_get_contents(
+                base_path() . '/tests/stubs/AGS_patch_human_grade_response_200.json'
+            ), associative: true)),
+    ]);
+    $this->putJson('/api/admin/orders/' . $this->order->id . '/cards/' . $this->userCard->id . '/grades', [
+        'human_grade_values' => [
+            'front' => [
+                'center' => 2.50,
+                'surface' => 2.50,
+                'edge' => 2.50,
+                'corner' => 2.50,
+            ],
+            'back' => [
+                'center' => 2.50,
+                'surface' => 2.50,
+                'edge' => 2.50,
+                'corner' => 2.50,
+            ],
+        ],
+    ])
+    ->assertOk()
+    ->assertJsonFragment([
+        'grade' => 7,
+    ])
+    ->assertJsonCount(8, 'data.generated_images')
+    ->assertJsonCount(4, 'data.overall_values')
+    ->assertJsonFragment([
+        "grade" => [
+            "grade" => 7,
+            "nickname" => "NM",
+        ],
+    ]);
+
+    Http::assertSent(function ($request) {
+        return $request->url() == 'https://ags.api/v2/robograding/certificates/?certificate_id=' . $this->userCardCertificate->number;
+    });
+});

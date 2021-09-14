ac4735be4056a24311872b96a097105eab474687
Luis Molina <lm.molinab@gmail.com>
2021-09-14 15:12:58 -0500

    added log in seeder

--- database/seeders/UpdateMultipleOrderItemsSeeder.php
+++ database/seeders/UpdateMultipleOrderItemsSeeder.php
@@ -14,19 +14,26 @@ class UpdateMultipleOrderItemsSeeder extends Seeder
      */
     public function run()
     {
-        $items = OrderItem::where('quantity', '>', 1)->where('order_id', '>', 10)->get();
+        $items = OrderItem::where('quantity', '>', 1)->get();
+        $this->command->info('Found ' . count($items). ' items.');
 
         foreach ($items as $item) {
+            $this->command->info('Processing Order Item ' . $item->id);
             for ($i = 0; $i < $item->quantity - 1; $i++) {
                 $newItem = $item->replicate();
                 $newItem->quantity = 1;
                 $newItem->declared_value_total = $item->declared_value_per_unit;
                 $newItem->save();
+
+                $this->command->info('Created Order Item ' . $newItem->id);
             }
 
             $item->quantity = 1;
             $item->declared_value_total = $item->declared_value_per_unit;
             $item->save();
+            $this->command->info('Updated Order Item ' . $item->id);
         }
+
+        $this->command->info('Process completed.');
     }
 }

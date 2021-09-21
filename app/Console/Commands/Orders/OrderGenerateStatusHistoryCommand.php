53852eff4037d9877938fd6f83765a8ed2718b40
Dumitrana Alinus <alin.designstudio@gmail.com>
2021-09-21 04:30:09 +0300

    add: small fixes

--- app/Console/Commands/Orders/OrderGenerateStatusHistoryCommand.php
+++ app/Console/Commands/Orders/OrderGenerateStatusHistoryCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands\Orders;
 
 use App\Exceptions\API\Admin\OrderStatusHistoryWasAlreadyAssigned;
 use App\Models\Order;
+use App\Models\OrderStatusHistory;
 use App\Services\Admin\OrderService;
 use App\Services\Admin\OrderStatusHistoryService;
 use Illuminate\Console\Command;
@@ -50,13 +51,18 @@ class OrderGenerateStatusHistoryCommand extends Command
         $password = $this->secret("Password of '$email'");
 
         auth()->attempt(compact('email', 'password'));
+        $user = auth() -> user();
 
         foreach ($orders as $order) {
             if ($order->order_status_id) {
                 $this->info("- Found Order [$order->id] with status '$order->order_status_id'");
                 for ($orderStatusId = 1; $orderStatusId <= $order->order_status_id; $orderStatusId++) {
                     try {
-                        $this->orderStatusHistoryService->addStatusToOrder($orderStatusId, $order);
+                        OrderStatusHistory::create([
+                           'order_id' => $order->id,
+                           'order_status_id' => $orders,
+                           'user_id' => $user?->id,
+                        ]);
                         $status = "OK";
                     } catch (\Exception $e) {
                         $message = $e->getMessage();

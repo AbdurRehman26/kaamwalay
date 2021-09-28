130c336b9822b7e2f3a43e1280e8e631ae718886
Ali Raza <ali@wooter.com>
2021-09-28 21:28:43 +0500

    email customer  order shipped

--- app/Listeners/API/Order/OrderStatusChangedListener.php
+++ app/Listeners/API/Order/OrderStatusChangedListener.php
@@ -63,6 +63,11 @@ class OrderStatusChangedListener implements ShouldQueue
     protected function handleShipped(OrderStatusChangedEvent $event)
     {
         // Order Shipped logics
+        $this->sendEmail($event, EmailService::TEMPLATE_SLUG_CARD_SHIPED, [
+            'FIRST_NAME' => $event->user->first_name,
+            'TRACKING_NUMBER' => $event->order->orderShipment->tracking_number,
+            'TRACKING_URL' => $event->order->orderShipment->tracking_url,
+        ]);
     }
 
     protected function sendEmail(OrderStatusChangedEvent $event, string $template, array $vars)

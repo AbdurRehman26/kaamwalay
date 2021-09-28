e9a40c70fbb530d6291f246d2c861159793530b0
Dumitrana Alinus <alin.designstudio@gmail.com>
2021-09-28 05:02:06 +0300

    fix: user card grade response

--- app/Http/Resources/API/Admin/Order/OrderItem/OrderItemResource.php
+++ app/Http/Resources/API/Admin/Order/OrderItem/OrderItemResource.php
@@ -4,29 +4,34 @@ namespace App\Http\Resources\API\Admin\Order\OrderItem;
 
 use App\Http\Resources\API\BaseResource;
 use App\Http\Resources\API\CardProduct\CardProductResource;
+use App\Http\Resources\API\Customer\UserCard\UserCardResource;
 use App\Models\OrderItemStatus;
+use App\Models\OrderItemStatusHistory;
 
 class OrderItemResource extends BaseResource
 {
     public function toArray($request): array
     {
-        $gradedHistory = $this->orderItemStatusHistory()->where('order_item_status_id', OrderItemStatus::GRADED)->latest()->first();
+        $isGraded = $this->resource->order_item_status_id >= OrderItemStatus::GRADED;
 
         return [
-            'id' => $this->id,
-            'order_id' => $this->order_id,
-            'quantity' => $this->quantity,
-            'declared_value_per_unit' => $this->declared_value_per_unit,
-            'card_product' => new CardProductResource($this->cardProduct),
-            'status' => new OrderItemStatusHistoryResource($this->orderItemStatusHistory()->latest()->first()),
-            'certificate_number' => $this->userCard?->certificate_number,
-            'grade' => [
-                'grade' => $this?->userCard?->overall_grade,
-                'nickname' => $this?->userCard?->overall_grade_nickname,
-            ],
-
-            'graded_by' => $this->when($this->order_item_status_id === OrderItemStatus::GRADED, $gradedHistory?->user?->getFullName()),
-            'graded_at' => $this->when($this->order_item_status_id === OrderItemStatus::GRADED, $this->formatDate($gradedHistory?->updated_at)),
+            'id' => $this->resource->id,
+            'order_id' => $this->resource->order_id,
+            'quantity' => $this->resource->quantity,
+            'declared_value_per_unit' => $this->resource->declared_value_per_unit,
+            'card_product' => new CardProductResource($this->resource->cardProduct),
+            'status' => new OrderItemStatusHistoryResource($this->resource->orderItemStatusHistory()->latest()->first()),
+            
+            // TODO: move certificate number inside the right relationship.
+            'certificate_number' => $this->resource->userCard?->certificate_number,
+            'user_card' => $this->whenLoaded('userCard', UserCardResource::class),
+            'graded_by' => $this->when($isGraded, fn () => $this->getGradedStatusHistory()?->user?->getFullName()),
+            'graded_at' => $this->when($isGraded, fn () => $this->formatDate($this->getGradedStatusHistory()?->updated_at)),
         ];
+    }
+
+    private function getGradedStatusHistory(): ?OrderItemStatusHistory
+    {
+        return $this->resource->orderItemStatusHistory()->where('order_item_status_id', OrderItemStatus::GRADED)->latest()->first();
     }
 }
--- app/Http/Resources/API/Customer/Order/OrderItem/OrderItemResource.php
+++ app/Http/Resources/API/Customer/Order/OrderItem/OrderItemResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\API\Customer\Order\OrderItem;
 
 use App\Http\Resources\API\CardProduct\CardProductResource;
+use App\Http\Resources\API\Customer\UserCard\UserCardResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderItemResource extends JsonResource
@@ -10,17 +11,15 @@ class OrderItemResource extends JsonResource
     public function toArray($request): array
     {
         return [
-            'id' => $this->id,
-            'quantity' => $this->quantity,
-            'order_id' => $this->order_id,
-            'declared_value_per_unit' => $this->declared_value_per_unit,
-            'card_product' => new CardProductResource($this->cardProduct),
-            'status' => new OrderItemStatusResource($this->orderItemStatus),
-            'grade' => [
-                'grade' => $this?->userCard?->overall_grade,
-                'nickname' => $this?->userCard?->overall_grade_nickname,
-            ],
-            'certificate_number' => $this->userCard?->userCardCertificate?->number,
+            'id' => $this->resource->id,
+            'quantity' => $this->resource->quantity,
+            'order_id' => $this->resource->order_id,
+            'declared_value_per_unit' => $this->resource->declared_value_per_unit,
+            // TODO: move certificate number inside the right relationship.
+            'certificate_number' => $this->resource->userCard?->certificate_number,
+            'card_product' => new CardProductResource($this->resource->cardProduct),
+            'status' => new OrderItemStatusResource($this->resource->orderItemStatus),
+            'user_card' => $this->whenLoaded('userCard', UserCardResource::class),
         ];
     }
 }

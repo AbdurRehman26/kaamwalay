5983e4bc3fc991ea3f1eb21b8fd77703d397dc06
Dumitrana Alinus <alin.designstudio@gmail.com>
2021-10-02 09:37:13 +0300

    fixes

--- resources/ts/shared/entities/OrderItemEntity.ts
+++ resources/ts/shared/entities/OrderItemEntity.ts
@@ -2,6 +2,7 @@ import { Type } from 'class-transformer';
 import { CardProductEntity } from './CardProductEntity';
 import { Entity } from './Entity';
 import { OrderItemStatusEntity } from './OrderItemStatusEntity';
+import { UserCardEntity } from './UserCardEntity';
 
 export class OrderItemEntity extends Entity {
     public quantity!: number;
@@ -14,4 +15,7 @@ export class OrderItemEntity extends Entity {
 
     @Type(() => CardProductEntity)
     public cardProduct!: CardProductEntity;
+
+    @Type(() => UserCardEntity)
+    public userCard!: UserCardEntity;
 }

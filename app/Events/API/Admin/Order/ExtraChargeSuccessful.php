7e63f7d0110aca532c0582d86388f6dc7ac2df9d
AbdurRehman26 <AbdurRehman26@users.noreply.github.com>
2021-10-11 14:03:34 +0000

    Fix styling

--- app/Events/API/Admin/Order/ExtraChargeSuccessful.php
+++ app/Events/API/Admin/Order/ExtraChargeSuccessful.php
@@ -3,7 +3,6 @@
 namespace App\Events\API\Admin\Order;
 
 use App\Http\Resources\API\Customer\Order\OrderPaymentResource;
-use App\Models\Order;
 use Illuminate\Contracts\Queue\ShouldBeEncrypted;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Events\Dispatchable;

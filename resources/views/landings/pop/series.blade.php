e763bc5fd544c336ef190d8210510555397dd6c1
Luis Molina <lm.molinab@gmail.com>
2021-10-14 10:07:31 -0500

    release date format

--- resources/views/landings/pop/series.blade.php
+++ resources/views/landings/pop/series.blade.php
@@ -144,7 +144,7 @@
                                     <a href="{{ route('pop.set', ['seriesId' => $cardSetsReport->card_series_id, 'setId' => $cardSetsReport->card_set_id]) }}" class="pop-list__table__info">
                                         <div class="pop-list__table__info-text">
                                             <p class="pop-list__table__info-heading">{{$cardSetsReport->name}}</p>
-                                            <p class="pop-list__table__info-subheading">{{$cardSetsReport->release_date}}</p>
+                                            <p class="pop-list__table__info-subheading">{{$cardSetsReport->release_date->format('m/d/Y')}}</p>
                                         </div>
                                     </a>
                                 </td>

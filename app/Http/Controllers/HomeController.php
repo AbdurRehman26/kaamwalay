a925b0836f74df638d65d8b226f5d82417cf32c4
Kazmi <sydabdrehman@gmail.com>
2021-10-19 18:07:46 +0500

    dd removed from controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,16 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Events\API\Customer\Order\OrderRefunded;
-use App\Models\OrderPayment;
 use Illuminate\Http\RedirectResponse;
 
 class HomeController extends Controller
 {
     public function getView(): RedirectResponse
     {
-        OrderRefunded::dispatch(OrderPayment::find(1)->order, ['amount' => 500]);
-        dd(1);
         return redirect()->route('dashboard.main', ['path' => '/']);
     }
 }

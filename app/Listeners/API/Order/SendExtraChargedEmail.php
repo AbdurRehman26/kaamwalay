8ec59c292d0292e0ee9775d30bf8619f48dcd3e8
Kazmi <sydabdrehman@gmail.com>
2021-10-19 17:46:02 +0500

    email extra charge fixed

--- app/Listeners/API/Order/SendExtraChargedEmail.php
+++ app/Listeners/API/Order/SendExtraChargedEmail.php
@@ -31,15 +31,17 @@ class SendExtraChargedEmail
         $card = $event->orderPayment->card;
 
         $this->emailService->sendEmail(
-            [ 'email' => $user->email, 'name' => $user->name ],
+            [[ $user->email => $user->name ]],
             $this->emailService::SUBJECT[$this->emailService::TEMPLATE_SLUG_SUBMISSION_EXTRA_CHARGED],
             $this->emailService::TEMPLATE_SLUG_SUBMISSION_EXTRA_CHARGED,
             [
+                'ORDER_NUMBER' => $order->order_number,
                 'TOTAL_AMOUNT' => number_format($order->grand_total, 2),
                 'SUB_TOTAL' => number_format($order->service_fee, 2),
                 'SHIPPING_FEE' => number_format($order->shipping_fee, 2),
                 'EXTRA_CHARGE' => number_format($event->orderPayment->amount, 2),
                 'CARD' => $card ? ($card['brand'] . ' ending in ' . $card['last4']) : 'N/A',
+                'SUBMISSION_URL' => config('app.url') . '/dashboard/submissions/' . $order->id . '/view',
             ],
         );
     }

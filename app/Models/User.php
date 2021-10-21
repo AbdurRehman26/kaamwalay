457a9262e1dea7dba1300b9706a588ee2da6f182
Nabeel Ahmed <thisisnabeelahmed@gmail.com>
2021-10-21 20:10:28 +0500

    Fixed visibility of assignCustomerNumber method Fixed customer number generation for AGS users

--- app/Models/User.php
+++ app/Models/User.php
@@ -124,7 +124,7 @@ class User extends Authenticatable implements JWTSubject
         return $this->hasMany(Order::class);
     }
 
-    private function assignCustomerNumber(): self
+    public function assignCustomerNumber(): self
     {
         if (! $this->customer_number) {
             $this->customer_number = SerialNumberService::customer($this->id)->toString();

5696a8c2d07cdc1e6d8fc866b498c89f01678465
Nabeel Ahmed <thisisnabeelahmed@gmail.com>
2021-10-23 01:59:24 +0500

    Upgraded packages Published Horizon updated assets Added static analysis in git hook

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -9,5 +9,8 @@ module.exports = {
         `bash -c "./vendor/bin/sail run laravel.test ./vendor/bin/php-cs-fixer fix --config=.php-cs-fixer.php --allow-risky=yes ${files.join(
             ' ',
         )} || echo "Couldn\'t run php-cs-fixer."`,
+        `bash -c "./vendor/bin/sail run laravel.test ./vendor/bin/phpstan analyze ${files.join(
+            ' ',
+        )} || echo "Couldn\'t run php-cs-fixer."`,
     ],
 };

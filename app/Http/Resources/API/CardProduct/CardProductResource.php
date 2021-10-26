eb8745ed93c122183567ba4b4594777bcfd7c86d
Kazmi <sydabdrehman@gmail.com>
2021-10-27 04:01:09 +0500

    string issue fixed

--- app/Http/Resources/API/CardProduct/CardProductResource.php
+++ app/Http/Resources/API/CardProduct/CardProductResource.php
@@ -9,21 +9,21 @@ class CardProductResource extends BaseResource
     public function toArray($request): array
     {
         return [
-            "id" => $this->id,
-            "long_name" => $this->getLongName(),
-            "short_name" => $this->getShortName(),
-            "name" => $this->name,
-            "card_category_name" => $this->cardCategory->name,
-            "card_set_name" => $this->cardSet->name,
-            "card_series_name" => $this->cardSet->cardSeries->name,
-            "release_date" => $this->formatDate($this->cardSet->release_year),
-            "release_year" => $this->cardSet->release_year,
-            "card_number_order" => $this->getFormattedCardNumber(),
-            "image_path" => $this->image_path,
-            "language" => $this->language,
-            "variant" => $this->variant,
-            "surface" => $this->surface,
-            "edition" => $this->edition,
+            'id' => $this->id,
+            'long_name' => $this->getLongName(),
+            'short_name' => $this->getShortName(),
+            'name' => $this->name,
+            'card_category_name' => $this->cardCategory->name,
+            'card_set_name' => $this->cardSet->name,
+            'card_series_name' => $this->cardSet->cardSeries->name,
+            'release_date' => $this->formatDate($this->cardSet->release_year),
+            'release_year' => $this->cardSet->release_year,
+            'card_number_order' => $this->getFormattedCardNumber(),
+            'image_path' => $this->image_path,
+            'language' => $this->language,
+            'variant' => $this->variant,
+            'surface' => $this->surface,
+            'edition' => $this->edition,
         ];
     }
 }

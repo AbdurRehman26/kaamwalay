0b793bb66f1b75e74023e4a726bed0bf932f1f4c
Muhammad Nauman <naumancs@gmail.com>
2021-10-28 00:25:31 +0500

    added the method to get payment type

--- app/Http/Resources/API/Customer/Order/OrderPaymentResource.php
+++ app/Http/Resources/API/Customer/Order/OrderPaymentResource.php
@@ -54,4 +54,16 @@ class OrderPaymentResource extends JsonResource
             ],
         ];
     }
+
+    /**
+     * @param int<1, 3> $type
+     */
+    public function getPaymentType(int $type): string
+    {
+        return match ($type) {
+            OrderPayment::TYPE_ORDER_PAYMENT => 'order_payment',
+            OrderPayment::TYPE_EXTRA_CHARGE => 'extra_charge',
+            OrderPayment::TYPE_REFUND => 'refund',
+        };
+    }
 }

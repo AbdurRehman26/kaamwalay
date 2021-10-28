d5da6437eb6cd35261d6f52502b37504b6aed905
Dumitrana Alinus <alin.designstudio@gmail.com>
2021-10-28 16:18:58 +0300

    chore: maintenance

--- resources/ts/shared/components/RefundsAndExtraCharges/PaymentEditNotesModal.tsx
+++ resources/ts/shared/components/RefundsAndExtraCharges/PaymentEditNotesModal.tsx
@@ -47,7 +47,7 @@ export default function PaymentEditNotesModal({
 
     useEffect(() => {
         setNotes(existingNotes);
-    }, [showEditNotes]);
+    }, [existingNotes, showEditNotes]);
 
     return (
         <div>

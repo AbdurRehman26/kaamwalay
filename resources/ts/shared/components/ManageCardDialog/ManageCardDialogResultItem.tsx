d233be70ae976934b1afb269cf62f6d6d4efe4eb
cepem <emanuel.cepoi@outlook.com>
2021-11-07 15:32:37 +0200

    Refactored front-end card names usages in order to match the new back-end names

--- resources/ts/shared/components/ManageCardDialog/ManageCardDialogResultItem.tsx
+++ resources/ts/shared/components/ManageCardDialog/ManageCardDialogResultItem.tsx
@@ -98,7 +98,7 @@ export function ManageCardDialogResultItem({ hit }: ManageCardDialogResultItemPr
                         ) : null}
                     </Box>
                 }
-                secondary={item.getDescription()}
+                secondary={item.getLongName()}
                 primaryTypographyProps={{ variant: 'body2' }}
                 secondaryTypographyProps={{ variant: 'caption' }}
                 className={classes.listItemText}

c2b1f3a142191fd505047df140afdcbee5263383
Kazmi <sydabdrehman@gmail.com>
2021-12-01 23:24:55 +0500

    form update

--- resources/ts/dashboard/pages/Profile/BasicInfo/BasicInfo.tsx
+++ resources/ts/dashboard/pages/Profile/BasicInfo/BasicInfo.tsx
@@ -1,6 +1,6 @@
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import React from 'react';
+import React, { useReducer } from 'react';
 import makeStyles from '@mui/styles/makeStyles';
 import Table from '@mui/material/Table';
 import TableRow from '@mui/material/TableRow';
@@ -11,12 +11,18 @@ import { useSharedSelector } from '@shared/hooks/useSharedSelector';
 import { CameraAlt } from '@mui/icons-material';
 import Radio, { RadioProps } from '@mui/material/Radio';
 import withStyles from '@mui/styles/withStyles';
+import Divider from '@mui/material/Divider';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const useStyles = makeStyles((theme) => ({
     mainContainer: {
         width: '100%',
         padding: '16px',
-        marginTop: '25px',
+        marginTop: '0px',
+    },
+    editContainer: {
+        padding: 20,
     },
     headingLabel: {
         fontFamily: 'DDT',
@@ -106,6 +112,27 @@ const useStyles = makeStyles((theme) => ({
         marginTop: '24px',
         marginBottom: '12px',
     },
+    divider: {
+        width: '100%',
+        margin: theme.spacing(2.5, 0, 2.5),
+    },
+    buttonsContainer: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'right',
+        marginTop: '40px',
+        marginBottom: '64px',
+    },
+    nextBtn: {
+        color: '#fff',
+        width: '140px',
+        height: '48px',
+    },
+    backBtn: {
+        marginRight: '12px',
+        color: '#20BFB8',
+    },
 }));
 
 const GreenRadio = withStyles({
@@ -117,6 +144,17 @@ const GreenRadio = withStyles({
     checked: {},
 })((props: RadioProps) => <Radio color="default" {...props} />);
 
+function reducer(state: any, action: { type: string }) {
+    switch (action.type) {
+        case 'EDIT_NAME':
+            return { ...state, editName: !state.editName };
+        case 'EDIT_USERNAME':
+            return { ...state, editUserName: !state.editUserName };
+        default:
+            return { ...state };
+    }
+}
+
 /**
  *
  * @author: Kazmi <abdur.rehman@wooter.co>
@@ -126,24 +164,14 @@ const GreenRadio = withStyles({
  */
 export function BasicInfo() {
     const classes = useStyles();
+    const [state, dispatch] = useReducer(reducer, {
+        type: '',
+        editName: false,
+        editUserName: false,
+    });
 
     const user$ = useSharedSelector((state) => state.authentication.user);
 
-    const onChangeFile = ({ target }: any) => {
-        console.log();
-        const formData = new FormData();
-        formData.append('file', target.files[0]);
-
-        const requestOptions = {
-            method: 'POST',
-            body: formData,
-        };
-        fetch('http://localhost/api/upload-file', requestOptions)
-            .then((response) => response.json())
-            .then((data) => console.log(data.id));
-
-        // `current` points to the mounted file input element
-    };
     const avatarIcon = (
         <IconButton aria-label="delete" size="small">
             <CameraAlt fontSize="medium" />
@@ -155,6 +183,8 @@ export function BasicInfo() {
             <Typography variant={'h1'} className={classes.headingLabel}>
                 Profile
             </Typography>
+
+            <Divider className={classes.divider} />
 
             <Paper className={classes.mainContainer} variant={'outlined'}>
                 <Typography variant={'h1'} className={classes.subHeadingLabel}>
@@ -172,39 +202,107 @@ export function BasicInfo() {
                                 <TableCell align="left">Personalize your account with a photo</TableCell>
                                 <TableCell align="right">
                                     {avatarIcon}
-                                    <input
-                                        onChange={onChangeFile}
-                                        accept="image/*"
-                                        id="outlined-button-file"
-                                        multiple
-                                        type="file"
-                                    />
+                                    <input accept="image/*" id="outlined-button-file" multiple type="file" />
                                 </TableCell>
                             </TableRow>
                             <TableRow>
-                                <TableCell component="th" scope="row" align={'left'}>
-                                    <Typography variant={'subtitle1'} className={classes.textLabel}>
-                                        NAME
-                                    </Typography>
-                                </TableCell>
-                                <TableCell align="left">{user$?.firstName + ' ' + user$?.lastName}</TableCell>
-                                <TableCell align="right" className={classes.editBtn}>
-                                    EDIT
-                                </TableCell>
+                                {!state.editName && (
+                                    <>
+                                        <TableCell component="th" scope="row" align={'left'}>
+                                            <Typography variant={'subtitle1'} className={classes.textLabel}>
+                                                NAME
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell align="left">{user$?.firstName + ' ' + user$?.lastName}</TableCell>
+
+                                        <TableCell align="right">
+                                            <Button
+                                                variant={'text'}
+                                                size={'medium'}
+                                                onClick={() => dispatch({ type: 'EDIT_NAME' })}
+                                            >
+                                                Edit
+                                            </Button>
+                                        </TableCell>
+                                    </>
+                                )}
+
+                                {state.editName && (
+                                    <div className={classes.editContainer}>
+                                        <Typography variant={'h1'} className={classes.subHeadingLabel}>
+                                            Name
+                                        </Typography>
+
+                                        <TextField label="Enter Notes" rows={1} sx={{ marginTop: '16px' }} fullWidth />
+                                        <TextField label="Enter Notes" rows={1} sx={{ marginTop: '16px' }} fullWidth />
+
+                                        <div className={classes.buttonsContainer}>
+                                            <Button
+                                                variant={'text'}
+                                                color={'secondary'}
+                                                className={classes.backBtn}
+                                                onClick={() => {
+                                                    dispatch({ type: 'EDIT_NAME' });
+                                                }}
+                                            >
+                                                Cancel
+                                            </Button>
+                                            <Button variant={'contained'} color={'primary'} className={classes.nextBtn}>
+                                                Save
+                                            </Button>
+                                        </div>
+                                    </div>
+                                )}
                             </TableRow>
 
                             <TableRow>
-                                <TableCell component="th" scope="row" align={'left'}>
-                                    <Typography variant={'subtitle1'} className={classes.textLabel}>
-                                        USERNAME
-                                    </Typography>
-                                </TableCell>
-                                <TableCell align="left" className={classes.valueLabel}>
-                                    {user$?.username}
-                                </TableCell>
-                                <TableCell align="right" className={classes.editBtn}>
-                                    EDIT
-                                </TableCell>
+                                {!state.editUserName && (
+                                    <>
+                                        <TableCell component="th" scope="row" align={'left'}>
+                                            <Typography variant={'subtitle1'} className={classes.textLabel}>
+                                                USERNAME
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell align="left" className={classes.valueLabel}>
+                                            {user$?.username}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            <Button
+                                                variant={'text'}
+                                                size={'medium'}
+                                                onClick={() => dispatch({ type: 'EDIT_USERNAME' })}
+                                            >
+                                                Edit
+                                            </Button>
+                                        </TableCell>
+                                    </>
+                                )}
+
+                                {state.editUserName && (
+                                    <div className={classes.editContainer}>
+                                        <Typography variant={'h1'} className={classes.subHeadingLabel}>
+                                            Username
+                                        </Typography>
+
+                                        <TextField label="Enter Notes" rows={1} sx={{ marginTop: '16px' }} fullWidth />
+
+                                        <div className={classes.buttonsContainer}>
+                                            <Button
+                                                variant={'text'}
+                                                color={'secondary'}
+                                                className={classes.backBtn}
+                                                onClick={() => {
+                                                    dispatch({ type: 'EDIT_USERNAME' });
+                                                }}
+                                            >
+                                                Cancel
+                                            </Button>
+                                            <Button variant={'contained'} color={'primary'} className={classes.nextBtn}>
+                                                Save
+                                            </Button>
+                                        </div>
+                                    </div>
+                                )}
                             </TableRow>
                             <TableRow>
                                 <TableCell component="th" scope="row" align={'left'}>
@@ -215,8 +313,14 @@ export function BasicInfo() {
                                 <TableCell align="left" className={classes.valueLabel}>
                                     ********
                                 </TableCell>
-                                <TableCell align="right" className={classes.editBtn}>
-                                    EDIT
+                                <TableCell align="right">
+                                    <Button
+                                        variant={'text'}
+                                        size={'medium'}
+                                        onClick={() => dispatch({ type: 'EDIT_NAME' })}
+                                    >
+                                        Edit
+                                    </Button>
                                 </TableCell>
                             </TableRow>
                             <TableRow>
@@ -227,9 +331,6 @@ export function BasicInfo() {
                                 </TableCell>
                                 <TableCell align="left" className={classes.valueLabel}>
                                     {user$?.customerNumber}
-                                </TableCell>
-                                <TableCell align="right" className={classes.editBtn}>
-                                    EDIT
                                 </TableCell>
                             </TableRow>
                         </TableBody>
@@ -252,8 +353,14 @@ export function BasicInfo() {
                                 <TableCell align="left" className={classes.valueLabel}>
                                     Personalize your account with a photo
                                 </TableCell>
-                                <TableCell align="right" className={classes.editBtn}>
-                                    EDIT
+                                <TableCell align="right">
+                                    <Button
+                                        variant={'text'}
+                                        size={'medium'}
+                                        onClick={() => dispatch({ type: 'EDIT_NAME' })}
+                                    >
+                                        Edit
+                                    </Button>
                                 </TableCell>
                             </TableRow>
                             <TableRow>
@@ -265,8 +372,14 @@ export function BasicInfo() {
                                 <TableCell align="left" className={classes.valueLabel}>
                                     {user$?.customerNumber}
                                 </TableCell>
-                                <TableCell align="right" className={classes.editBtn}>
-                                    EDIT
+                                <TableCell align="right">
+                                    <Button
+                                        variant={'text'}
+                                        size={'medium'}
+                                        onClick={() => dispatch({ type: 'EDIT_NAME' })}
+                                    >
+                                        Edit
+                                    </Button>
                                 </TableCell>
                             </TableRow>
                         </TableBody>

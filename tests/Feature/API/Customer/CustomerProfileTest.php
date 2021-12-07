1099e8d0106d7768b9330820ce7dec5ef387b412
Kazmi <sydabdrehman@gmail.com>
2021-12-07 17:10:29 +0500

    test added

---
+++ tests/Feature/API/Customer/CustomerProfileTest.php
@@ -0,0 +1,73 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+
+uses(WithFaker::class);
+
+beforeEach(function () {
+    $this->user = User::factory()->create();
+    $this->anotherUser = User::factory()->create();
+});
+
+test('a customer can update his profile', function () {
+    $this->actingAs($this->user);
+
+    $response = $this->postJson('/api/customer/update-profile', [
+            'first_name' => 'first',
+            'last_name' => 'Last',
+            'email_subscription' => true,
+            'phone' => '1234567890',
+            'username' => 'test24',
+            'profile_image' => 'https://via.placeholder.com/150'
+    ]);
+
+    $response->assertSuccessful();
+    $response->assertStatus(200);
+    $response->assertJsonStructure([
+        'data' => [
+            'id',
+            'first_name',
+            'last_name',
+            'email_subscription',
+            'phone',
+            'username',
+            'email',
+            'profile_image',
+            'customer_number',
+        ],
+    ]);
+});
+
+test('customer profile update required fields error', function () {
+    $this->actingAs($this->user);
+
+    $this->actingAs($this->user);
+
+    $response = $this->postJson('/api/customer/update-profile', [
+        'first_name' => '',
+        'last_name' => '',
+        'email_subscription' => '',
+        'phone' => '',
+        'username' => '',
+        'profile_image' => ''
+    ]);
+
+    $response->assertInvalid([
+        'first_name',
+        'last_name',
+        'email_subscription',
+        'username',
+    ]);
+});
+
+test('a customer`s username can not be duplicate', function () {
+    $this->actingAs($this->user);
+
+    $response = $this->postJson('/api/customer/update-profile', [
+        'username' => $this->anotherUser->username,
+    ]);
+
+    $response->assertUnprocessable();
+
+});

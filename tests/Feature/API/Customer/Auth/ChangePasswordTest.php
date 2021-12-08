64c799e5d76f65dd7420a6da7923983afa979b82
Kazmi <sydabdrehman@gmail.com>
2021-12-08 14:19:21 +0500

    change password validations added

---
+++ tests/Feature/API/Customer/Auth/ChangePasswordTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\User;
+
+test('user can change password', function () {
+    $user = User::factory()->create();
+
+    $this->actingAs($user);
+
+    $response = $this->postJson('/api/auth/password/change', [
+        'current_password' => 'password',
+        'password' => '123Robograding456',
+        'password_confirmation' => '123Robograding456'
+    ]);
+
+    $response->assertStatus(201);
+
+})->group('auth');
+
+test('user is validated while changing password', function () {
+    $user = User::factory()->create();
+
+    $this->actingAs($user);
+
+    $response = $this->postJson('/api/auth/password/change', [
+        'current_password' => '1password',
+        'password' => 'password',
+        'password_confirmation' => '1password'
+    ]);
+
+    $response->assertStatus(422);
+
+    $response->assertInvalid([
+        'current_password',
+        'password',
+    ]);
+
+
+})->group('auth');

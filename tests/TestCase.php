1df64a9751de6eac05cb60a7ed2975c81b9fb523
Dumitrana Alinus <alin.designstudio@gmail.com>
2021-12-17 22:21:16 +0200

    add: ability to change bucket on file service by admins

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,16 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Spatie\Permission\PermissionRegistrar;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, LazilyRefreshDatabase;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        // now re-register all the roles and permissions (clears cache and reloads relations)
+        $this->app->make(PermissionRegistrar::class)->registerPermissions();
+    }
 }

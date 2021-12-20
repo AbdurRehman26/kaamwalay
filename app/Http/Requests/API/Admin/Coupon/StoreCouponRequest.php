0e5e7595068bf79e50bef7fa0e3a69543f3eb51b
Muhammad Nauman <naumancs@gmail.com>
2021-12-20 18:09:09 +0500

    added validation rules

--- app/Http/Requests/API/Admin/Coupon/StoreCouponRequest.php
+++ app/Http/Requests/API/Admin/Coupon/StoreCouponRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\API\Admin\Coupon;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreCouponRequest extends FormRequest
 {
@@ -13,7 +14,7 @@ class StoreCouponRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return $this->user()->isAdmin();
     }
 
     /**
@@ -24,7 +25,13 @@ class StoreCouponRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'code' => ['required'],
+            'discount_type' => ['required', 'in:fixed,percentage'],
+            'discount_value' => ['required', 'numeric'],
+            'coupon_applicable_id' => ['required', 'exists:coupon_applicables'],
+            'available_from' => ['required', 'date_format:Y-m-d H:i:s'],
+            'is_permanent' => ['required', 'filled'],
+            'available_till' => [Rule::requiredIf(boolval($this->get('is_permanent'))), 'date_format:Y-m-d H:i:s'],
         ];
     }
 }

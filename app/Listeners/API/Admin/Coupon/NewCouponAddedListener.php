d6675868dfbb374648da227acd3314b317aa494e
muhammad-nauman <muhammad-nauman@users.noreply.github.com>
2021-12-21 12:02:50 +0000

    Fix styling

--- app/Listeners/API/Admin/Coupon/NewCouponAddedListener.php
+++ app/Listeners/API/Admin/Coupon/NewCouponAddedListener.php
@@ -4,8 +4,6 @@ namespace App\Listeners\API\Admin\Coupon;
 
 use App\Events\API\Admin\Coupon\NewCouponAdded;
 use App\Services\Admin\Coupon\CouponService;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 
 class NewCouponAddedListener
 {

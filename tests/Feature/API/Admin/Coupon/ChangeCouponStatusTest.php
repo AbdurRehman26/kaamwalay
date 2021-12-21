9c35fdd2c735d6fe2597a7e63ab76c32fc96234d
muhammad-nauman <muhammad-nauman@users.noreply.github.com>
2021-12-21 15:06:03 +0000

    Fix styling

--- tests/Feature/API/Admin/Coupon/ChangeCouponStatusTest.php
+++ tests/Feature/API/Admin/Coupon/ChangeCouponStatusTest.php
@@ -6,8 +6,8 @@ use App\Models\User;
 use Database\Seeders\RolesSeeder;
 use Illuminate\Foundation\Testing\WithFaker;
 use function Pest\Laravel\actingAs;
-use function Pest\Laravel\seed;
 use function Pest\Laravel\putJson;
+use function Pest\Laravel\seed;
 
 uses(WithFaker::class);
 

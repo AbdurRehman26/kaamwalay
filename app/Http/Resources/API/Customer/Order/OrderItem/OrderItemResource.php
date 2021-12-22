b3457f56ae6009c939671ec87334ca87b1249730
Ali Raza <ali@wooter.com>
2021-12-22 13:03:21 +0500

    code fix

--- app/Http/Resources/API/Customer/Order/OrderItem/OrderItemResource.php
+++ app/Http/Resources/API/Customer/Order/OrderItem/OrderItemResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\API\Customer\Order\OrderItem;
 
 use App\Http\Resources\API\BaseResource;
+use App\Http\Resources\API\CardProduct\CardProductResource;
 
 class OrderItemResource extends BaseResource
 {
@@ -14,6 +15,7 @@ class OrderItemResource extends BaseResource
             'order_id' => $this->order_id,
             'declared_value_per_unit' => $this->declared_value_per_unit,
             'notes' => $this->notes ?? '',
+            'card_product' => new CardProductResource($this->cardProduct),
             'status' => new OrderItemStatusResource($this->orderItemStatus),
         ];
     }

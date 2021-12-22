d7223f4cc319d39fecc9b005299599733aa06669
Muhammad Nauman <naumancs@gmail.com>
2021-12-22 18:11:39 +0500

    updated route to remove duplication

--- resources/apidocs/endpoints/admin/coupons/coupon-applicables/index.js
+++ resources/apidocs/endpoints/admin/coupons/coupon-applicables/index.js
@@ -1,5 +1,5 @@
 /**
- * @api {get} /admin/coupons/applicables List Coupon Applicables
+ * @api {get} /admin/coupon-applicables List Coupon Applicables
  * @apiName List Coupon Applicables
  * @apiGroup Admin Coupons
  *

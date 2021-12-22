129ece9e1e915872e0b87fbe3cf15656246b70c3
AbdurRehman26 <AbdurRehman26@users.noreply.github.com>
2021-12-22 20:32:13 +0000

    Fix styling

--- app/Services/Coupon/CouponApplicable/ServiceLevelCoupon.php
+++ app/Services/Coupon/CouponApplicable/ServiceLevelCoupon.php
@@ -15,6 +15,7 @@ class ServiceLevelCoupon extends CouponApplicables implements CouponApplicableIn
     public function getPercentageDiscount(Coupon $coupon, Order|array $order): float
     {
         $serviceFee = $this->getPaymentPlan($order)->price * array_sum(array_column($this->getOrderItems($order), 'quantity'));
+
         return (($coupon->discount_value * $serviceFee) / 100);
     }
 }

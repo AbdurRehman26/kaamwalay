ad28db274a65f27160ae102758bcf6be42f46a3c
Ali Raza <ali@wooter.com>
2021-12-23 17:44:44 +0500

    code revert

--- app/Http/Resources/API/Customer/Order/OrderItem/OrderItemResource.php
+++ app/Http/Resources/API/Customer/Order/OrderItem/OrderItemResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\API\Customer\Order\OrderItem;
 
 use App\Http\Resources\API\BaseResource;
 use App\Http\Resources\API\CardProduct\CardProductResource;
+use App\Http\Resources\API\Customer\UserCard\UserCardResource;
 
 class OrderItemResource extends BaseResource
 {
@@ -14,9 +15,11 @@ class OrderItemResource extends BaseResource
             'quantity' => $this->quantity,
             'order_id' => $this->order_id,
             'declared_value_per_unit' => $this->declared_value_per_unit,
-            'notes' => $this->notes ?? '',
+            // TODO: move certificate number inside the right relationship.
+            'certificate_number' => $this->userCard?->certificate_number,
             'card_product' => new CardProductResource($this->cardProduct),
             'status' => new OrderItemStatusResource($this->orderItemStatus),
+            'user_card' => $this->whenLoaded('userCard', UserCardResource::class),
         ];
     }
 }

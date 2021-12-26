b4488f51748b2a745356ab5dfebdfc424ddaddc2
Dumitrana Alinus <alin.designstudio@gmail.com>
2021-12-27 01:39:29 +0200

    rm: jest and TS check from the commit hook.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-    '**/*.ts?(x)': (files) => [`eslint --cache --ext ".ts,.tsx --fix ${files.join(' ')}`, 'tsc-files --noEmit'],
-    '**/*.spec.ts?(x)': 'jest --bail --findRelatedTests',
+    // '**/*.spec.ts?(x)': 'jest --bail --findRelatedTests',
+    '**/*.ts?(x)': (files) => [
+        // 'tsc-files --noEmit'
+        `eslint --cache --ext ".ts,.tsx --fix ${files.join(' ')}`,
+    ],
     '**/*.{js,jsx,ts,tsx,json,scss}': ['prettier -w'],
     '**/*.php': (files) => [
         `bash -c "./vendor/bin/sail run laravel.test ./vendor/bin/php-cs-fixer fix --config=.php-cs-fixer.php --allow-risky=yes ${files.join(

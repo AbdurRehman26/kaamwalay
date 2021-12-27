3f253048d461fe12c640daf865460f7a73988fde
Kazmi <sydabdrehman@gmail.com>
2021-12-27 14:15:30 +0500

    doc fixed for order

--- resources/apidocs/endpoints/customer/orders/create.js
+++ resources/apidocs/endpoints/customer/orders/create.js
@@ -168,6 +168,12 @@
  *                "code": "insured_shipping",
  *                "name": "Insured Shipping"
  *            },
+ *            "coupon": {
+ *                  id: 1,
+ *                  code: "HvTGSYpak9",
+ *                  discount_statement: "20.00 Off",
+ *            },
+ *            "discounted_amount" : 40.00,
  *            "service_fee": 2556,
  *            "shipping_fee": 43,
  *            "grand_total": 263

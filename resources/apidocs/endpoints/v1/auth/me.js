0dd08d43e13d7cbfb7184e661a15e88b035935a1
Nabeel Ahmed <thisisnabeelahmed@gmail.com>
2021-12-28 05:43:59 +0500

    Added version in API Docs

---
+++ resources/apidocs/endpoints/v1/auth/me.js
@@ -0,0 +1,34 @@
+/**
+ * @api {get} /v1/auth/me Logged In User
+ * @apiName Logged In User Detail
+ * @apiGroup Authentication
+ *
+ * @apiVersion 1.0.0
+ *
+ * @apiUse header_main
+ * @apiUse Authorization
+ *
+ * @apiSuccess {Object} data containing token and user object
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *      {
+ *          "data": {
+ *              "user": {
+ *                  "id": 18,
+ *                  "first_name": "Jane",
+ *                  "last_name": "Doe",
+ *                  "email": "test@test.test",
+ *                  "username": "test",
+ *                  "phone": null,
+ *                  "stripe_id": "cus_K0651IOZjCy9Wa",
+ *                  "roles": [
+ *                      {
+ *                          "id": 2,
+ *                          "name": "customer"
+ *                      }
+ *                  ]
+ *              }
+ *          }
+ *      }
+ */

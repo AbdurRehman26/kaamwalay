390c9bcc7f71f77d754ae1caa1389bb5d33cbab9
Luis Molina <lm.molinab@gmail.com>
2022-01-04 10:05:33 -0500

    order payment details for dispaying

--- app/Http/Resources/API/V1/Customer/Order/OrderPaymentResource.php
+++ app/Http/Resources/API/V1/Customer/Order/OrderPaymentResource.php
@@ -21,7 +21,7 @@ class OrderPaymentResource extends BaseResource
 
         if ($this->order->paymentMethod->code === 'paypal') {
             return $this->paypalData(json_decode($this->response, associative: true) ?? []);
-        }  else if ($this->order->paymentMethod->code === 'ags') {
+        } else if ($this->order->paymentMethod->code === 'ags') {
             return $this->collectorCoinData(json_decode($this->response, associative: true) ?? []);
         }
 

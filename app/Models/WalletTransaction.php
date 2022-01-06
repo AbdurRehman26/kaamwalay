4f36cd38c654dffa46dba36664e9bf33449be214
muhammad-nauman <muhammad-nauman@users.noreply.github.com>
2022-01-06 21:04:50 +0000

    Fix styling

--- app/Models/WalletTransaction.php
+++ app/Models/WalletTransaction.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Casts\WalletTransactionReason;
 use App\Casts\WalletTransactionType;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;

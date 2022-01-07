5424fdd186c9a2634fe3e201b7c4e6ce01ed3b8c
Muhammad Nauman <naumancs@gmail.com>
2022-01-07 21:42:24 +0500

    moved single action controllers to single controller

--- tests/Feature/API/V1/Admin/Wallet/CustomerWalletHistoryTest.php
+++ tests/Feature/API/V1/Admin/Wallet/CustomerWalletHistoryTest.php
@@ -27,7 +27,7 @@ test('admin can get a wallet transaction history', function () {
     ))->create();
 
     actingAs($adminUser);
-    getJson(route('customer-wallet-history', ['wallet' => $customer->wallet]))
+    getJson(route('customer.wallet.history', ['wallet' => $customer->wallet]))
         ->assertOk()
         ->assertJsonCount(3, 'data');
 });

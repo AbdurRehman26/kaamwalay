b25ee38dfa8c5c9d242cabbf639bf4641e4f3f0b
Muhammad Nauman <naumancs@gmail.com>
2022-01-09 17:34:55 +0500

    fixed existing test

--- tests/Feature/API/V1/Customer/CardProduct/CardProductTest.php
+++ tests/Feature/API/V1/Customer/CardProduct/CardProductTest.php
@@ -1,38 +1,44 @@
 <?php
 
 use App\Jobs\ProcessImage;
+use App\Models\CardCategory;
 use App\Models\User;
 use Database\Seeders\RolesSeeder;
 use Illuminate\Support\Facades\Bus;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\postJson;
+use function Pest\Laravel\seed;
 
 beforeEach(function () {
-    $this->seed([
-        RolesSeeder::class,
-    ]);
+    seed([RolesSeeder::class]);
 
-    $this->user = User::factory()
-        ->withRole(config('permission.roles.customer'))
-        ->create();
-
-    $this->actingAs($this->user);
+    actingAs(User::factory()->withRole(config('permission.roles.customer'))->create());
 });
 
 test('customers can create cards manually', function () {
     Bus::fake();
 
-    $response = $this->postJson('/api/v1/customer/cards', [
+    postJson('/api/v1/customer/cards', [
         'name' => 'Lorem Ipsum',
         'description' => 'Lorem ipsum dolor sit amet.',
         'image_path' => 'http://www.google.com',
-    ]);
-
-    $response->assertSuccessful();
-    $response->assertJsonFragment([
-        'name' => 'Lorem Ipsum',
-        'short_name' => 'Added Manually',
-        'long_name' => 'Lorem ipsum dolor sit amet.',
-        'image_path' => 'http://www.google.com',
-    ]);
+        'card_category_id' => CardCategory::factory()->create()->id,
+    ])
+        ->assertSuccessful()
+        ->assertJsonFragment([
+            'name' => 'Lorem Ipsum',
+            'short_name' => 'Added Manually',
+            'long_name' => 'Lorem ipsum dolor sit amet.',
+            'image_path' => 'http://www.google.com',
+        ]);
 
     Bus::assertDispatched(ProcessImage::class);
+});
+
+test('customers can not create cards manually without specifying card category', function () {
+    postJson('/api/v1/customer/cards', [
+        'name' => 'Lorem Ipsum',
+        'description' => 'Lorem ipsum dolor sit amet.',
+        'image_path' => 'http://www.google.com',
+    ])->assertUnprocessable();
 });

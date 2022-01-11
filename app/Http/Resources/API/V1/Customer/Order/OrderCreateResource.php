e53b939d38f34106dc5206c6998907b433b1cdf2
Kazmi <sydabdrehman@gmail.com>
2022-01-11 17:15:53 +0500

    order create resource grand total fixed

--- app/Http/Resources/API/V1/Customer/Order/OrderCreateResource.php
+++ app/Http/Resources/API/V1/Customer/Order/OrderCreateResource.php
@@ -25,7 +25,7 @@ class OrderCreateResource extends JsonResource
             'shipping_method' => new ShippingMethodResource($this->shippingMethod),
             'service_fee' => $this->service_fee,
             'shipping_fee' => $this->shipping_fee,
-            'grand_total' => $this->grand_total,
+            'grand_total' => $this->grand_total_to_be_paid,
             'amount_paid_from_wallet' => $this->amount_paid_from_wallet,
         ];
     }

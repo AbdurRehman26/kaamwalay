5eb95c8e9c6dfa6464ca2ece899493236fccba09
Kazmi <sydabdrehman@gmail.com>
2022-01-11 18:58:07 +0500

    fixed static issue

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -236,7 +236,7 @@ class Order extends Model
 
     public function getGrandTotalCentsAttribute(): int
     {
-        return ($this->grand_total_to_be_paid) * 100;
+        return (int) (($this->grand_total_to_be_paid) * 100);
     }
 
     public function getGrandTotalToBePaidAttribute(): float

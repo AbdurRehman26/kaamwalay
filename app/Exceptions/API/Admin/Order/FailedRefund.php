b227ba8bdde2c30895d870e17559aff7aad51404
Luis Molina <lm.molinab@gmail.com>
2022-01-20 09:30:03 -0500

    added render method to refund fail exception

--- app/Exceptions/API/Admin/Order/FailedRefund.php
+++ app/Exceptions/API/Admin/Order/FailedRefund.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions\API\Admin\Order;
 
 use Exception;
+use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class FailedRefund extends Exception
@@ -15,4 +16,13 @@ class FailedRefund extends Exception
      * @var int
      */
     protected $code = Response::HTTP_UNPROCESSABLE_ENTITY;
+
+    /**
+     * @param Request|array $request
+     * @return JsonResponse
+     */
+    public function render($request): JsonResponse
+    {
+        return new JsonResponse([ 'error' => $this->message ], $this->code);
+    }
 }

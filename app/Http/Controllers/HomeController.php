b0fbf46c6c3395ccb9caa7c0c7b67724cb35d4e1
Dumitrana Alinus <alin.designstudio@gmail.com>
2022-02-18 08:55:55 +0200

    add: home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CardCategory;
+use App\Models\PaymentPlan;
 use Illuminate\View\View;
 
 class HomeController extends Controller
 {
     public function getView(): View
     {
-        return view('landings.home.view');
+        $categories = CardCategory::query()->latest()->limit(8)->get();
+        $services = PaymentPlan::query()->orderBy('display_position')->get();
+
+        return view('landings.home.view', compact('categories', 'services'));
     }
 }

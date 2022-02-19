b7b54f0686ddad39b9da7e3da36a943308611a9f
AbdurRehman26 <AbdurRehman26@users.noreply.github.com>
2022-02-19 12:33:06 +0000

    Fix styling

--- app/Console/Commands/PopReports/InitializePopReports.php
+++ app/Console/Commands/PopReports/InitializePopReports.php
@@ -29,7 +29,7 @@ class InitializePopReports extends Command
             '0'
         );
 
-        if($selectedOption === "All" ){
+        if ($selectedOption === "All") {
             $popReportService->initializePopReportsForAll();
         }
 

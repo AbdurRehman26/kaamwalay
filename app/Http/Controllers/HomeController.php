a4cc7923be044aad796e0faf5ed0979bb9264060
Nabeel Ahmed <thisisnabeelahmed@gmail.com>
2022-02-21 19:06:24 +0500

    CR fixes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,14 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\CardCategory;
 use App\Models\PaymentPlan;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 
 class HomeController extends Controller
 {
     public function getView(): View
     {
-        $categories = CardCategory::query()->latest()->limit(8)->get();
-        $services = PaymentPlan::query()->orderBy('display_position')->get();
+        $categories = Cache::remember(
+            'homepage:card_categories',
+            now()->addMonth(),
+            fn () => CardCategory::latest()->limit(8)->get()
+        );
+
+        $services = Cache::remember(
+            'homepage:payment_plans',
+            now()->addMonth(),
+            fn () => PaymentPlan::orderBy('display_position')->get()
+        );
 
         return view('landings.home.view', compact('categories', 'services'));
     }

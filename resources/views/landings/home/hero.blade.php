1fe52a544e25b68331c4fbb211988e95fdf4f581
Nabeel Ahmed <thisisnabeelahmed@gmail.com>
2022-02-23 02:34:22 +0500

    Fix

--- resources/views/landings/home/hero.blade.php
+++ resources/views/landings/home/hero.blade.php
@@ -7,9 +7,9 @@
 
     <div class="Home-heroContent">
         <div class="container">
-            <h1 class="Home-heroTitle" data-aos="fade-up" data-aos-easing="ease-in-out">The fastest, most accurate way to grade your cards.</h1>
-            <h2 class="Home-heroSubtitle" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-delay="100">Get your cards graded with artificial intelligence.</h2>
-            <a href="/dashboard/submissions/new" class="Home-buttonCta" data-aos="fade-in" data-aos-easing="ease-in-out" data-aos-delay="200">Grade your cards</a>
+            <h1 class="Home-heroTitle">The fastest, most accurate way to grade your cards.</h1>
+            <h2 class="Home-heroSubtitle">Get your cards graded with artificial intelligence.</h2>
+            <a href="/dashboard/submissions/new" class="Home-buttonCta">Grade your cards</a>
         </div>
     </div>
 </div>

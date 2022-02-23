c8b571791f681c6b7754100ceca0c3533ce533dc
Kazmi <sydabdrehman@gmail.com>
2022-02-23 21:36:35 +0500

    removed test for migration schema

--- tests/Feature/Command/MigrateShipmentsToNewSchemaTest.php
+++ tests/Feature/Command/MigrateShipmentsToNewSchemaTest.php
@@ -10,9 +10,6 @@ use App\Models\User;
 use App\Services\Admin\V2\OrderStatusHistoryService;
 use Illuminate\Support\Facades\Event;
 
-use function Pest\Laravel\assertDatabaseCount;
-use function Pest\Laravel\assertDatabaseHas;
-
 beforeEach(function () {
     Event::fake([
         OrderStatusChangedEvent::class,
@@ -43,36 +40,4 @@ beforeEach(function () {
             'order_item_customer_shipment_id' => $this->orderItemCustomerShipment->id,
             'order_item_shipment_id' => $this->orderItemShipment->id,
         ]);
-});
-
-it('migrates existing customer shipments to new schema', function () {
-    $this->artisan('orders:migrate-shipments-to-new-schema')
-        ->expectsQuestion('Your account email', $this->user->email)
-        ->assertExitCode(0);
-
-    $this->order->refresh();
-
-    assertDatabaseCount('order_customer_shipments', 1);
-    assertDatabaseHas('order_customer_shipments', [
-        'tracking_number' => $this->orderItemCustomerShipment->tracking_number,
-        'shipping_provider' => $this->orderItemCustomerShipment->shipping_provider,
-    ]);
-
-    expect($this->order->order_customer_shipment_id)->not()->toBeNull();
-});
-
-it('migrates existing admin shipments to new schema', function () {
-    $this->artisan('orders:migrate-shipments-to-new-schema')
-        ->expectsQuestion('Your account email', $this->user->email)
-        ->assertExitCode(0);
-
-    $this->order->refresh();
-
-    assertDatabaseCount('order_shipments', 1);
-    assertDatabaseHas('order_shipments', [
-        'tracking_number' => $this->orderItemShipment->tracking_number,
-        'shipping_provider' => $this->orderItemShipment->shipping_provider,
-    ]);
-
-    expect($this->order->order_shipment_id)->not()->toBeNull();
 });

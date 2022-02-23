cdade3ad7a273bd7f9537ffad93c0e95d47167f1
Kazmi <sydabdrehman@gmail.com>
2022-02-23 21:22:23 +0500

    categories tests fixed finally

--- tests/Feature/API/V2/Customer/CardCategory/CardCategoriesTest.php
+++ tests/Feature/API/V2/Customer/CardCategory/CardCategoriesTest.php
@@ -3,6 +3,8 @@
 use App\Models\CardCategory;
 use App\Models\User;
 use Database\Seeders\RolesSeeder;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\getJson;
 use function Pest\Laravel\seed;
@@ -10,10 +12,18 @@ use function Pest\Laravel\seed;
 test('customer can receive card categories', function () {
     seed([RolesSeeder::class]);
     actingAs(User::factory()->withRole(config('permission.roles.customer'))->create());
-    CardCategory::factory()->count(5)->create();
+
+    CardCategory::factory()->count(4)
+        ->state(new Sequence(
+            ['is_enabled' => true],
+            ['is_enabled' => true],
+            ['is_enabled' => false],
+            ['is_enabled' => false]
+        ))
+        ->create();
 
     getJson(route('v2.cards.categories'))
         ->assertOk()
-        ->assertJsonCount(5, 'data')
+        ->assertJsonCount(CardCategory::enabled()->count(), 'data')
         ->assertJsonStructure(['data' => [['id', 'name']]]);
 });

1e89f320ca1ca202fcd3c4c4bc8d2f0696e3b75a
Kazmi <sydabdrehman@gmail.com>
2022-02-23 20:20:19 +0500

    category seeder fixed

--- database/seeders/CardCategoriesSeeder.php
+++ database/seeders/CardCategoriesSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\CardCategory;
 use Illuminate\Database\Seeder;
 
 class CardCategoriesSeeder extends Seeder
@@ -14,15 +15,18 @@ class CardCategoriesSeeder extends Seeder
      */
     public function run()
     {
-        
-        \DB::table('card_categories')->insert([
-            [
-                'created_at' => '2021-08-07 21:10:18',
-                'id' => 1,
-                'name' => 'Pokemon',
-                'updated_at' => '2021-08-07 21:10:18',
-            ],
-        ]);
-        
+        $cardCategory = CardCategory::where('name',  '=', 'Pokemon')->first();
+
+        if(empty($cardCategory)){
+            \DB::table('card_categories')->insert([
+                [
+                    'created_at' => '2021-08-07 21:10:18',
+                    'id' => 1,
+                    'name' => 'Pokemon',
+                    'updated_at' => '2021-08-07 21:10:18',
+                ],
+            ]);
+        }
+
     }
 }

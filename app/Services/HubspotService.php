9243e3782b822bef65431eff1886b8c21839389a
aliwooter <aliwooter@users.noreply.github.com>
2022-03-03 16:31:54 +0000

    Fix styling

--- app/Services/HubspotService.php
+++ app/Services/HubspotService.php
@@ -12,8 +12,8 @@ use SevenShores\Hubspot\Resources\Deals;
 use SevenShores\Hubspot\Resources\Owners;
 
 class HubspotService
-{   
-      // @phpstan-ignore-next-line
+{
+    // @phpstan-ignore-next-line
     public function getClient(): Client
     {
         // @phpstan-ignore-next-line

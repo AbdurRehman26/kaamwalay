838892e62b4c8e1420b8deed4301969110c1877d
Kazmi <sydabdrehman@gmail.com>
2022-03-08 23:58:49 +0500

    changes for PR

--- app/Http/Resources/API/V1/Customer/Order/OrderCreateResource.php
+++ app/Http/Resources/API/V1/Customer/Order/OrderCreateResource.php
@@ -29,7 +29,6 @@ class OrderCreateResource extends JsonResource
             'payment_method_id' => $this->payment_method_id,
             'grand_total' => $this->grand_total_to_be_paid,
             'amount_paid_from_wallet' => $this->amount_paid_from_wallet,
-            'order_step' => $this->order_step,
         ];
     }
 }

2218a26a9280e4855138eec8a155e537272af855
Kazmi <sydabdrehman@gmail.com>
2022-03-15 18:08:15 +0500

    done redirection

--- resources/ts/dashboard/components/SubmissionStep01Content.tsx
+++ resources/ts/dashboard/components/SubmissionStep01Content.tsx
@@ -2,9 +2,11 @@ import Divider from '@mui/material/Divider';
 import makeStyles from '@mui/styles/makeStyles';
 import React, { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
-import { getServiceLevels } from '../redux/slices/newSubmissionSlice';
+import { getServiceLevels, setCustomStep, setServiceLevel } from '../redux/slices/newSubmissionSlice';
 import ServiceLevelItem from './ServiceLevelItem';
 import StepDescription from './StepDescription';
+import { useLocation } from 'react-router-dom';
+import { find } from 'lodash';
 
 const useStyles = makeStyles((theme) => ({
     pageContainer: {
@@ -24,11 +26,22 @@ export function SubmissionStep01Content() {
     const classes = useStyles();
     const dispatch = useAppDispatch();
     const serviceLevels = useAppSelector((state) => state.newSubmission.step01Data.availableServiceLevels);
+    const { search } = useLocation();
+    const params: any = new URLSearchParams(search);
+    const plan = params?.get('plan');
+
     useEffect(() => {
         dispatch(getServiceLevels());
     }, [dispatch]);
 
-    useEffect(() => {}, [serviceLevels]);
+    useEffect(() => {
+        if (plan && serviceLevels.length > 1) {
+            const selectedService: any = find(serviceLevels, (service) => service.id === parseInt(plan));
+            const { id, price, turnaround, type, maxProtectionAmount } = selectedService;
+            dispatch(setServiceLevel({ id, price, turnaround, type, maxProtectionAmount }));
+            dispatch(setCustomStep(1));
+        }
+    }, [serviceLevels, dispatch, plan]);
 
     return (
         <div className={classes.pageContainer}>

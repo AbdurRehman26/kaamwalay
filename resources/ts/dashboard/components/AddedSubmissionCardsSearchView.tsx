1f264ce1b220beff952d636fe6d105b883f90aa3
Muhammad Nauman <naumancs@gmail.com>
2022-03-16 23:09:41 +0500

    review changes

--- resources/ts/dashboard/components/AddedSubmissionCardsSearchView.tsx
+++ resources/ts/dashboard/components/AddedSubmissionCardsSearchView.tsx
@@ -3,8 +3,9 @@ import Typography from '@mui/material/Typography';
 import { useAppSelector } from '../redux/hooks';
 import { SearchResultItemCardProps } from '../redux/slices/newSubmissionSlice';
 import SearchResultItemCard from './SearchResultItemCard';
-import { styled } from '@mui/material/styles';
 import { Fragment } from 'react';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
 
 function AddedSubmissionCardsMobileSearchView() {
     const selectedCards = useAppSelector((state) => state.newSubmission.step02Data.selectedCards);
@@ -14,50 +15,31 @@ function AddedSubmissionCardsMobileSearchView() {
     }
 
     return (
-        <SearchListRoot>
-            <div className={'SearchList-Title'}>
+        <>
+            <Grid container alignItems={'center'} mt={4.5}>
                 <Typography variant={'subtitle1'} fontWeight={500}>
                     Added Cards
                 </Typography>
-            </div>
+            </Grid>
             {selectedCards.map((row: SearchResultItemCardProps) => (
                 <Fragment key={row.id}>
-                    <div className={'SearchList-Container'}>
-                        <div title={row.shortName || row.name}>
-                            <SearchResultItemCard
-                                key={row.id}
-                                id={row.id}
-                                image={row.image}
-                                longName={row.longName}
-                                shortName={row.shortName}
-                                name={row.name}
-                                addedMode
-                                reviewMode
-                            />
-                        </div>
-                    </div>
+                    <Stack mt={1.5} title={row.shortName || row.name}>
+                        <SearchResultItemCard
+                            key={row.id}
+                            id={row.id}
+                            image={row.image}
+                            longName={row.longName}
+                            shortName={row.shortName}
+                            name={row.name}
+                            addedMode
+                            reviewMode
+                        />
+                    </Stack>
                     <Divider light />
                 </Fragment>
             ))}
-        </SearchListRoot>
+        </>
     );
 }
 
 export default AddedSubmissionCardsMobileSearchView;
-
-const SearchListRoot = styled('div')(
-    {
-        '.SearchList-Title': {
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            marginTop: 36,
-        },
-        '.SearchList-Container': {
-            display: 'flex',
-            flexDirection: 'column',
-            marginTop: 12,
-        },
-    },
-    { name: 'SearchListRoot' },
-);

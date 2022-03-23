e016646e65fb9e08463d55c4f7ea74eef25e25d2
Muhammad Nauman <naumancs@gmail.com>
2022-03-23 16:08:58 +0500

    fixed grand total calculation

--- app/Services/Order/V2/OrderPaymentService.php
+++ app/Services/Order/V2/OrderPaymentService.php
@@ -172,6 +172,9 @@ class OrderPaymentService
             + $this->order->shipping_fee
             - $this->order->discounted_amount
             - $this->order->payment_method_discounted_amount
+            - $this->order->refund_total
+            - $this->order->amount_paid_from_wallet
+            + $this->order->extra_charge_total
         );
 
         GrandTotalValidator::validate($this->order);

a2af99420a108da4769598a7509bd99234eae2c2
muhammad-nauman <muhammad-nauman@users.noreply.github.com>
2022-03-24 10:32:46 +0000

    Fix styling

--- app/Http/Resources/API/V2/Customer/UserCard/UserCardListResource.php
+++ app/Http/Resources/API/V2/Customer/UserCard/UserCardListResource.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Resources\API\V2\Customer\UserCard;
 
-use App\Models\UserCard;
 use App\Http\Resources\API\V1\Customer\UserCard\UserCardListResource as V1UserCardListResource;
+use App\Models\UserCard;
 
 /** @mixin UserCard */
 class UserCardListResource extends V1UserCardListResource

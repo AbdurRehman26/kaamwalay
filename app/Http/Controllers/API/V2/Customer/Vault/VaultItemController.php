ff4707e38e920be1c674713ffb4699a3d32dcde9
muhammad-nauman <muhammad-nauman@users.noreply.github.com>
2022-03-28 18:07:59 +0000

    Fix styling

--- app/Http/Controllers/API/V2/Customer/Vault/VaultItemController.php
+++ app/Http/Controllers/API/V2/Customer/Vault/VaultItemController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers\API\V2\Customer\Vault;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\API\V2\Customer\Vault\VaultItemCollection;
-use App\Http\Resources\API\V2\Customer\Vault\VaultItemResource;
 use App\Models\VaultItem;
-use Illuminate\Http\Request;
 
 class VaultItemController extends Controller
 {

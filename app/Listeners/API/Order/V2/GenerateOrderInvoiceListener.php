70e6d1df021df069e44b7489fcc21b1fd23d21b4
Muhammad Nauman <naumancs@gmail.com>
2022-03-28 20:28:45 +0500

    separate generate invoice event added

---
+++ app/Listeners/API/Order/V2/GenerateOrderInvoiceListener.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Listeners\API\Order\V2;
+
+use App\Events\API\Order\V2\GenerateOrderInvoice;
+use App\Exceptions\Services\Payment\InvoiceNotUploaded;
+use App\Models\Invoice;
+use App\Services\Payment\V2\InvoiceService;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class GenerateOrderInvoiceListener implements ShouldQueue
+{
+    use InteractsWithQueue;
+
+    public $tries = 5;
+
+    public function __construct(public InvoiceService $invoiceService)
+    {
+        //
+    }
+
+    /**
+     * @throws InvoiceNotUploaded
+     */
+    public function handle(GenerateOrderInvoice $event)
+    {
+        $order = $event->order;
+
+        if ($order->hasInvoice()) {
+            $order->invoice()->delete();
+        }
+
+        $this->invoiceService->saveInvoicePDF($order);
+    }
+}
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,6 +7,7 @@ use App\Concerns\Order\HasOrderPayments;
 use App\Contracts\Exportable;
 use App\Enums\Order\OrderPaymentStatusEnum;
 use App\Enums\Order\OrderStepEnum;
+use App\Events\API\Order\V2\GenerateOrderInvoice;
 use App\Http\Filters\AdminOrderSearchFilter;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -418,5 +419,12 @@ class Order extends Model implements Exportable
         $this->payment_status = OrderPaymentStatusEnum::PAID;
         $this->paid_at = now();
         $this->save();
+
+        GenerateOrderInvoice::dispatch($this);
+    }
+
+    public function hasInvoice(): bool
+    {
+        return $this->invoice()->exists();
     }
 }

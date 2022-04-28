8294f6dcaf1e8fd1fc0003cd57c0a847114688a2
Nabeel Ahmed <thisisnabeelahmed@gmail.com>
2022-04-28 07:39:22 +0500

    Show grades on public card page directly from Robograding instead of AGS

--- app/Services/Order/UserCardService.php
+++ app/Services/Order/UserCardService.php
@@ -10,6 +10,7 @@ use App\Models\User;
 use App\Models\UserCard;
 use App\Models\UserCardCertificate;
 use App\Services\Admin\CardGradingService;
+use Carbon\Carbon;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Str;
 use Spatie\QueryBuilder\AllowedFilter;
@@ -93,5 +94,101 @@ class UserCardService
         ])
         ->defaultSort('-order_item_status_histories.created_at')
         ->paginate($itemsPerPage);
+    }
+
+    public function getDataForPublicCardPage(string $certificateId): array
+    {
+        $userCard = UserCard::where('certificate_number', $certificateId)->first();
+
+        if ($userCard->orderItem->order->order_status_id !== OrderStatus::SHIPPED) {
+            return [
+                'grades_available' => false,
+            ];
+        }
+
+        return [
+            'grades_available' => true,
+            'certificate_id' => $userCard->certificate_number,
+            'grade' => $this->prepareGradeForPublicCardPage($userCard),
+            'card' => [
+                'name' => $userCard->orderItem->cardProduct->name,
+                'full_name' => $userCard->orderItem->cardProduct->getSearchableName(),
+                'image_path' => $userCard->orderItem->cardProduct->image_path,
+                'type' => $userCard->orderItem->cardProduct->cardCategory->name,
+                'series' => $userCard->orderItem->cardProduct->cardSet->cardSeries->name,
+                'set' => $userCard->orderItem->cardProduct->cardSet->name,
+                'release_date' => ! empty($userCard->orderItem->cardProduct->cardSet->release_date) ?
+                    Carbon::parse($userCard->orderItem->cardProduct->cardSet->release_date)->format('F d, Y') :
+                    null,
+                'number' => $userCard->orderItem->cardProduct->card_number_order ?? null,
+            ],
+            'overall' => $this->prepareOverallGradesForPublicCardPage($userCard),
+            'front_scan' => $this->prepareFrontScanGradesForPublicCardPage($userCard),
+            'back_scan' => $this->prepareBackScanGradesForPublicCardPage($userCard),
+            'generated_images' => $this->prepareGeneratedImagesForPublicCardPage($userCard),
+        ];
+    }
+
+    protected function prepareGradeForPublicCardPage(UserCard $userCard): array
+    {
+        return [
+            'grade' => $this->preparePreciseValue($userCard->overall_grade),
+            'nickname' => $userCard->overall_grade_nickname,
+        ];
+    }
+
+    protected function prepareOverallGradesForPublicCardPage(UserCard $userCard): array
+    {
+        return [
+            'centering' => $this->preparePreciseValue($userCard->overall_values['center']),
+            'surface' => $this->preparePreciseValue($userCard->overall_values['surface']),
+            'edges' => $this->preparePreciseValue($userCard->overall_values['edge']),
+            'corners' => $this->preparePreciseValue($userCard->overall_values['corner']),
+        ];
+    }
+
+    /**
+     * It returns precise value for display.
+     * e.g. 8.00 will be converted to 8, 8.50 will be converted to 8.5, 8.125 will be converted to 8.1
+     *
+     * @param  string  $value
+     * @return float
+     */
+    protected function preparePreciseValue(string $value): float
+    {
+        $gradeValue = (float) $value;
+
+        if (floor($gradeValue) === $gradeValue) {
+            return floor($gradeValue);
+        } else {
+            return round($gradeValue, 1);
+        }
+    }
+
+    protected function prepareFrontScanGradesForPublicCardPage(UserCard $userCard): array
+    {
+        return [
+            'centering' =>  number_format((float) $userCard->human_grade_values['front']['center'], 2),
+            'surface' => number_format((float) $userCard->human_grade_values['front']['surface'], 2),
+            'edges' => number_format((float) $userCard->human_grade_values['front']['edge'], 2),
+            'corners' => number_format((float) $userCard->human_grade_values['front']['corner'], 2),
+        ];
+    }
+
+    protected function prepareBackScanGradesForPublicCardPage(UserCard $userCard): array
+    {
+        return [
+            'centering' => number_format((float) $userCard->human_grade_values['back']['center'], 2),
+            'surface' => number_format((float) $userCard->human_grade_values['back']['surface'], 2),
+            'edges' => number_format((float) $userCard->human_grade_values['back']['edge'], 2),
+            'corners' => number_format((float) $userCard->human_grade_values['back']['corner'], 2),
+        ];
+    }
+
+    protected function prepareGeneratedImagesForPublicCardPage(UserCard $userCard): array
+    {
+        return array_filter($userCard->generated_images, function (array $imageData) {
+            return $imageData['output_image'] !== null;
+        });
     }
 }

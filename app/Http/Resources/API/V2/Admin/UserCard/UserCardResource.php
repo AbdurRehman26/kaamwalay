02d30556c424bde309d9d173141917695dc05860
Muhammad Nauman <naumancs@gmail.com>
2022-04-30 00:38:25 +0500

    added resources

---
+++ app/Http/Resources/API/V2/Admin/UserCard/UserCardResource.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Resources\API\V2\Admin\UserCard;
+
+use App\Http\Resources\API\V1\Admin\Order\UserCardResource as V1UserCardResource;
+
+class UserCardResource extends V1UserCardResource
+{
+    //
+}

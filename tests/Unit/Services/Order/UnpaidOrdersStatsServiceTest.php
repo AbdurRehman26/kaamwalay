37bbd2a0f50e0de16735611d760a83547d82bdce
numair97 <numairsiddiquie98@gmail.com>
2022-05-11 11:57:56 +0500

    fix failing tests

--- tests/Unit/Services/Order/UnpaidOrdersStatsServiceTest.php
+++ tests/Unit/Services/Order/UnpaidOrdersStatsServiceTest.php
@@ -45,7 +45,7 @@ it('calculates daily unpaid orders stats', function () {
 
     $unpaidDailyStats = $this->unpaidOrdersStatsService->calculateDailyStats($getRandomOrder->created_at->toDateString());
 
-    expect($orders)->toBe($unpaidDailyStats['unpaidTotal']);
+    expect($orders)->toBe($unpaidDailyStats['unpaid_total']);
 })->group('unpaid-orders-stats');
 
 it('calculates monthly unpaid orders stats for the current month', function () {
@@ -57,5 +57,5 @@ it('calculates monthly unpaid orders stats for the current month', function () {
 
     $unpaidMonthlyStats = $this->unpaidOrdersStatsService->calculateMonthlyStats($getRandomOrder->created_at->toDateString());
 
-    expect($orders)->toBe($unpaidMonthlyStats['unpaidTotal']);
+    expect($orders)->toBe($unpaidMonthlyStats['unpaid_total']);
 })->group('unpaid-orders-stats');

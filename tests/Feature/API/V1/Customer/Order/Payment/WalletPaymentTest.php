5ee6aa3552480312769ed4f3cf692856a05f61b3
Kazmi <sydabdrehman@gmail.com>
2022-05-16 15:45:31 +0500

    reverted code

--- tests/Feature/API/V1/Customer/Order/Payment/WalletPaymentTest.php
+++ tests/Feature/API/V1/Customer/Order/Payment/WalletPaymentTest.php
@@ -55,7 +55,6 @@ test('user can be charged successfully from wallet', function () {
         'payment_method_id' => $this->paymentMethod->id,
     ]);
     postJson("/api/v1/customer/orders/{$this->order->id}/payments")
-        ->dump()
         ->assertOk();
 
     expect($this->user->wallet->balance)->toBe((float) 1);

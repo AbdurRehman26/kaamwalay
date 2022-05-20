db00aceade0c26d59883b9d9ff84b625a3a58942
numair97 <numairsiddiquie98@gmail.com>
2022-05-20 16:37:36 +0500

    code improvements

--- resources/ts/dashboard/components/Counter.tsx
+++ resources/ts/dashboard/components/Counter.tsx
@@ -1,31 +1,31 @@
 import dayjs from 'dayjs';
 
-function getRemainingSeconds(nowDayjs: any, timestampDayjs: any) {
-    const seconds = timestampDayjs.diff(nowDayjs, 'seconds') % 60;
-    return padWithZeros(seconds, 2);
-}
-
-function getRemainingMinutes(nowDayjs: any, timestampDayjs: any) {
-    const minutes = timestampDayjs.diff(nowDayjs, 'minutes') % 60;
-    return padWithZeros(minutes, 2);
-}
-
-function getRemainingHours(nowDayjs: any, timestampDayjs: any) {
-    const hours = timestampDayjs.diff(nowDayjs, 'hours') % 24;
-    return padWithZeros(hours, 2);
-}
-
-function padWithZeros(number: any, minLength: any) {
-    const numberString = number.toString();
-    if (numberString.length >= minLength) {
-        return numberString;
-    }
-    return '0'.repeat(minLength - numberString.length) + numberString;
-}
-
 export function getRemainingTimeUntilMsTimestamp(timestampMs: any) {
     const timestampDayjs = dayjs(timestampMs);
     const nowDayjs = dayjs();
+
+    function getRemainingSeconds(nowDayjs: any, timestampDayjs: any) {
+        const seconds = timestampDayjs.diff(nowDayjs, 'seconds') % 60;
+        return padWithZeros(seconds, 2);
+    }
+
+    function getRemainingMinutes(nowDayjs: any, timestampDayjs: any) {
+        const minutes = timestampDayjs.diff(nowDayjs, 'minutes') % 60;
+        return padWithZeros(minutes, 2);
+    }
+
+    function getRemainingHours(nowDayjs: any, timestampDayjs: any) {
+        const hours = timestampDayjs.diff(nowDayjs, 'hours') % 24;
+        return padWithZeros(hours, 2);
+    }
+
+    function padWithZeros(number: any, minLength: any) {
+        const numberString = number.toString();
+        if (numberString.length >= minLength) {
+            return numberString;
+        }
+        return '0'.repeat(minLength - numberString.length) + numberString;
+    }
 
     if (timestampDayjs.isBefore(nowDayjs)) {
         return {

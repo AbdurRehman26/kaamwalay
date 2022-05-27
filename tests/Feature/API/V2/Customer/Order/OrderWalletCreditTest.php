3f5a89d5ecd6e91069c1f54578c3736270e23bc5
Kazmi <sydabdrehman@gmail.com>
2022-05-27 16:58:14 +0500

    event assert added

--- tests/Feature/API/V2/Customer/Order/OrderWalletCreditTest.php
+++ tests/Feature/API/V2/Customer/Order/OrderWalletCreditTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\API\Customer\Order\OrderPaid;
 use App\Models\Order;
 use App\Models\OrderStatus;
 use App\Models\PaymentMethod;
@@ -58,6 +59,7 @@ test('credit added to user`s wallet if he pays within 24 hrs', function () {
         ->assertJsonStructure(['data' => ['id', 'charges']])
         ->assertJsonPath('data.amount', $this->order->refresh()->grand_total_cents);
 
+    Event::assertDispatched(OrderPaid::class);
     expect(round(Wallet::find($this->user->wallet->id)->balance, 2))->toBe(round($walletAmount + ($this->order->grand_total * 5 / 100), 2));
     expect(round($this->user->wallet->lastTransaction->amount, 2))->toBe(round(($this->order->grand_total * 5 / 100), 2));
 })->group('order.wallet.credit');

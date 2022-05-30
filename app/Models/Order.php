0b42ba1563dbcc17f16eaddcc61094770bc4442e
Kazmi <sydabdrehman@gmail.com>
2022-05-30 18:10:53 +0500

    condition reversed

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -495,8 +495,8 @@ class Order extends Model implements Exportable
         return $query->whereBetween('created_at', [$monthStart, $monthEnd]);
     }
 
-    public function isNotOlderThanOneDay(): bool
+    public function isOlderThanOneDay(): bool
     {
-        return now()->diff($this->created_at)->days === 0;
+        return now()->diff($this->created_at)->days > 0;
     }
 }

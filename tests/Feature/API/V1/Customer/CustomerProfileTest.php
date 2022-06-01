535830f0e79f056155ffb4b20d344ff3a59dca6a
Nabeel Ahmed <thisisnabeelahmed@gmail.com>
2022-06-02 00:35:52 +0500

    Improved tests

--- tests/Feature/API/V1/Customer/CustomerProfileTest.php
+++ tests/Feature/API/V1/Customer/CustomerProfileTest.php
@@ -5,6 +5,8 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Bus;
 
+use function Pest\Laravel\putJson;
+
 uses(WithFaker::class);
 
 beforeEach(function () {
@@ -22,21 +24,17 @@ test('a customer can update his profile', function () {
         // Faking AGS update user API
         'ags.api/users/me/' => Http::response([]),
     ]);
-    $this->actingAs($this->user);
     Bus::fake(ProcessImage::class);
+    $this->actingAs($this->user);
 
-    $response = $this->putJson('/api/v1/customer/profile', [
-            'first_name' => 'first',
-            'last_name' => 'Last',
-            'email_subscription' => true,
-            'phone' => '1234567890',
-            'username' => 'test24',
-            'profile_image' => 'https://via.placeholder.com/150',
-    ]);
-
-    $response->assertSuccessful();
-    $response->assertStatus(200);
-    $response->assertJsonStructure([
+    putJson('/api/v1/customer/profile', [
+        'first_name' => 'first',
+        'last_name' => 'Last',
+        'email_subscription' => true,
+        'phone' => '1234567890',
+        'username' => 'test24',
+        'profile_image' => 'https://via.placeholder.com/150',
+    ])->assertSuccessful()->assertStatus(200)->assertJsonStructure([
         'data' => [
             'id',
             'first_name',
@@ -56,31 +54,25 @@ test('profile update does not dispatch Process Image job if no profile image', f
         // Faking AGS update user API
         'ags.api/users/me/' => Http::response([]),
     ]);
-    $this->actingAs($this->user);
     Bus::fake(ProcessImage::class);
+    $this->actingAs($this->user);
 
-    $response = $this->putJson('/api/v2/customer/profile', [
+    putJson('/api/v2/customer/profile', [
         'first_name' => 'first',
-    ]);
-
-    $response->assertSuccessful();
+    ])->assertSuccessful();
     Bus::assertNotDispatched(ProcessImage::class);
 });
 
 test('customer profile update required fields error', function () {
     $this->actingAs($this->user);
 
-    $this->actingAs($this->user);
-
-    $response = $this->putJson('/api/v1/customer/profile', [
+    putJson('/api/v1/customer/profile', [
         'first_name' => '',
         'last_name' => '',
         'phone' => '',
         'username' => '',
         'profile_image' => '',
-    ]);
-
-    $response->assertInvalid([
+    ])->assertInvalid([
         'first_name',
         'last_name',
         'username',
@@ -90,9 +82,7 @@ test('customer profile update required fields error', function () {
 test('a customer`s username can not be duplicate', function () {
     $this->actingAs($this->user);
 
-    $response = $this->putJson('/api/v1/customer/profile', [
+    putJson('/api/v1/customer/profile', [
         'username' => $this->anotherUser->username,
-    ]);
-
-    $response->assertUnprocessable();
+    ])->assertUnprocessable();
 });

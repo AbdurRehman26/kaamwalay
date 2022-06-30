4f357a6f9ab735ad684a39058af676b1100de2c4
Muhammad Nauman <naumancs@gmail.com>
2022-07-01 02:37:23 +0500

    review changes

--- config/configuration.php
+++ config/configuration.php
@@ -91,11 +91,11 @@ return [
             'auth' => true,
             'value' => env('FEATURE_ORDER_WALLET_CREDIT_PERCENTAGE'),
         ],
-        'cleaning_fee_per_card' => [
-            'value' => env('CLEANING_FEE_PER_CARD'),
+        'feature_order_cleaning_fee_per_card' => [
+            'value' => env('FEATURE_ORDER_CLEANING_FEE_PER_CARD'),
         ],
-        'cleaning_fee_max_cap' => [
-            'value' => env('CLEANING_FEE_MAX_CAP'),
+        'feature_order_cleaning_fee_max_cap' => [
+            'value' => env('FEATURE_ORDER_CLEANING_FEE_MAX_CAP'),
         ],
     ],
 ];

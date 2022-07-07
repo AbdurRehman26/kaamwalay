b7d15c097a69889d9a05a5915e110edede9916d3
Kazmi <sydabdrehman@gmail.com>
2022-07-08 02:19:58 +0500

    pr fix for reportable exception

--- app/Exceptions/Services/Admin/ServiceNotReportableException.php
+++ app/Exceptions/Services/Admin/ServiceNotReportableException.php
@@ -10,17 +10,8 @@ use Symfony\Component\HttpFoundation\Response;
 class ServiceNotReportableException extends Exception
 {
     /** @var string */
-    protected $message = 'Service is not exportable. Implement Reportable interface on service.';
+    protected $message = 'Service is not reportable. Implement Reportable interface on service.';
 
     /** @var int */
     protected $code = Response::HTTP_UNPROCESSABLE_ENTITY;
-
-    /**
-     * @param Request|array $request
-     * @return JsonResponse
-     */
-    public function render($request): JsonResponse
-    {
-        return new JsonResponse(['error' => $this->message], $this->code);
-    }
 }

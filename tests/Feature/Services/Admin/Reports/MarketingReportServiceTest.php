7b485e60c53f1f8f1a180d2e3c5c9003de370509
Kazmi <sydabdrehman@gmail.com>
2022-07-11 01:31:16 +0500

    reverted changes in test

--- tests/Feature/Services/Admin/Reports/MarketingReportServiceTest.php
+++ tests/Feature/Services/Admin/Reports/MarketingReportServiceTest.php
@@ -97,18 +97,18 @@ it('validates reports data for weekly, monthly and quarterly', function ($report
         'Number of customers who order 25-50 cards' => $queryCardsCalculation25To50->having(DB::raw('COUNT(order_items.id)'), '>=', 25)->having(DB::raw('COUNT(order_items.id)'), '<', 50)->count(),
         'Number of customers who order 50 - 100 cards' => $queryCardsCalculation50To100->having(DB::raw('COUNT(order_items.id)'), '>=', 50)->having(DB::raw('COUNT(order_items.id)'), '<', 100)->count(),
         'Number of customers that order 100+ cards' => $queryCardsCalculation100->having(DB::raw('COUNT(order_items.id)'), '>=', 100)->count(),
-        'Average number of days taken from confirmation to grading' => (int) $queryCardsConfirmationToGrading->select(DB::raw("AVG(orders.graded_at) - AVG(order_status_histories.created_at) as avg"))
+        'Average number of days taken from confirmation to grading' => (int) $queryCardsConfirmationToGrading->select(DB::raw("AVG(DATEDIFF(orders.graded_at, order_status_histories.created_at)) as avg"))
         ->where('orders.order_status_id', '>=', OrderStatus::GRADED)
         ->whereBetween("orders.graded_at", [$fromDate, $toDate])
         ->first()->avg . ' Day(s)',
 
-        'Average number of days taken from confirmation to shipping' => (int) $queryCardsConfirmationToGrading->select(DB::raw("AVG(orders.shipped_at) - AVG(order_status_histories.created_at) as avg"))
+        'Average number of days taken from confirmation to shipping' => (int) $queryCardsConfirmationToGrading->select(DB::raw("AVG(DATEDIFF(orders.shipped_at, order_status_histories.created_at)) as avg"))
             ->where('orders.order_status_id', '>=', OrderStatus::SHIPPED)
             ->whereBetween("orders.shipped_at", [$fromDate, $toDate])
             ->first()->avg . ' Day(s)',
 
-        'Average number of days taken from grading to shipping' => (int) Order::select(DB::raw("AVG(shipped_at) - AVG(graded_at) as avg"))->betweenDates($fromDate, $toDate)->first()->avg . ' Day(s)',
-        'Average time from submission to payment' => (int) Order::select(DB::raw("AVG(paid_at) - AVG(created_at) as avg"))->betweenDates($fromDate, $toDate)->first()->avg . ' Day(s)',
+        'Average number of days taken from grading to shipping' => (int) Order::select(DB::raw("AVG(DATEDIFF(shipped_at, graded_at)) as avg"))->betweenDates($fromDate, $toDate)->first()->avg . ' Day(s)',
+        'Average time from submission to payment' => (int) Order::select(DB::raw("AVG(DATEDIFF(paid_at, created_at)) as avg"))->betweenDates($fromDate, $toDate)->first()->avg . ' Day(s)',
     ];
 
     $reportData = $report->getDataForReport($fromDate, $toDate);

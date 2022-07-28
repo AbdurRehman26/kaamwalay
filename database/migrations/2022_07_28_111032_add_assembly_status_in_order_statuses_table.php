b9a447dd39a975e4e8f2eb65a624c0c1a99ae58c
Muhammad Nauman <naumancs@gmail.com>
2022-07-28 16:15:06 +0500

    Added new status

---
+++ database/migrations/2022_07_28_111032_add_assembly_status_in_order_statuses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_statuses', function (Blueprint $table) {
+            DB::table('order_statuses')->insert([
+                [
+                    'code' => 'assembly',
+                    'name' => 'Assembly',
+                    'description' => 'Order is graded and in assembly now.',
+                    'order_state_id' => 2,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ],
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_statuses', function (Blueprint $table) {
+            DB::table('order_statuses')->where('code', 'assembly')->delete();
+        });
+    }
+};

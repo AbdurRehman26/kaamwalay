a0a44553552a7ee7ac6dfb90aa34e6b354ffc614
numair97 <numairsiddiquie98@gmail.com>
2022-08-16 05:38:06 +0500

    done graph design for mobile view

--- resources/views/landings/feed/publiCardPage/population-graph.blade.php
+++ resources/views/landings/feed/publiCardPage/population-graph.blade.php
@@ -4,6 +4,8 @@
             <p class="feed-view__graph__title">AGS Population</p>
             <p class="feed-view__graph__subTitle">{{ $popData['totalPop'] }} Total </p>
         </div>
-        <div class="feed-view__graph__content"></div>
+        <div class="feed-view__graph__container">
+            <div class="feed-view__graph__content" style="min-width: 1200px;"></div>
+        </div>
     </div>
 </section>

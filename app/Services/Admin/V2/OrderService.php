83b0c5e1444fb0c16bf01db599f861e66bc53132
Muhammad Nauman <naumancs@gmail.com>
2022-08-25 20:10:14 +0500

    fix order as paid throwing error randomly

--- app/Services/Admin/V2/OrderService.php
+++ app/Services/Admin/V2/OrderService.php
@@ -166,4 +166,19 @@ class OrderService extends V1OrderService
         $orderStatusHistoryService = resolve(OrderStatusHistoryService::class);
         $orderStatusHistoryService->addStatusToOrder(OrderStatus::CANCELLED, $order, $user, 'Order cancelled by admin');
     }
+
+    public function createManualPayment(Order $order, User $user): Order
+    {
+        $manualPaymentMethodId = PaymentMethod::whereCode('manual')->value('id');
+
+        $order->payment_method_id = $manualPaymentMethodId;
+        $order->save();
+
+        $order->orderPayments()->create([
+            'payment_method_id' => $manualPaymentMethodId,
+            'user_id' => $user->id,
+        ]);
+
+        return $order;
+    }
 }

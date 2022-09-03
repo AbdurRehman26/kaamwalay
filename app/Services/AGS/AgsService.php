cd990239626a63c1d8239ff046ed02d26239175d
numair97 <numair97@users.noreply.github.com>
2022-09-03 23:19:11 +0000

    Fix styling

--- app/Services/AGS/AgsService.php
+++ app/Services/AGS/AgsService.php
@@ -100,14 +100,15 @@ class AgsService
             return [];
         }
         if (
-            !empty($data['results'][0]['front_slab_image']) &&
-            !empty($data['results'][0]['back_slab_image'])
+            ! empty($data['results'][0]['front_slab_image']) &&
+            ! empty($data['results'][0]['back_slab_image'])
         ) {
             return  [
                     'front_slab_image' => $data['results'][0]['front_slab_image'],
                     'back_slab_image' => $data['results'][0]['back_slab_image'],
             ];
         }
+
         return [
             'image_path' => $data['results'][0]['card']['image_path'] ?? null,
         ];

24154524ea32108426f42c3f4db9898b51919d13
Muhammad Nauman <naumancs@gmail.com>
2022-09-07 20:24:36 +0500

    fix static analysis

--- app/Services/Order/Validators/CouponAppliedValidator.php
+++ app/Services/Order/Validators/CouponAppliedValidator.php
@@ -10,12 +10,14 @@ class CouponAppliedValidator
     public static function validate(array $data): void
     {
         if (! empty($data['coupon']['code'])) {
+            /** @var array<int, array> $items */
+            $items = $data['items'];
             throw_unless(
                 CouponService::returnCouponIfValid(
                     $data['coupon']['code'],
                     [
                         'couponables_id' => $data['coupon']['couponables_id'] ?? $data['payment_plan']['id'],
-                        'items_count' => collect($data['items'])->sum('quantity'),
+                        'items_count' => collect($items)->sum('quantity'),
                     ]
                 ),
                 CouponExpiredOrInvalid::class

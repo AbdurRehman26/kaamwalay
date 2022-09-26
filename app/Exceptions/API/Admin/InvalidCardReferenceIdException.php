4227fbba6415f03adebe7b79c52ac7605a8fb96e
Muhammad Nauman <naumancs@gmail.com>
2022-09-26 17:59:21 +0500

    fixed order listing and card exception when card reference ID is null

---
+++ app/Exceptions/API/Admin/InvalidCardReferenceIdException.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Exceptions\API\Admin;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class InvalidCardReferenceIdException extends Exception
+{
+    /** @var string */
+    protected $message = 'This card does not contain card reference ID from AGS.';
+
+    /** @var int */
+    protected $code = Response::HTTP_UNPROCESSABLE_ENTITY;
+
+    /**
+     * @param Request|array $request
+     * @return JsonResponse
+     */
+    public function render($request): JsonResponse
+    {
+        return new JsonResponse([ 'error' => $this->message ], $this->code);
+    }
+}

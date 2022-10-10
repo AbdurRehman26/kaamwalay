9eeedf7359e6a83024b55044550627799bbca164
Kazmi <sydabdrehman@gmail.com>
2022-10-11 00:14:03 +0200

    PR changes fixed

--- app/Http/Controllers/API/V2/Admin/Cards/CardRarityController.php
+++ app/Http/Controllers/API/V2/Admin/Cards/CardRarityController.php
@@ -27,7 +27,7 @@ class CardRarityController extends Controller
         return new CardRarityResource(CardRarity::create($request->validated()));
     }
 
-    public function update(UpdateCardRarityRequest $request, int $cardRarityId): JsonResponse
+    public function update(int $cardRarityId, UpdateCardRarityRequest $request): JsonResponse
     {
         CardRarity::where('id', $cardRarityId)->update([
             'name' => $request->name,

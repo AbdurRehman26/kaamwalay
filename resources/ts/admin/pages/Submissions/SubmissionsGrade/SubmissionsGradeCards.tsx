8577e2310d87138b1592b9277f4598661f8bb496
Muhammad Nauman <naumancs@gmail.com>
2022-10-30 18:37:53 +0500

    added a condition to not call AGS if not needed

--- resources/ts/admin/pages/Submissions/SubmissionsGrade/SubmissionsGradeCards.tsx
+++ resources/ts/admin/pages/Submissions/SubmissionsGrade/SubmissionsGradeCards.tsx
@@ -87,7 +87,7 @@ export function SubmissionsGradeCards() {
                         value: declaredValue,
                     }),
                 );
-                await loadGrades();
+                await loadGrades(false);
             }
         },
         [dispatch, loadGrades, id],

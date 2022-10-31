85c5997bbcbef64f64eab0209b5f7d619225237e
numair97 <numairsiddiquie98@gmail.com>
2022-10-31 23:01:34 +0500

    created command to close old deals

---
+++ app/Console/Commands/Hubspot/CloseOldDeals.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands\Hubspot;
+
+use App\Enums\Order\OrderPaymentStatusEnum;
+use App\Services\HubspotService;
+use Illuminate\Console\Command;
+use App\Models\Order;
+
+class CloseOldDeals extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'hubspot:close-paid-order-deals';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command will close deals in which customer has atleast one paid order';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(HubspotService $hubspotService)
+    {
+        $orders = Order::where('payment_status', OrderPaymentStatusEnum::PAID)->get();
+        foreach($orders as $order)
+        {
+            $hubspotService->updateDealStageForPaidOrder($order);
+        }
+        return Command::SUCCESS;
+    }
+}

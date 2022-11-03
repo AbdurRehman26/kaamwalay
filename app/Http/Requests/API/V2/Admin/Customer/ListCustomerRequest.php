4083bb42e5de41f6be231244cd6b515fa4437944
Muhammad Nauman <naumancs@gmail.com>
2022-11-03 16:28:17 +0500

    resource changes

---
+++ app/Http/Requests/API/V2/Admin/Customer/ListCustomerRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Requests\API\V2\Admin\Customer;
+
+use App\Http\Requests\API\V1\Admin\Customer\ListCustomerRequest as V1ListCustomerRequest;
+
+class ListCustomerRequest extends V1ListCustomerRequest
+{
+
+}

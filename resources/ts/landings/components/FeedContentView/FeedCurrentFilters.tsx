7af5b46e5e5ab373ff495782c793f6f0cf59222e
numair97 <numairsiddiquie98@gmail.com>
2022-11-09 18:07:23 +0500

    fixed css issue

--- resources/ts/landings/components/FeedContentView/FeedCurrentFilters.tsx
+++ resources/ts/landings/components/FeedContentView/FeedCurrentFilters.tsx
@@ -16,13 +16,6 @@ const CurrentRefinementBox = styled(Box)({
         display: 'inline-flex',
         flexWrap: 'wrap',
     },
-    '.FilterList': {
-        display: 'inline-flex',
-        flexWrap: 'wrap',
-        [theme.breakpoints.down('sm')]: {
-            flexWrap: 'nowrap',
-        },
-    },
     '.Chip': {
         width: '100%',
         height: '40px',
@@ -64,7 +57,7 @@ const CustomCurrentRefinements = connectCurrentRefinements(({ items, refine }) =
                 {items.map((item) => (
                     <li key={item.label}>
                         {item.items ? (
-                            <ul className={'FilterList'}>
+                            <ul className={'CurrentFilterList'}>
                                 {item.items.map((nested) => (
                                     <li key={nested.label} className={'List'}>
                                         <Chip

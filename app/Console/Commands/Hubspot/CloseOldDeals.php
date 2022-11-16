2931acc4e1ef241b5e8761e8aad1a4b02cdb2fac
numair97 <numairsiddiquie98@gmail.com>
2022-11-17 02:16:19 +0500

    updated condition

--- app/Console/Commands/Hubspot/CloseOldDeals.php
+++ app/Console/Commands/Hubspot/CloseOldDeals.php
@@ -8,6 +8,7 @@ use App\Models\Order;
 use App\Models\User;
 use App\Services\HubspotService;
 use Illuminate\Console\Command;
+use SevenShores\Hubspot\Resources\Deals;
 
 class CloseOldDeals extends Command
 {
@@ -32,12 +33,19 @@ class CloseOldDeals extends Command
      */
     public function handle(HubspotService $hubspotService)
     {
-        $deals = HubspotDeal::get();
+        $deals = (new Deals($hubspotService->getClient()))->all(['properties' => 'dealstage'])->getData()->deals;
+
         foreach ($deals as $deal) {
-            $user = User::where('email', $deal->user_email)->first();
-            $order = Order::where('user_id', $user->id)->where('payment_status', OrderPaymentStatusEnum::PAID)->first();
-            if ($order) {
-                $hubspotService->updateDealStageForPaidOrder($order);
+            if ($deal->properties->dealstage->value === config('services.hubspot.pipline_stage_id_new_customer')) {
+                $hubspotDeal = HubspotDeal::where('deal_id', $deal->dealId)->first();
+                if ($hubspotDeal) {
+                    $user = User::where('email', $hubspotDeal->user_email)->first();
+                    $order = Order::where('user_id', $user->id)->where('payment_status', OrderPaymentStatusEnum::PAID)->first();
+                    if ($order) {
+                        $this->info("Moving $user->first_name deal from " . config('services.hubspot.pipline_stage_id_new_customer') . " to " . config('services.hubspot.pipline_stage_id_closed'));
+                        $hubspotService->updateDealStageForPaidOrder($order);
+                    }
+                }
             }
         }
 

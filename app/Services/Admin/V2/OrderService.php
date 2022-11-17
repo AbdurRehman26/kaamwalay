13035704d82a0ae08d3613f6f4139422fc7ba2ea
Muhammad Nauman <9109152+muhammad-nauman@users.noreply.github.com>
2022-11-17 18:03:03 +0500

    Added pagination handling

--- app/Services/Admin/V2/OrderService.php
+++ app/Services/Admin/V2/OrderService.php
@@ -228,11 +228,18 @@ class OrderService extends V1OrderService
      */
     public function getGrades(Order $order): Collection
     {
-        $grades = $this->agsService->getGrades($this->getOrderCertificates($order));
+        $itemsCount = $order->orderItems()->count();
+
+        $agsPageCount = (int) (ceil($itemsCount / 10) * 10) / 10;
+
+        $itemsPerPage = 10;
 
         $cards = $this->getCardsForGrading($order);
 
-        $this->updateLocalGrades($grades['results'] ?? [], $cards);
+        for ($iterator = 0; $iterator < $agsPageCount; $iterator++) {
+            $grades = $this->agsService->getGrades($this->getOrderCertificates($order), $iterator * $itemsPerPage);
+            $this->updateLocalGrades($grades['results'] ?? [], $cards);
+        }
 
         return $cards;
     }

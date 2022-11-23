b7b3834ee3262d595c04f2620ffb1887dede6724
numair97 <numairsiddiquie98@gmail.com>
2022-11-24 01:32:46 +0500

    changes in naming convention and few improvements

---
+++ app/Console/Commands/Salesman/LinkCustomersToSalesman.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands\Salesman;
+
+use Illuminate\Console\Command;
+use App\Imports\CustomersImport;
+use App\Models\Salesman;
+use App\Models\User;
+use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Arr;
+
+class LinkCustomersToSalesman extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'salesman:link-salesman-with-users';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command will link given customers emails sheet with the give salesman email';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $fileName = $this->ask('Filename (ExcelFile.xlsx)');
+        $salesmanEmail = $this->ask('SalesMan Email');
+        if ($fileName && $salesmanEmail) {
+            $emails = Arr::flatten(Excel::toArray(new CustomersImport, $fileName, 's3', \Maatwebsite\Excel\Excel::XLSX)[0]);
+            $user = User::where('email', $salesmanEmail)->first();
+            $salesman = Salesman::where('user_id', $user->id)->first();
+            User::whereIn('email', $emails)->update(
+                [
+                    'salesman_id' => $salesman->id,
+                ],
+            );
+            $this->info('Linked Customers With Salesman Successfully');
+        }
+        return Command::SUCCESS;
+    }
+}

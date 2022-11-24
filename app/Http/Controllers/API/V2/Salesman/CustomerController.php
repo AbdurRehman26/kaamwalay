c179b55ea9fa6c4e5fadd2cbe563b35b7f114c28
Kazmi <sydabdrehman@gmail.com>
2022-11-24 23:27:33 +0100

    fixed salesman customer directory

--- app/Http/Controllers/API/V2/Salesman/CustomerController.php
+++ app/Http/Controllers/API/V2/Salesman/CustomerController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers\API\V2\Salesman;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\API\V2\Admin\Customer\ListCustomerRequest;
-use App\Http\Requests\API\V2\Admin\Customer\StoreCustomerRequest;
-use App\Http\Resources\API\V2\Admin\Customer\CustomerCollection;
-use App\Http\Resources\API\V2\Admin\Customer\CustomerResource;
+use App\Http\Requests\API\V2\Salesman\Customer\ListCustomerRequest;
+use App\Http\Requests\API\V2\Salesman\Customer\StoreCustomerRequest;
+use App\Http\Resources\API\V2\Salesman\Customer\CustomerCollection;
+use App\Http\Resources\API\V2\Salesman\Customer\CustomerResource;
+use App\Models\User;
 use App\Services\Salesman\V2\CustomerService;
 
 class CustomerController extends Controller
@@ -18,6 +19,13 @@ class CustomerController extends Controller
     public function index(ListCustomerRequest $request): CustomerCollection
     {
         return new CustomerCollection($this->customerService->getCustomers());
+    }
+
+    public function show(User $customer): CustomerResource
+    {
+        $customer->load('salesman');
+
+        return new CustomerResource($customer);
     }
 
     public function store(StoreCustomerRequest $request): CustomerResource

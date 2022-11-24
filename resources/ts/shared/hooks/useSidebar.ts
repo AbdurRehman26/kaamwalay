8c115a3aa067e17b6766a139609c881f7ec8a6ee
Kazmi <sydabdrehman@gmail.com>
2022-11-25 00:49:11 +0100

    fixed js

--- resources/ts/shared/hooks/useSidebar.ts
+++ resources/ts/shared/hooks/useSidebar.ts
@@ -5,7 +5,7 @@ import { drawerVisibility } from '../redux/slices/pageSlice';
 
 export function useSidebar(): [state: boolean, setVisibility: (visibility: boolean) => void] {
     const dispatch = useAppDispatch();
-    const drawerState = useAppSelector((state) => state.page.drawerOpened);
+    const drawerState = useAppSelector((state) => state.pageSlice.drawerOpened);
     const setVisibility = useCallback(
         (state: boolean) => {
             dispatch(drawerVisibility(state));

555d4ae4fdc32d1a273d7ab3365f28d481134545
Kazmi <sydabdrehman@gmail.com>
2022-11-26 18:53:03 +0100

    tests fixed

--- routes/v2/salesman.php
+++ routes/v2/salesman.php
@@ -32,37 +32,43 @@ Route::middleware(['auth', 'role:salesman'])->group(function () {
 
     Route::prefix('orders')->group(function () {
         Route::apiResource('payment-methods', PaymentMethodController::class)->only(['index', 'show'])->names([
-            'index' => 'admin.payment-methods.index',
-            'show' => 'admin.payment-methods.show',
+            'index' => 'salesman.payment-methods.index',
+            'show' => 'salesman.payment-methods.show',
         ]);
         Route::apiResource('payment-plans', PaymentPlanController::class)->only(['index', 'show'])->names([
-            'index' => 'admin.payment-plans.index',
-            'show' => 'admin.payment-plans.show',
+            'index' => 'salesman.payment-plans.index',
+            'show' => 'salesman.payment-plans.show',
         ]);
         Route::apiResource('shipping-methods', ShippingMethodController::class)->only(['index', 'show'])->names([
-            'index' => 'admin.shipping-methods.index',
-            'show' => 'admin.shipping-methods.show',
+            'index' => 'salesman.shipping-methods.index',
+            'show' => 'salesman.shipping-methods.show',
         ]);
         Route::post('shipping-fee', ShippingFeeController::class);
     });
 
-    Route::apiResource('orders', OrderController::class)->only(['index', 'show', 'store', 'destroy']);
+    Route::apiResource('orders', OrderController::class)
+        ->only(['index', 'show', 'store'])
+        ->names([
+            'index' => 'salesman.orders.index',
+            'show' => 'salesman.orders.show',
+            'store' => 'salesman.orders.store',
+        ]);
 
     Route::apiResource('customers', CustomerController::class)->only(['index', 'store', 'show'])
         ->names([
-            'index' => 'customers.index',
-            'store' => 'customers.store',
-            'show' => 'customers.show',
+            'index' => 'salesman.customers.index',
+            'store' => 'salesman.customers.store',
+            'show' => 'salesman.customers.show',
         ]);
 
     Route::get('customer/{user}/addresses', [CustomerAddressController::class, 'getCustomerAddresses']);
 
     Route::prefix('wallets')->group(function () {
         Route::get('{wallet}', [CustomerWalletController::class, 'show'])
-            ->name('customer.wallet.show');
+            ->name('salesman.wallet.show');
         Route::post('{wallet}/credit', [CustomerWalletController::class, 'creditToWallet'])
-            ->name('customer.wallet.credit');
+            ->name('salesman.wallet.credit');
         Route::get('{wallet}/history', [CustomerWalletController::class, 'getTransactionsHistory'])
-            ->name('customer.wallet.history');
+            ->name('salesman.wallet.history');
     });
 });

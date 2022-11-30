636cb9754da5706dd2ebdf4f70f7e5c422530ab3
Kazmi <sydabdrehman@gmail.com>
2022-11-30 16:31:05 +0100

    fixed for quantity depending prices

--- app/Services/Salesman/V2/Order/CreateOrderService.php
+++ app/Services/Salesman/V2/Order/CreateOrderService.php
@@ -7,7 +7,9 @@ use App\Exceptions\API\Admin\OrderStatusHistoryWasAlreadyAssigned;
 use App\Models\Country;
 use App\Models\CustomerAddress;
 use App\Models\Order;
+use App\Models\OrderPaymentPlan;
 use App\Models\OrderStatus;
+use App\Models\PaymentPlan;
 use App\Models\User;
 use App\Services\Order\OrderNumberGeneratorService;
 use App\Services\Order\V2\CreateOrderService as BaseCreateOrderService;
@@ -106,5 +108,31 @@ class CreateOrderService extends BaseCreateOrderService
         $this->order->save();
         $this->order->order_number = OrderNumberGeneratorService::generate($this->order);
         $this->order->save();
+    }
+
+    protected function storePaymentPlan(array $paymentPlan): void
+    {
+        // This payment_plan_id is linked to originalPaymentPlan relationship
+        $this->order->payment_plan_id = $paymentPlan['id'];
+
+        $paymentPlan = PaymentPlan::find($paymentPlan['id']);
+        $priceRanges = $paymentPlan->paymentPlanRanges;
+
+        // @phpstan-ignore-next-line
+        $totalItems = collect($this->data['items'])->sum('quantity');
+
+        $priceRange = $priceRanges->first(function ($item, $key) use ($totalItems) {
+            return ($item->min_cards <= $totalItems && $item->max_cards >= $totalItems) ||
+                ($item->min_cards <= $totalItems && ! $item->max_cards);
+        });
+
+        $orderPaymentPlan = OrderPaymentPlan::create([
+            'price' => $priceRange ? $priceRange->price : $paymentPlan->price,
+            'max_protection_amount' => $paymentPlan->max_protection_amount,
+            'turnaround' => $paymentPlan->turnaround,
+        ]);
+
+        // Column for paymentPlan relationship is order_payment_plan_id
+        $this->order->paymentPlan()->associate($orderPaymentPlan);
     }
 }

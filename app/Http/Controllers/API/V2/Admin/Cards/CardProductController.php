43b505a1395988ade2cfebfc0e67c88333875921
Luis Molina <lm.molinab@gmail.com>
2022-12-16 08:48:28 -0500

    adding extra validation check for duplicity of card in store and update requests

--- app/Http/Controllers/API/V2/Admin/Cards/CardProductController.php
+++ app/Http/Controllers/API/V2/Admin/Cards/CardProductController.php
@@ -4,21 +4,50 @@ namespace App\Http\Controllers\API\V2\Admin\Cards;
 
 use App\Exceptions\API\Admin\CardProductCanNotBeDeleted;
 use App\Exceptions\API\Admin\CardProductCanNotBeUpdated;
-use App\Http\Controllers\API\V1\Admin\Cards\CardProductController as V1CardProductController;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\API\V2\Admin\Card\StoreCardProductRequest;
 use App\Http\Requests\API\V2\Admin\Card\UpdateCardProductRequest;
 use App\Http\Resources\API\V2\CardProduct\CardProductCollection;
 use App\Http\Resources\API\V2\CardProduct\CardProductResource;
+use App\Models\CardCategory;
 use App\Models\CardProduct;
+use App\Services\Admin\Card\CardProductService;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
-class CardProductController extends V1CardProductController
+class CardProductController extends Controller
 {
+    public function __construct(protected CardProductService $cardProductService)
+    {
+    }
+
     public function index(): CardProductCollection
     {
         $cards = $this->cardProductService->getCards();
 
         return new CardProductCollection($cards);
+    }
+
+    public function store(StoreCardProductRequest $request): CardProductResource|JsonResponse
+    {
+        try {
+            $card = $this->cardProductService->create($request->validated());
+        } catch (Exception $e) {
+            return new JsonResponse(
+                [
+                    'error' => $e->getMessage(),
+                ],
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+
+        return new CardProductResource($card);
+    }
+
+    public function getOptionsValues(CardCategory $cardCategory): JsonResponse
+    {
+        return new JsonResponse($this->cardProductService->getOptionsValues($cardCategory));
     }
 
     public function show(CardProduct $cardProduct): CardProductResource

d2a518d148c2ebceab9029069dbcf57835e2f270
Kazmi <sydabdrehman@gmail.com>
2022-12-19 00:22:46 +0100

    verify coupon exception handled

--- app/Http/Controllers/API/V2/Customer/CouponController.php
+++ app/Http/Controllers/API/V2/Customer/CouponController.php
@@ -22,7 +22,16 @@ class CouponController extends Controller
 
     public function show(string $couponCode, ShowCouponRequest $request): JsonResponse|CouponResource
     {
-        $coupon = $this->couponService->returnCouponIfValid($couponCode, $request->only('couponables_id', 'items_count'));
+        try {
+            $coupon = $this->couponService->returnCouponIfValid($couponCode, $request->only('couponables_id', 'items_count'));
+        } catch (Exception $e) {
+            return new JsonResponse(
+                    [
+                        'error' => $e->getMessage(),
+                    ],
+                    $e->getCode()
+                );
+        }
 
         return new CouponResource($coupon);
     }

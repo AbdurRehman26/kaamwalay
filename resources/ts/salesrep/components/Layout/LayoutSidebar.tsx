2d1678aaf0f81485778a8c4b1192ee21919f5591
Ali Raza <ali@wooter.com>
2022-12-20 21:17:57 +0500

    fix styling

--- resources/ts/salesrep/components/Layout/LayoutSidebar.tsx
+++ resources/ts/salesrep/components/Layout/LayoutSidebar.tsx
@@ -56,8 +56,8 @@ function LayoutSidebar() {
             classes={{ paper: classes.drawerPaper }}
         >
             <List>
-                <LayoutSidebarItem icon={DashboardIcon} title={'Dashboard'} href={'/dashboard'} exact />
-                <LayoutSidebarItem icon={AllInboxIcon} title={'Submissions'} href={'/submissions'} exact />
+                <LayoutSidebarItem icon={DashboardIcon} title={'Dashboard'} href={'/dashboard'} />
+                <LayoutSidebarItem icon={AllInboxIcon} title={'Submissions'} href={'/submissions'} />
                 <LayoutSidebarItem icon={SellIcon} title={'Promo Codes'} href={'/promo-codes'} />
                 <LayoutSidebarItem icon={AccountCircleOutlinedIcon} title={'Customers'} href={'/customers'} />
             </List>

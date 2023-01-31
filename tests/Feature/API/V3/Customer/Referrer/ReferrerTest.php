90faf945e56e24e9c5d5aa8510458e0bd4cb6dd8
Luis Molina <lm.molinab@gmail.com>
2023-01-31 13:04:21 -0500

    added a test for referral code generation

--- tests/Feature/API/V3/Customer/Referrer/ReferrerTest.php
+++ tests/Feature/API/V3/Customer/Referrer/ReferrerTest.php
@@ -4,16 +4,23 @@ use App\Models\Order;
 use App\Models\Referrer;
 use App\Models\ReferrerEarnedCommission;
 use App\Models\User;
+use Database\Seeders\RolesSeeder;
 use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Events\API\Auth\CustomerRegistered;
+use App\Listeners\API\Referrer\GenerateReferrerOnCustomerRegister;
+use App\Services\Referrer\ReferrerService;
 
 use function Pest\Laravel\getJson;
+uses(WithFaker::class);
 
 beforeEach(function () {
     $this->referrer = Referrer::factory()->create();
-    $this->actingAs($this->referrer->user);
 });
 
 test('a referrer can get his own referral data', function () {
+    $this->actingAs($this->referrer->user);
+
     $this->getJson(route('v3.customer.referral'))
         ->assertSuccessful()
         ->assertJsonStructure(['data' => [
@@ -31,6 +38,8 @@ test('a referrer can get his own referral data', function () {
 });
 
 test('a referrer can get information about their referees', function () {
+    $this->actingAs($this->referrer->user);
+
     User::factory()->count(3)->state(new Sequence(
         ['referred_by' => $this->referrer->user_id],
     ))->create();
@@ -57,6 +66,8 @@ test('a referrer can get information about their referees', function () {
 });
 
 test('a referrer can get information about their commission earnings', function () {
+    $this->actingAs($this->referrer->user);
+
     $users = User::factory()->count(3)->state(new Sequence(
         ['referred_by' => $this->referrer->user_id],
     ))->create();
@@ -91,4 +102,32 @@ test('a referrer can get information about their commission earnings', function 
                 ],
             ],
         ]);
+});
+
+test('a referral code is assigned after user registered', function () {
+    $this->seed(RolesSeeder::class);
+    Bus::fake();
+    Event::fake();
+
+    $email = $this->faker->safeEmail();
+    $requestData = [
+        'first_name' => $this->faker->firstName(),
+        'last_name' => $this->faker->lastName(),
+        'email' => $email,
+        'username' => $this->faker->userName(),
+        'password' => 'passWord1',
+        'password_confirmation' => 'password',
+        'phone' => '',
+    ];
+
+    $this->postJson('/api/v2/auth/register', $requestData);
+
+    Event::assertListening(CustomerRegistered::class, GenerateReferrerOnCustomerRegister::class);
+
+    $user = User::whereEmail($email)->first();
+    $listener = new GenerateReferrerOnCustomerRegister(new ReferrerService());
+    $listener->handle(new CustomerRegistered($user, $requestData));
+
+    $this->assertNotNull($user->referrer);
+    expect($user->referrer->referral_code)->toBeString()->toHaveLength(5);
 });

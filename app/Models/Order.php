7dc1b86cd79bded12fe7f84b82d2dca56a98a418
Ali Raza <ali@wooter.com>
2023-02-07 11:12:48 +0500

    static fix

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -409,8 +409,8 @@ class Order extends Model implements Exportable
     }
 
     /**
-     * @param  Builder<Order>  $query
-     * @param  string|int  $status
+     * @param Builder<Order>  $query
+     * @param int  $status
      * @return Builder<Order>
      */
     public function scopeReferredBy(Builder $query, int $status): Builder

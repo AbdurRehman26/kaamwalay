66b9f9a8eabd61a39e75258ee4cfc4179485e5df
Ali Raza <ali@wooter.com>
2023-02-16 18:43:21 +0500

    removed file

--- app/Http/Requests/API/V3/Admin/Customer/StoreCustomerRequest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Requests\API\V3\Admin\Customer;
-
-use App\Http\Requests\API\V2\Admin\Customer\StoreCustomerRequest as V2CustomerStoreCustomerRequest;
-
-class StoreCustomerRequest extends V2CustomerStoreCustomerRequest
-{
-}

663f9d1e13432dc28a83e145a6da7ce8e56f0159
Kazmi <sydabdrehman@gmail.com>
2023-02-19 21:57:20 +0100

    design fixed for withdrawals

--- resources/ts/dashboard/pages/Referral/TabsContent/Withdrawals/WithdrawFunds.tsx
+++ resources/ts/dashboard/pages/Referral/TabsContent/Withdrawals/WithdrawFunds.tsx
@@ -5,25 +5,88 @@ import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+import theme from '@shared/styles/theme';
 
+const WithDrawDiv = styled(Grid)({
+    background: '#FFFFFF',
+    width: '100%',
+    marginLeft: '20px',
+    [theme.breakpoints.down('sm')]: {
+        marginLeft: '0px',
+        marginBottom: '15px',
+    },
+
+    '.AmountHeading': {
+        marginTop: '40px',
+        fontWeight: '500',
+        fontSize: '20px',
+    },
+    '.AmountValue': {
+        color: theme.palette.primary,
+    },
+    '.Caption': {
+        margin: '3px 0px',
+        width: '50%',
+        fontWeight: 400,
+        fontSize: '14px',
+        lineHeight: '20px',
+        color: 'rgba(0, 0, 0, 0.54)',
+        letterSpacing: '0.15px',
+    },
+    '.AccountInfo': {
+        '&:last-child': {
+            borderRadius: '0px 0px 4px 4px',
+        },
+    },
+    '.AccountInfoHeading': {
+        marginTop: '40px',
+        fontWeight: 500,
+        fontSize: '16px',
+        lineHeight: '24px',
+        letterSpacing: '0.15px',
+    },
+    '.PayoutAccountInfoBox': {
+        margin: '5px 0px',
+    },
+    '.Divider': {
+        margin: '5% 0px 2% 0',
+    },
+    '.WithDrawalBtnDiv': {
+        '.Caption': {
+            fontSize: '12px !important',
+        },
+        '.Button': {
+            marginTop: '20px',
+            padding: '15px',
+            backgroundColor: '#DDDDDD',
+            color: 'rgba(0, 0, 0, 0.24)',
+        },
+    },
+});
 export function WithdrawFunds() {
     return (
-        <Grid>
+        <WithDrawDiv>
             <Breadcrumbs aria-label="breadcrumb" separator={<NavigateNextIcon fontSize="small" />}>
                 <Typography>Referral Program</Typography>
                 <Typography>Withdraw Funds</Typography>
             </Breadcrumbs>
             <Grid>
-                <Typography>Withdrawal Amount: $20.00</Typography>
-                <Typography>
+                <Typography className={'AmountHeading'}>
+                    Withdrawal Amount: <span className={'AmountValue'}>$20.00</span>
+                </Typography>
+                <Typography className={'Caption'}>
                     It can take 5-7 days for the withdrawal to finalize. It will show up as “pending” until it’s
                     completed.
                 </Typography>
             </Grid>
-            <Grid>
-                <Typography>Payout Account (PayPal)</Typography>
-                <Typography>Please enter the PayPal email where you would like to be paid out.</Typography>
+            <Grid className={'AccountInfo'}>
+                <Typography className={'AccountInfoHeading'}>Payout Account (PayPal)</Typography>
+                <Typography className={'Caption'}>
+                    Please enter the PayPal email where you would like to be paid out.
+                </Typography>
                 <TextField
+                    className={'PayoutAccountInfoBox'}
                     placeholder="Enter Paypal Email"
                     fullWidth
                     value={''}
@@ -33,18 +96,18 @@ export function WithdrawFunds() {
                         shrink: true,
                     }}
                 />
-                <Typography>Don’t have one? Create a PayPal Account</Typography>
+                <Typography className={'Caption'}>Don’t have one? Create a PayPal Account</Typography>
             </Grid>
-            <Divider />
-            <Grid>
-                <Typography>
+            <Divider className={'Divider'} />
+            <Grid className={'WithDrawalBtnDiv'}>
+                <Typography className={'Caption'}>
                     Once you start a withdrawal, it can’t be undone. The withdrawal amount will be deducted from your
                     Withdrawable Commission, and you will be able to see the pending transaction in your Withdrawals
                     section. It can take 5-7 days for the funds to transfer to your payout account.
                 </Typography>
-                <Button>START WITHDRAWAL</Button>
+                <Button className={'Button'}>START WITHDRAWAL</Button>
             </Grid>
-        </Grid>
+        </WithDrawDiv>
     );
 }
 

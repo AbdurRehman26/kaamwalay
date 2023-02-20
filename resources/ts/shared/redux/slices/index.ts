5e20daabc3d3d1fc439a7f7e8945d26cd6a14ce1
numair97 <numairsiddiquie98@gmail.com>
2023-02-21 01:36:45 +0500

    fixed diverge issue

--- resources/ts/shared/redux/slices/index.ts
+++ resources/ts/shared/redux/slices/index.ts
@@ -31,7 +31,6 @@ export { adminOrdersListSlice as adminOrdersList } from './adminOrdersListSlice'
 export { adminCustomerReferralSignUpSlice as adminCustomerReferralSignUp } from './adminCustomerReferralSignUpSlice';
 export { adminCustomerReferralCommissionSlice as adminCustomerReferralCommission } from './adminCustomerReferralCommissionSlice';
 export { adminCustomersListSlice as adminCustomersList } from './adminCustomersListSlice';
-export { adminOrdersListSlice as adminOrdersList } from './adminOrdersListSlice';
 
 /*
  *

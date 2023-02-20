dbcd76e88a2e4443bb2e30b197b8a4606fea2b92
numair97 <numairsiddiquie98@gmail.com>
2023-02-21 01:03:53 +0500

    make referral table and empty state component reusable

---
+++ resources/ts/dashboard/pages/Referral/ReferralTable.tsx
@@ -0,0 +1,96 @@
+import Grid from '@mui/material/Grid';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import EnhancedTableHead from '@shared/components/Tables/EnhancedTableHead';
+import { TableSortType } from '@shared/constants/TableSortType';
+import { useAppSelector } from '@dashboard/redux/hooks';
+import { setCommissionEarningsFilter, setCustomerSignUpsFilter } from '@dashboard/redux/slices/referralProgramSlice';
+
+const StyledTableContainer = styled(TableContainer)({
+    margin: '20px 0px',
+    border: '1px solid #E0E0E0',
+    borderRadius: '3px 3px 0px 0px',
+    '.TableHeading': {
+        background: '#F9F9F9',
+        fontWeight: 500,
+        fontSize: '16px',
+        lineHeight: '24px',
+        letterSpacing: '0.15px',
+        color: 'rgba(0, 0, 0, 0.87)',
+        padding: '20px 15px',
+    },
+});
+
+interface props {
+    heading: string;
+    tableHeadings: any;
+    isCustomerSignup?: boolean;
+    tableRows: any;
+    count: number;
+    paginationProps: any;
+}
+
+export function ReferralTable({ heading, tableHeadings, isCustomerSignup, tableRows, count, paginationProps }: props) {
+    const [order, setOrder] = useState<TableSortType>('desc');
+    const [orderBy, setOrderBy] = useState<string>('created_at');
+
+    const sortCustomerSignUpFilter = useAppSelector(
+        (state) => state.referralProgramSlice.customerSignUpsFilter.signUpsfilter,
+    );
+    const sortCommissionEarningsFilter = useAppSelector(
+        (state) => state.referralProgramSlice.commissionEarningsFilter.commissionEarningFilter,
+    );
+    const dispatch = useDispatch();
+
+    const handleRequestSort = (event: React.MouseEvent<unknown>, property: string) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+        if (isCustomerSignup) {
+            dispatch(setCustomerSignUpsFilter(!sortCustomerSignUpFilter));
+        } else {
+            dispatch(setCommissionEarningsFilter(!sortCommissionEarningsFilter));
+        }
+    };
+
+    return (
+        <StyledTableContainer>
+            <Grid sx={{ minWidth: 800 }}>
+                <Typography className={'TableHeading'}>
+                    {heading} ({count})
+                </Typography>
+            </Grid>
+            <Table sx={{ minWidth: 800 }}>
+                <EnhancedTableHead
+                    onRequestSort={handleRequestSort}
+                    order={order}
+                    orderBy={orderBy}
+                    headCells={tableHeadings}
+                    isReferral
+                />
+                <TableBody>{tableRows}</TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TablePagination
+                            sx={{
+                                background: '#F9F9F9',
+                                borderRadius: '0px 0px 3px 3px',
+                            }}
+                            {...paginationProps}
+                        />
+                    </TableRow>
+                </TableFooter>
+            </Table>
+        </StyledTableContainer>
+    );
+}
+
+export default ReferralTable;
--- resources/ts/dashboard/pages/Referral/TabsContent/Referrals/CommissionEarnings.tsx
+++ resources/ts/dashboard/pages/Referral/TabsContent/Referrals/CommissionEarnings.tsx
@@ -1,13 +1,45 @@
 import MonetizationOnTwoToneIcon from '@mui/icons-material/MonetizationOnTwoTone';
+import Avatar from '@mui/material/Avatar';
 import CircularProgress from '@mui/material/CircularProgress';
+import Grid from '@mui/material/Grid';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+import { round } from 'lodash';
+import moment from 'moment';
 import { useEffect } from 'react';
 import EnhancedTableHeadCell from '@shared/components/Tables/EnhancedTableHeadCell';
+import { ReferralCommissionEarningsEntity } from '@shared/entities/ReferralCommissionEarningsEntity';
 import { bracketParams } from '@shared/lib/api/bracketParams';
 import { toApiPropertiesObject } from '@shared/lib/utils/toApiPropertiesObject';
 import { useListReferralCommissionEarningsQuery } from '@shared/redux/hooks/useReferralCommissionEarningsQuery';
 import { useAppSelector } from '@dashboard/redux/hooks';
-import EmptyStates from './EmptyStates';
-import ReferralTable from './ReferralTable';
+import EmptyStates from '../../EmptyStates';
+import ReferralTable from '../../ReferralTable';
+
+const StyledTableCell = styled(TableCell)({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: '#F9F9F9;',
+        fontWeight: 500,
+        fontSize: '10px',
+        lineHeight: '16px',
+        letterSpacing: '0.75px',
+        textTransform: 'uppercase',
+        color: 'rgba(0, 0, 0, 0.54)',
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontWeight: 400,
+        fontSize: '14px',
+        lineHeight: '20px',
+        letterSpacing: '0.1px',
+        color: 'rgba(0, 0, 0, 0.87)',
+        '&:last-child': {
+            fontWeight: 700,
+            color: '#20BFB8',
+        },
+    },
+});
 
 const headings: EnhancedTableHeadCell[] = [
     {
@@ -64,6 +96,9 @@ export function CommissionEarnings() {
 
         ...bracketParams(),
     });
+
+    const { data, paginationProps } = commissionEarnings$;
+
     useEffect(
         () => {
             if (!commissionEarnings$.isLoading) {
@@ -78,13 +113,30 @@ export function CommissionEarnings() {
         [sortCommissionFilter],
     );
 
-    if (commissionEarnings$.data.length === 0 && commissionEarnings$.isLoading) {
+    if (data.length === 0 && commissionEarnings$.isLoading) {
         return <CircularProgress />;
     }
 
+    const tableRows = data?.map((data: ReferralCommissionEarningsEntity) => (
+        <TableRow key={data?.id}>
+            <StyledTableCell>
+                <Grid container alignItems={'center'}>
+                    <Avatar src={data?.profileImage ?? ''}>{data?.getInitials()}</Avatar>
+                    <Grid item xs container pl={2}>
+                        <Typography sx={{ fontSize: '14px' }}>{data?.fullName}</Typography>
+                    </Grid>
+                </Grid>
+            </StyledTableCell>
+            <StyledTableCell>{data?.paidAt ? moment(data?.paidAt).format('lll') : '-'}</StyledTableCell>
+            <StyledTableCell align={'right'}>{data?.cards}</StyledTableCell>
+            <StyledTableCell align={'right'}>${round(data?.submissionTotal, 2).toFixed(2)}</StyledTableCell>
+            <StyledTableCell align={'right'}>${round(data?.commission, 2).toFixed(2)}</StyledTableCell>
+        </TableRow>
+    ));
+
     return (
         <>
-            {commissionEarnings$.data.length === 0 ? (
+            {data.length === 0 ? (
                 <EmptyStates
                     heading={'No Commission Earnings'}
                     description={'You haven’t earned any commission, yet.'}
@@ -92,9 +144,11 @@ export function CommissionEarnings() {
                 />
             ) : (
                 <ReferralTable
-                    tableData={commissionEarnings$}
                     heading={'Commission Earnings'}
                     tableHeadings={headings}
+                    tableRows={tableRows}
+                    count={data.length}
+                    paginationProps={paginationProps}
                 />
             )}
         </>

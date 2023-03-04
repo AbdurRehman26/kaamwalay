a527cdca59ef1cc1f09be3a2fe60a721b59bd62e
numair97 <numairsiddiquie98@gmail.com>
2023-03-04 05:36:58 +0500

    added full name search support and also small fixes on FE side for searching

--- app/Http/Filters/AdminOrderSearchFilter.php
+++ app/Http/Filters/AdminOrderSearchFilter.php
@@ -9,16 +9,22 @@ class AdminOrderSearchFilter implements Filter
 {
     public function __invoke(Builder $query, $value, string $property)
     {
-        $query->whereLike(
-            [
-                'order_number',
-                'user.customer_number',
-                'user.first_name',
-                'user.last_name',
-                'user.email',
-                'orderItems.userCard.certificate_number',
-            ],
-            $value
-        );
+        $fullNameArray = explode(' ', $value);
+        if (count($fullNameArray) === 2) {
+            $query->whereLike('user.first_name', $fullNameArray[0])->whereLike('user.last_name', $fullNameArray[1]);
+        } else {
+            $query->whereLike(
+                [
+                    'order_number',
+                    'user.customer_number',
+                    'user.first_name',
+                    'user.last_name',
+                    'user.email',
+                    'orderItems.userCard.certificate_number',
+                ],
+                $value
+            );
+        }
+        
     }
 }
--- app/Http/Filters/AdminSalesmanSearchFilter.php
+++ app/Http/Filters/AdminSalesmanSearchFilter.php
@@ -12,14 +12,19 @@ class AdminSalesmanSearchFilter implements Filter
      */
     public function __invoke(Builder $query, $value, string $property): void
     {
-        $query->whereLike(
-            [
-                'first_name',
-                'last_name',
-                'email',
-                'phone',
-            ],
-            $value
-        );
+        $fullNameArray = explode(' ', $value);
+        if (count($fullNameArray) === 2) {
+            $query->where('first_name', $fullNameArray[0])->where('last_name', $fullNameArray[1]);
+        } else {
+            $query->whereLike(
+                [
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'phone',
+                ],
+                $value
+            );
+        }
     }
 }

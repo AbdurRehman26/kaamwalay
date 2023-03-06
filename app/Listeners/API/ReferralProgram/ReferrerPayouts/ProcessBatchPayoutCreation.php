e5724f2658e613f27a9d2340549b13d655fd1f2e
Kazmi <sydabdrehman@gmail.com>
2023-03-06 15:10:57 +0100

    merge done

--- app/Listeners/API/ReferralProgram/ReferrerPayouts/ProcessBatchPayoutCreation.php
+++ app/Listeners/API/ReferralProgram/ReferrerPayouts/ProcessBatchPayoutCreation.php
@@ -4,8 +4,8 @@ namespace App\Listeners\API\ReferralProgram\ReferrerPayouts;
 
 use App\Events\API\Admin\ReferralProgram\BatchPayoutCreated;
 use App\Services\Admin\V3\ReferralProgram\ReferrerPayoutService;
-use Illuminate\Contracts\Queue\ShouldBeEncrypted;
 use Illuminate\Contracts\Queue\ShouldQueue;
+
 class ProcessBatchPayoutCreation implements ShouldQueue
 {
     public function __construct(protected ReferrerPayoutService $referrerPayoutService)

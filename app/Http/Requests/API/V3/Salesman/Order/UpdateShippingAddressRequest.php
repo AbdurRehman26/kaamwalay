a44594c80113ed4bfc127f49de000d50b0f65677
Luis Molina <lm.molinab@gmail.com>
2023-03-06 11:33:21 -0500

    static analysis fix

--- app/Http/Requests/API/V3/Salesman/Order/UpdateShippingAddressRequest.php
+++ app/Http/Requests/API/V3/Salesman/Order/UpdateShippingAddressRequest.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Requests\API\V3\Salesman\Order;
 
+use App\Models\Order;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateShippingAddressRequest extends FormRequest
 {
     public function authorize(): bool
     {
+        /** @var Order $order */
         $order = $this->route('order');
 
         return auth()->user()->isSalesman() && $order->salesman()->exists() && $order->salesman()->is(auth()->user());

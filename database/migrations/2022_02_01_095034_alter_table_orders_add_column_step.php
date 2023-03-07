9dddc19c865128cee73b64b3f1d7712b32afc09c
Shift <shift@laravelshift.com>
2023-03-07 14:15:09 +0000

    Adopt anonymous migrations

--- database/migrations/2022_02_01_095034_alter_table_orders_add_column_step.php
+++ database/migrations/2022_02_01_095034_alter_table_orders_add_column_step.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterTableOrdersAddColumnStep extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -30,4 +30,4 @@ class AlterTableOrdersAddColumnStep extends Migration
             $table->dropColumn('order_step');
         });
     }
-}
+};

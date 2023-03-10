1b89420b360916809f370eea3015207d48ecaa5b
numair97 <numairsiddiquie98@gmail.com>
2023-03-10 06:12:00 +0500

    fixed issues

--- resources/views/landings/referralhome/community.blade.php
+++ resources/views/landings/referralhome/community.blade.php
@@ -8,43 +8,43 @@
 
     <div class="container ReferralHome-sectionContainer ReferralHome-featuresGrid" data-aos="fade-up"data-aos-delay="100">
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-            <img src="{{ asset('assets/images/landings/referralhome/9.png') }}" alt="" class="ReferralHome-communityIcon">
+            <img src="{{ asset('assets/images/landings/referralhome/9.png') }}" alt="" class="ReferralHome-communityIcon" data-imagebox="gallery" >
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-            <img src="{{ asset('assets/images/landings/referralhome/6.jpg') }}" alt="" class="ReferralHome-communityIcon">
+            <img src="{{ asset('assets/images/landings/referralhome/6.jpg') }}" alt="" class="ReferralHome-communityIcon" data-imagebox="gallery">
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-            <img src="{{ asset('assets/images/landings/referralhome/2.jpg') }}" alt="" class="ReferralHome-communityIcon">
+            <img src="{{ asset('assets/images/landings/referralhome/2.jpg') }}" alt="" class="ReferralHome-communityIcon" data-imagebox="gallery">
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-            <img src="{{ asset('assets/images/landings/referralhome/5.jpg') }}" alt="" class="ReferralHome-communityIcon">
+            <img src="{{ asset('assets/images/landings/referralhome/5.jpg') }}" alt="" class="ReferralHome-communityIcon" data-imagebox="gallery">
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-                <img src="{{ asset('assets/images/landings/referralhome/1.jpg') }}" alt="" class="ReferralHome-communityIcon">
+                <img src="{{ asset('assets/images/landings/referralhome/1.jpg') }}" alt="" class="ReferralHome-communityIcon" data-imagebox="gallery">
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-                <img src="{{ asset('assets/images/landings/referralhome/10.jpg') }}" alt="" class="ReferralHome-communityIcon">
+                <img src="{{ asset('assets/images/landings/referralhome/10.jpg') }}" alt="" class="ReferralHome-communityIcon" data-imagebox="gallery">
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-                <img src="{{ asset('assets/images/landings/referralhome/11.jpg') }}" alt="" class="ReferralHome-communityIcon">
+                <img src="{{ asset('assets/images/landings/referralhome/11.jpg') }}" alt="" class="ReferralHome-communityIcon" data-imagebox="gallery">
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-                <img src="{{ asset('assets/images/landings/referralhome/12.jpg') }}" alt="" class="ReferralHome-communityIcon">
+                <img src="{{ asset('assets/images/landings/referralhome/12.jpg') }}" alt="" class="ReferralHome-communityIcon" data-imagebox="gallery">
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-                <img src="{{ asset('assets/images/landings/referralhome/7.jpg') }}" alt="" class="ReferralHome-communityIcon">
+                <img src="{{ asset('assets/images/landings/referralhome/7.jpg') }}" alt="" class="ReferralHome-communityIcon" data-imagebox="gallery">
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-                <img src="{{ asset('assets/images/landings/referralhome/4.jpg') }}" alt="" class="ReferralHome-communityIcon">
+                <img src="{{ asset('assets/images/landings/referralhome/4.jpg') }}" alt="" class="ReferralHome-communityIcon" data-imagebox="gallery">
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-                <img src="{{ asset('assets/images/landings/referralhome/3.jpg') }}" alt="" class="ReferralHome-communityIcon">
+                <img src="{{ asset('assets/images/landings/referralhome/3.jpg') }}" alt="" class="ReferralHome-communityIcon" data-imagebox="gallery">
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-                <img src="{{ asset('assets/images/landings/referralhome/8.jpg') }}" alt="" class="ReferralHome-communityIcon">
+                <img src="{{ asset('assets/images/landings/referralhome/8.jpg') }}" alt="" class="ReferralHome-communityIcon" data-imagebox="gallery">
         </div>
         <div class="container text-center">
-            <button onclick="(function(){window.scrollTo(0, 0)})()" class="ReferralHome-button">Join Them</button>
+            <button onclick="(function(){window.scrollTo(0, 0)})()" class="ReferralHome-button" data-aos="fade-up"data-aos-delay="100">Join Them</button>
         </div>
     </div>
 </section>
--- resources/views/landings/referralhome/view.blade.php
+++ resources/views/landings/referralhome/view.blade.php
@@ -1,23 +1,27 @@
 <x-layout>
     <x-slot name="head">
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-        <link rel="stylesheet" type="text/css"
-              href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
-        <link rel="stylesheet" type="text/css"
-              href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/tobiasroeder/imagebox@1.3.0/dist/imagebox.min.css">
     </x-slot>
-        @include('landings.referralhome.signup')
-        @include('landings.referralhome.about')
-        @include('landings.referralhome.steps')
-        @include('landings.referralhome.community')
-        @include('landings.referralhome.conclusion')
+    @include('landings.referralhome.signup')
+    @include('landings.referralhome.about')
+    @include('landings.referralhome.steps')
+    @include('landings.referralhome.community')
+    @include('landings.referralhome.conclusion')
     <x-slot name="body">
+        <style>
+            .ib-image {
+                transform: translate(-50%, -50%) rotate(0deg) !important;
+            }
+        </style>
         <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
         <script src="https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js"></script>
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script type="text/javascript"
-                src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/gh/tobiasroeder/imagebox@1.3.0/dist/imagebox.min.js"></script>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
         <script>
             (function() {
                 AOS.init({
@@ -26,18 +30,17 @@
                 });
             })();
 
-        const myGallery = GLightbox({
-            elements: [
-                {
-                    'href':  "{{ asset('assets/images/landings/referralhome/home.mp4') }}",
+            const myGallery = GLightbox({
+                elements: [{
+                    'href': "{{ asset('assets/images/landings/referralhome/home.mp4') }}",
                     'type': 'video',
-                    'source': 'assets' 
-                },
-            ]
-        });
-        function playVideo() {
-            myGallery.open();
-        }
+                    'source': 'assets'
+                }, ]
+            });
+
+            function playVideo() {
+                myGallery.open();
+            }
         </script>
     </x-slot>
 </x-layout>

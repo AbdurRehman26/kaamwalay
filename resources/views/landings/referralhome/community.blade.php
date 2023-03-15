3c0d607d8b197e04350ec3963fb93a224e52aff4
numair97 <numairsiddiquie98@gmail.com>
2023-03-15 20:21:57 +0500

    fixed requested changes

--- resources/views/landings/referralhome/community.blade.php
+++ resources/views/landings/referralhome/community.blade.php
@@ -8,62 +8,62 @@
 
     <div class="container ReferralHome-sectionContainer ReferralHome-featuresGrid" data-aos="fade-up" data-aos-delay="100">
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-            <a href="{{ asset('assets/images/landings/referralhome/9.png') }}" data-type="image" data-gallery="details" data-effect="zoom" class="glightbox" data-aos="fade-left">
+            <a href="{{ asset('assets/images/landings/referralhome/9.png') }}" class="glightbox">
                 <img src="{{ asset('assets/images/landings/referralhome/9.png') }}" alt="" class="ReferralHome-communityIcon">
             </a>
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-            <a href="{{ asset('assets/images/landings/referralhome/6.jpg') }}" data-type="image" data-gallery="details" data-effect="zoom" class="glightbox" data-aos="fade-left">
+            <a href="{{ asset('assets/images/landings/referralhome/6.jpg') }}" class="glightbox">
                 <img src="{{ asset('assets/images/landings/referralhome/6.jpg') }}" alt="" class="ReferralHome-communityIcon">
             </a>
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-            <a href="{{ asset('assets/images/landings/referralhome/2.jpg') }}" data-type="image" data-gallery="details" data-effect="zoom" class="glightbox" data-aos="fade-left">
+            <a href="{{ asset('assets/images/landings/referralhome/2.jpg') }}" class="glightbox">
                 <img src="{{ asset('assets/images/landings/referralhome/2.jpg') }}" alt="" class="ReferralHome-communityIcon">
             </a>
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-            <a href="{{ asset('assets/images/landings/referralhome/5.jpg') }}" data-type="image" data-gallery="details" data-effect="zoom" class="glightbox" data-aos="fade-left">
+            <a href="{{ asset('assets/images/landings/referralhome/5.jpg') }}" class="glightbox">
                 <img src="{{ asset('assets/images/landings/referralhome/5.jpg') }}" alt="" class="ReferralHome-communityIcon">
             </a>
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-            <a href="{{ asset('assets/images/landings/referralhome/1.jpg') }}" data-type="image" data-gallery="details" data-effect="zoom" class="glightbox" data-aos="fade-left">
+            <a href="{{ asset('assets/images/landings/referralhome/1.jpg') }}" class="glightbox">
                 <img src="{{ asset('assets/images/landings/referralhome/1.jpg') }}" alt="" class="ReferralHome-communityIcon">
             </a>
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-            <a href="{{ asset('assets/images/landings/referralhome/10.jpg') }}" data-type="image" data-gallery="details" data-effect="zoom" class="glightbox" data-aos="fade-left">
+            <a href="{{ asset('assets/images/landings/referralhome/10.jpg') }}" class="glightbox">
                 <img src="{{ asset('assets/images/landings/referralhome/10.jpg') }}" alt="" class="ReferralHome-communityIcon">
             </a>
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-            <a href="{{ asset('assets/images/landings/referralhome/11.jpg') }}" data-type="image" data-gallery="details" data-effect="zoom" class="glightbox" data-aos="fade-left">
+            <a href="{{ asset('assets/images/landings/referralhome/11.jpg') }}" class="glightbox">
                 <img src="{{ asset('assets/images/landings/referralhome/11.jpg') }}" alt="" class="ReferralHome-communityIcon">
             </a>
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-            <a href="{{ asset('assets/images/landings/referralhome/12.jpg') }}" data-type="image" data-gallery="details" data-effect="zoom" class="glightbox" data-aos="fade-left">
+            <a href="{{ asset('assets/images/landings/referralhome/12.jpg') }}" class="glightbox">
                 <img src="{{ asset('assets/images/landings/referralhome/12.jpg') }}" alt="" class="ReferralHome-communityIcon">
             </a>
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-            <a href="{{ asset('assets/images/landings/referralhome/7.jpg') }}" data-type="image" data-gallery="details" data-effect="zoom" class="glightbox" data-aos="fade-left">
+            <a href="{{ asset('assets/images/landings/referralhome/7.jpg') }}" class="glightbox">
                 <img src="{{ asset('assets/images/landings/referralhome/7.jpg') }}" alt="" class="ReferralHome-communityIcon">
             </a>
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-            <a href="{{ asset('assets/images/landings/referralhome/4.jpg') }}" data-type="image" data-gallery="details" data-effect="zoom" class="glightbox" data-aos="fade-left">
+            <a href="{{ asset('assets/images/landings/referralhome/4.jpg') }}" class="glightbox">
                 <img src="{{ asset('assets/images/landings/referralhome/4.jpg') }}" alt="" class="ReferralHome-communityIcon">
             </a>
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-            <a href="{{ asset('assets/images/landings/referralhome/3.jpg') }}" data-type="image" data-gallery="details" data-effect="zoom" class="glightbox" data-aos="fade-left">
+            <a href="{{ asset('assets/images/landings/referralhome/3.jpg') }}" class="glightbox">
                 <img src="{{ asset('assets/images/landings/referralhome/3.jpg') }}" alt="" class="ReferralHome-communityIcon">
             </a>
         </div>
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--fourth">
-            <a href="{{ asset('assets/images/landings/referralhome/8.jpg') }}" data-type="image" data-gallery="details" data-effect="zoom" class="glightbox" data-aos="fade-left">
+            <a href="{{ asset('assets/images/landings/referralhome/8.jpg') }}" class="glightbox">
                 <img src="{{ asset('assets/images/landings/referralhome/8.jpg') }}" alt="" class="ReferralHome-communityIcon">
             </a>
         </div>

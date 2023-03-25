c69ad67ee8585ad2f37c0f5d69e97790fb72c88c
numair97 <numairsiddiquie98@gmail.com>
2023-03-26 03:33:40 +0500

    added code for responsiveness

--- resources/views/landings/partners/hero.blade.php
+++ resources/views/landings/partners/hero.blade.php
@@ -1,12 +1,11 @@
 <x-layout.header />
 <div class="partner-hero">
-    <div class="container-fluid align-middle partner-hero__container">
+    <div class="partner-hero__container">
         <div class="partner-hero__overlay">
-            <div class="partner-hero__content">
+            <div class="container partner-hero__content">
                 <p class="partner-hero__title-first">EARN MONEY <span class="partner-hero__title-second">WITH THE</span></p>
                 <h1 class="partner-hero__title"> AGS Partner Program</h1>
-                <h2 class="partner-hero__subtitle">Refer your friends/followers and give them {{ config('robograding.feature_referral_discount_percentage') }}% OFF their first RoboGrading submission.</h2>
-                <h2 class="partner-hero__subtitle">You will get commission as soon as they pay. Not store credit, you’ll earn cold hard cash!</h2>
+                <h2 class="partner-hero__subtitle">Refer your friends/followers and give them {{ config('robograding.feature_referral_discount_percentage') }}% OFF their first RoboGrading submission. You will get commission as soon as they pay. Not store credit, you’ll earn cold hard cash!</h2>
                 <div data-atom="auth-button" data-class-name="partner-hero__auth-button" data-button-content="START EARNING">
                     {{-- JS runtime actions --}}
                 </div>

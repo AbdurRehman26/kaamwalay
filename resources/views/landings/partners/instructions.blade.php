1d685d20699237c36ed17fef206d0f41b8d4a75d
numair97 <numairsiddiquie98@gmail.com>
2023-03-26 01:25:07 +0500

    added animate

--- resources/views/landings/partners/instructions.blade.php
+++ resources/views/landings/partners/instructions.blade.php
@@ -1,10 +1,10 @@
 <section class="partner-instructions">
     <div class="container partner-instructions__container">
         <div class="partner-instructions__heading-div">
-            <h1 class="partner-instructions__heading">How It Works</h1>
+            <h1 class="partner-instructions__heading" data-aos="fade" data-aos-delay="100">How It Works</h1>
         </div>
         <div class="partner-instructions__images-div">
-            <div class="partner-instructions__instruction-div">
+            <div class="partner-instructions__instruction-div" data-aos="fade" data-aos-delay="100">
                 <div class="partner-instructions__image-div">
                     <img class="partner-instructions__image" src="{{ asset('assets/images/landings/partner/share.svg') }}" alt="share" />
                 </div>
@@ -16,7 +16,7 @@
                     <p class="partner-instructions__image-content__description">Share your link with all your friends and followers.</p>
                 </div>
             </div>
-            <div class="partner-instructions__instruction-div">
+            <div class="partner-instructions__instruction-div" data-aos="fade" data-aos-delay="100">
                 <div class="partner-instructions__image-div">
                     <img class="partner-instructions__image" src="{{ asset('assets/images/landings/partner/pay.svg') }}" alt="pay" />
                 </div>
@@ -28,7 +28,7 @@
                     <p class="partner-instructions__image-content__description">They click your link to sign up and pay for a submission.</p>
                 </div>
             </div>
-            <div class="partner-instructions__instruction-div">
+            <div class="partner-instructions__instruction-div" data-aos="fade" data-aos-delay="100">
                 <div class="partner-instructions__image-div">
                     <img class="partner-instructions__image" src="{{ asset('assets/images/landings/partner/earn-commission.svg') }}" alt="earn-commission" />
                 </div>
@@ -41,7 +41,7 @@
                 </div>
             </div>
         </div>
-        <div data-atom="auth-button" data-class-name="partner-hero__auth-button" data-button-content="START REFERRING">
+        <div data-atom="auth-button" data-class-name="partner-hero__auth-button" data-button-content="START REFERRING" data-aos="fade">
             {{-- JS runtime actions --}}
         </div>
     </div>
--- resources/views/landings/partners/marketing-content.blade.php
+++ resources/views/landings/partners/marketing-content.blade.php
@@ -1,10 +1,10 @@
 <section class="marketing-content">
     <div class="container">
         <div class="marketing-content__heading-div">
-            <p class="marketing-content__heading-div__small-text">PRE-made Content for social media </p>
-            <h1 class="marketing-content__heading-div__heading">Share these posts with your link</h1>
+            <p class="marketing-content__heading-div__small-text" data-aos="fade">PRE-made Content for social media </p>
+            <h1 class="marketing-content__heading-div__heading" data-aos="fade">Share these posts with your link</h1>
         </div>
-        <div class="marketing-content__images-div">
+        <div class="marketing-content__images-div" data-aos="fade-up">
             <div class="marketing-content__image-div">
                 <a href="{{ asset('assets/images/landings/partner/agsPartnerProgramPost1.png') }}" class="glightbox">
                     <img src="{{ asset('assets/images/landings/partner/agsPartnerProgramPost1.png') }}" alt="marketing-content" class="marketing-content__image-div__image">
@@ -66,7 +66,7 @@
                 </a>
             </div>
         </div>
-        <div data-atom="auth-button" data-class-name="partner-hero__auth-button" data-button-content="START SHARING">
+        <div data-atom="auth-button" data-class-name="partner-hero__auth-button" data-button-content="START SHARING" data-aos="fade-up">
             {{-- JS runtime actions --}}
         </div>
     </div>

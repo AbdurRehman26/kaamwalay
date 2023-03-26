6eef7fc63a07054cf9e618ecfa99ea1067572189
numair97 <numairsiddiquie98@gmail.com>
2023-03-26 15:11:27 +0500

    fixed css issues

--- resources/views/landings/partners/content.blade.php
+++ resources/views/landings/partners/content.blade.php
@@ -6,7 +6,7 @@
         <div class="partner-content__text-div">
             <p class="partner-content__small-title" data-aos="fade-left">Spread the word & Rake in the rewards</p>
             <h1 class="partner-content__title" data-aos="fade-left">You could earn thousands of dollars a month!</h1>
-            <p class="partner-content__description" data-aos="fade-left">The more you promote AGS, the more you earn. You will make commission for every paid submission (forever) from people your refer.Commission can be <span class="partner-content__bold-text">$1 per card in submission</span> or <span class="partner-content__bold-text">10% of total cost of submission</span> (whichever is lower).</p>
+            <p class="partner-content__description" data-aos="fade-left">The more you promote AGS, the more you earn. You will make commission for every paid submission (forever) from people your refer. Commission can be <span class="partner-content__bold-text">$1 per card in submission</span> or <span class="partner-content__bold-text">10% of total cost of submission</span> (whichever is lower).</p>
             <div data-aos="fade-left" data-atom="auth-button" data-class-name="partner-hero__auth-button" data-button-content="JOIN THE PARTNER PROGRAM">
                 {{-- JS runtime actions --}}
             </div>

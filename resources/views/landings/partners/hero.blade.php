c49f45187848a23a19ff46d55323645c44a03877
numair97 <numairsiddiquie98@gmail.com>
2023-03-26 15:44:22 +0500

    fixed overflow issue on mobile view

--- resources/views/landings/partners/hero.blade.php
+++ resources/views/landings/partners/hero.blade.php
@@ -6,7 +6,7 @@
                 <p class="partner-hero__title-first">EARN MONEY <span class="partner-hero__title-second">WITH THE</span></p>
                 <h1 class="partner-hero__title"> AGS Partner Program</h1>
                 <h2 class="partner-hero__subtitle">Refer your friends/followers and give them {{ config('robograding.feature_referral_discount_percentage') }}% OFF their first RoboGrading submission. You will get commission as soon as they pay. Not store credit, you’ll earn cold hard cash!</h2>
-                <div data-atom="auth-button" data-class-name="partner-hero__auth-button" data-button-content="START EARNING">
+                <div data-atom="auth-button" data-class-name="partner-hero__auth-button partner-hero__auth-button__font-color" data-button-content="START EARNING">
                     {{-- JS runtime actions --}}
                 </div>
             </div>

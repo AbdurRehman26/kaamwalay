0d359921f93667e9916838a9f1f9857b89dc0e9e
numair97 <numairsiddiquie98@gmail.com>
2023-03-29 01:38:53 +0500

    changed text

--- resources/views/landings/referral/hero.blade.php
+++ resources/views/landings/referral/hero.blade.php
@@ -4,7 +4,7 @@
         <div class="referral-hero__overlay">
             <div class="container referral-hero__content">
                 <p class="referral-hero__title-first">EARN MONEY <span class="referral-hero__title-second">WITH THE</span></p>
-                <h1 class="referral-hero__title"> AGS referral Program</h1>
+                <h1 class="referral-hero__title">  AGS Partner Program</h1>
                 <h2 class="referral-hero__subtitle">Refer your friends/followers and give them {{ config('robograding.feature_referral_discount_percentage') }}% OFF their first RoboGrading submission. You will get commission as soon as they pay. Not store credit, you’ll earn cold hard cash!</h2>
                 <div data-atom="auth-button" data-class-name="referral-hero__auth-button referral-hero__auth-button__font-color" data-button-content="START EARNING">
                     {{-- JS runtime actions --}}

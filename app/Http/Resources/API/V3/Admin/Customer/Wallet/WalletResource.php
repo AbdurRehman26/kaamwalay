a4120cb74cbe78edbe425f5bedb5cb23f66b2d75
Luis Molina <lm.molinab@gmail.com>
2023-04-07 17:05:31 -0500

    v3 wallet resource

---
+++ app/Http/Resources/API/V3/Admin/Customer/Wallet/WalletResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources\API\V3\Admin\Customer\Wallet;
+
+use App\Http\Resources\API\BaseResource;
+use App\Models\Wallet;
+use Illuminate\Http\Request;
+
+/** @mixin Wallet */
+class WalletResource extends BaseResource
+{
+    /**
+     * Transform the resource into an array.
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'balance' => $this->balance,
+        ];
+    }
+}

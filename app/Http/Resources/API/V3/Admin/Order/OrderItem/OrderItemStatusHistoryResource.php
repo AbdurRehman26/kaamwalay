26604bafc8ae8ad4803f0b747ab8b3f8434c362f
Luis Molina <lm.molinab@gmail.com>
2023-04-07 11:51:55 -0500

    workaround for issue with loading user when not needed

--- app/Http/Resources/API/V3/Admin/Order/OrderItem/OrderItemStatusHistoryResource.php
+++ app/Http/Resources/API/V3/Admin/Order/OrderItem/OrderItemStatusHistoryResource.php
@@ -13,10 +13,21 @@ class OrderItemStatusHistoryResource extends JsonResource
 {
     public function toArray($request): array
     {
+        $orderItemStatusResource = null;
+        $userResource = null;
+
+        if ($this->resource->relationLoaded('orderItemStatus')){
+            $orderItemStatusResource =new OrderItemStatusResource($this->orderItemStatus);
+        }
+
+        if ($this->resource->relationLoaded('user')){
+            $userResource = new UserResource($this->user);
+        }
+
         return [
             'id' => $this->id,
-            'order_item_status' => $this->whenLoaded('orderItemStatus', new OrderItemStatusResource($this->orderItemStatus)),
-            'user' => $this->whenLoaded('user', new UserResource($this->user)),
+            'order_item_status' => $this->whenLoaded('orderItemStatus', $orderItemStatusResource),
+            'user' => $this->whenLoaded('user', $userResource),
             'notes' => $this->notes,
             'updated_at' => $this->updated_at,
         ];

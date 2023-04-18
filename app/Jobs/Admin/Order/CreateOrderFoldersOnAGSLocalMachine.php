f6e9f9aef2130a379193fb0ced7f3b77a088e463
numair97 <numair97@users.noreply.github.com>
2023-04-18 22:51:07 +0000

    Fix styling

--- app/Jobs/Admin/Order/CreateOrderFoldersOnAGSLocalMachine.php
+++ app/Jobs/Admin/Order/CreateOrderFoldersOnAGSLocalMachine.php
@@ -11,8 +11,8 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Throwable;
 
 class CreateOrderFoldersOnAGSLocalMachine implements ShouldQueue

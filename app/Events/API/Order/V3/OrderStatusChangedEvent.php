031f9b425c1a0e479838f5330105c1c9cce6e686
Luis Molina <lm.molinab@gmail.com>
2023-04-26 12:22:55 -0500

    new tests files

---
+++ app/Events/API/Order/V3/OrderStatusChangedEvent.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Events\API\Order\V3;
+
+use App\Events\API\Order\V2\OrderStatusChangedEvent as V2OrderStatusChangedEvent;
+
+class OrderStatusChangedEvent extends V2OrderStatusChangedEvent
+{
+}

557c2db03dbc14ca8140b7f721b5d145dce6f4c0
Nabeel Ahmed <thisisnabeelahmed@gmail.com>
2023-07-27 16:19:21 +0500

    Testing Pint GA

--- app/Http/Resources/API/V1/Admin/Order/OrderResource.php
+++ app/Http/Resources/API/V1/Admin/Order/OrderResource.php
@@ -58,7 +58,7 @@ class OrderResource extends BaseResource
             'id' => $this->id,
             'order_number' => $this->order_number,
             'number_of_cards' => (int)$this->orderItems()->sum('quantity'),
-            'total_declared_value' => (float) $this->orderItems()->sum('declared_value_total'),
+            'total_declared_value' => (float)$this->orderItems()->sum('declared_value_total'),
             'service_fee' => $this->service_fee,
             'shipping_fee' => $this->shipping_fee,
             'grand_total' => $this->grand_total,

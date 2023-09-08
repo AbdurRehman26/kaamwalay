a737700b69ef5749cf3896a30b0a2bf74d464a0d
numair97 <numairsiddiquie98@gmail.com>
2023-09-08 23:10:38 +0500

    updated code in referral page

--- resources/views/landings/referralhome/conclusion.blade.php
+++ resources/views/landings/referralhome/conclusion.blade.php
@@ -1,4 +1,4 @@
-<section class="ReferralHome-features">
+<section class="ReferralHome-section ReferralHome-features">
     <div class="container ReferralHome-sectionContainer ReferralHome-sectionContainer--center-center">
         <div class="ReferralHome-sectionColumn ReferralHome-sectionColumn--half ReferralHome-sectionColumn--center-center">
             <h4 class="ReferralHome-sectionContentCaption text-center" data-aos="fade-up">Join the
@@ -11,21 +11,29 @@
 
     <div class="container ReferralHome-sectionContainer">
         <div class="ReferralHome-cardImages">
-            <div class="ReferralHome-cardColumn hide-tablet">
-                <img src="{{ asset('assets/images/landings/home/card01.jpg') }}" alt="" data-aos="fade"
+            <div class="ReferralHome-cardColumn ReferralHome-cardColumn-Large">
+                <img src="{{ asset('assets/images/landings/home/card01.png') }}" alt="" data-aos="fade"
                     class="ReferralHome-cardImage" />
             </div>
-            <div class="ReferralHome-cardColumn">
-                <img src="{{ asset('assets/images/landings/home/card02.jpg') }}" alt="" data-aos="fade"
+            <div class="ReferralHome-cardColumn ReferralHome-cardColumn-Medium">
+                <img src="{{ asset('assets/images/landings/home/card02.png') }}" alt="" data-aos="fade"
                     data-aos-delay="50" class="ReferralHome-cardImage" />
             </div>
-            <div class="ReferralHome-cardColumn">
-                <img src="{{ asset('assets/images/landings/home/card03.jpg') }}" alt="" data-aos="fade"
+            <div class="ReferralHome-cardColumn ReferralHome-cardColumn-Small hide-tablet">
+                <img src="{{ asset('assets/images/landings/home/card03.png') }}" alt="" data-aos="fade"
                     class="ReferralHome-cardImage" />
             </div>
-            <div class="ReferralHome-cardColumn hide-tablet">
-                <img src="{{ asset('assets/images/landings/home/card04.jpg') }}" alt="" data-aos="fade"
+            <div class="ReferralHome-cardColumn ReferralHome-cardColumn-Small hide-tablet">
+                <img src="{{ asset('assets/images/landings/home/card04.png') }}" alt="" data-aos="fade"
                     data-aos-delay="50" class="ReferralHome-cardImage" />
+            </div>
+            <div class="ReferralHome-cardColumn ReferralHome-cardColumn-Medium">
+                <img src="{{ asset('assets/images/landings/home/card05.png') }}" alt="" data-aos="fade"
+                    class="ReferralHome-cardImage" />
+            </div>
+            <div class="ReferralHome-cardColumn ReferralHome-cardColumn-Large">
+                <img src="{{ asset('assets/images/landings/home/card06.png') }}" alt="" data-aos="fade"
+                    class="ReferralHome-cardImage" />
             </div>
         </div>
     </div>

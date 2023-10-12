fe7770fecd106800566d2f7993e2a6baa0b6e364
Kazmi <s.kazmi@metro-markets.org>
2023-10-13 01:36:30 +0200

    refactoring

---
+++ database/migrations/2023_10_12_230130_add_affirm_payment_method_in_payment_meethods_channel.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('payment_methods')->insert([
+            [
+                'code' => 'stripe_affirm',
+                'name' => 'Affirm',
+                'is_visible' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::table('payment_methods')
+            ->where('code', 'stripe_affirm')
+            ->delete();
+    }
+};

668eb9a03744ccf2ff8b6d82548edbf6907f4e72
Kazmi <s.kazmi@metro-markets.org>
2023-10-31 11:51:03 +0100

    tests debug

--- tests/Feature/API/V2/Customer/Order/Payment/AffirmOrderPaymentTest.php
+++ tests/Feature/API/V2/Customer/Order/Payment/AffirmOrderPaymentTest.php
@@ -4,29 +4,22 @@ use App\Exceptions\Services\Payment\PaymentNotVerified;
 use App\Models\Order;
 use App\Models\OrderPayment;
 use App\Models\OrderStatus;
-use App\Models\PaymentMethod;
 use App\Models\User;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Response;
 
 beforeEach(function () {
-    PaymentMethod::where('id', '=', 7)->delete();
-
     Event::fake();
+
     $user = User::factory()->create([
         'stripe_id' => Str::random(25),
-    ]);
-
-    $this->paymentMethod = PaymentMethod::factory()->create([
-        'id' => 7,
-        'code' => 'stripe_affirm',
     ]);
 
     $this->order = Order::factory()->create([
         'user_id' => $user->id,
         'coupon_id' => null,
-        'payment_method_id' => 1,
+        'payment_method_id' => 7,
         'order_status_id' => OrderStatus::PLACED,
     ]);
 

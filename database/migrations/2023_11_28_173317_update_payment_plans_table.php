306a74e4370507c5d3105f944ecd5ef4e7e08693
Kazmi <s.kazmi@metro-markets.org>
2023-11-28 23:25:12 +0100

    payment plan data updated

---
+++ database/migrations/2023_11_28_173317_update_payment_plans_table.php
@@ -0,0 +1,156 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $newPricing = [
+            [
+                'id' => 1,
+                'price' => 14,
+                'max_protection_amount' => 100,
+                'turnaround' => '30 Business Days',
+                'estimated_delivery_days_min' => 30,
+                'estimated_delivery_days_max' => 30,
+                'display_position' => 1
+            ],
+            [
+                'id' => 2,
+                'price' => 20,
+                'max_protection_amount' => 200,
+                'turnaround' => '20 Business Days',
+                'estimated_delivery_days_min' => 20,
+                'estimated_delivery_days_max' => 20,
+                'display_position' => 2
+            ],
+            [
+                'id' => 3,
+                'price' => 30,
+                'max_protection_amount' => 500,
+                'turnaround' => '10 Business Days',
+                'estimated_delivery_days_min' => 10,
+                'estimated_delivery_days_max' => 10,
+                'display_position' => 3
+            ],
+            [
+                'id' => 4,
+                'price' => 50,
+                'max_protection_amount' => 1000,
+                'turnaround' => '4-6 Business Days',
+                'estimated_delivery_days_min' => 4,
+                'estimated_delivery_days_max' => 6,
+                'display_position' => 4
+            ],
+            [
+                'id' => 5,
+                'price' => 75,
+                'max_protection_amount' => 2000,
+                'turnaround' => '2 Business Days',
+                'estimated_delivery_days_min' => 2,
+                'estimated_delivery_days_max' => 2,
+                'display_position' => 5
+            ],
+            [
+                'id' => 6,
+                'price' => 100,
+                'max_protection_amount' => 5000,
+                'turnaround' => 'Same Day',
+                'estimated_delivery_days_min' => 1,
+                'estimated_delivery_days_max' => 1,
+                'display_position' => 6
+            ],
+            [
+                'id' => 7,
+                'price' => 200,
+                'max_protection_amount' => 10000,
+                'turnaround' => 'Same Day',
+                'estimated_delivery_days_min' => 1,
+                'estimated_delivery_days_max' => 1,
+                'display_position' => 7
+            ],
+        ];
+
+        DB::table('payment_plans')->upsert($newPricing, ['id'], ['price', 'max_protection_amount', 'turnaround', 'estimated_delivery_days_min', 'estimated_delivery_days_max']);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        $newPricing = [
+            [
+                'id' => 1,
+                'price' => 14,
+                'max_protection_amount' => 100,
+                'turnaround' => '20 Business Days',
+                'estimated_delivery_days_min' => 20,
+                'estimated_delivery_days_max' => 20,
+                'display_position' => 1
+            ],
+            [
+                'id' => 2,
+                'price' => 20,
+                'max_protection_amount' => 200,
+                'turnaround' => '5 Business Days',
+                'estimated_delivery_days_min' => 5,
+                'estimated_delivery_days_max' => 5,
+                'display_position' => 2
+            ],
+            [
+                'id' => 3,
+                'price' => 30,
+                'max_protection_amount' => 500,
+                'turnaround' => '5 Business Days',
+                'estimated_delivery_days_min' => 5,
+                'estimated_delivery_days_max' => 5,
+                'display_position' => 3
+            ],
+            [
+                'id' => 4,
+                'price' => 50,
+                'max_protection_amount' => 1000,
+                'turnaround' => '2-3 Business Days',
+                'estimated_delivery_days_min' => 2,
+                'estimated_delivery_days_max' => 3,
+                'display_position' => 4
+            ],
+            [
+                'id' => 5,
+                'price' => 75,
+                'max_protection_amount' => 2000,
+                'turnaround' => '1 Business Day',
+                'estimated_delivery_days_min' => 1,
+                'estimated_delivery_days_max' => 1,
+                'display_position' => 5
+            ],
+            [
+                'id' => 6,
+                'price' => 100,
+                'max_protection_amount' => 5000,
+                'turnaround' => 'Same Day',
+                'estimated_delivery_days_min' => 1,
+                'estimated_delivery_days_max' => 1,
+                'display_position' => 6
+            ],
+            [
+                'id' => 7,
+                'price' => 200,
+                'max_protection_amount' => 10000,
+                'turnaround' => 'Same Day',
+                'estimated_delivery_days_min' => 1,
+                'estimated_delivery_days_max' => 1,
+                'display_position' => 7
+            ],
+        ];
+
+        DB::table('payment_plans')->upsert($newPricing, ['id'], ['price', 'max_protection_amount', 'turnaround', 'estimated_delivery_days_min', 'estimated_delivery_days_max']);
+    }
+};

5d5e4c867611b377a293f02d6bfd4e212356e2ee
Kazmi <s.kazmi@metro-markets.org>
2023-12-13 02:34:31 +0500

    moved method to helpers

--- app/Http/Resources/API/V3/Customer/CustomerResource.php
+++ app/Http/Resources/API/V3/Customer/CustomerResource.php
@@ -18,31 +18,11 @@ class CustomerResource extends BaseResource
     {
         return [
             'id' => $this->id,
-            'email' => $this->maskEmail(),
+            'email' => maskEmail($this->email),
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'customer_number' => $this->customer_number,
             'profile_image' => $this->profile_image,
         ];
-    }
-
-    public function maskEmail(): string
-    {
-        $email = $this->email;
-
-        // Separate the local part and domain
-        [$localPart, $domain] = explode('@', $email, 2);
-
-        // Mask all characters in the local part except the first letter
-        $maskedLocalPart = $localPart[0].str_repeat('*', max(0, strlen($localPart) - 1));
-
-        // Separate the domain into subdomain and top-level domain
-        [$subdomain, $tld] = explode('.', $domain, 2);
-
-        // Mask all characters in the subdomain
-        $maskedSubdomain = str_repeat('*', strlen($subdomain));
-
-        // Combine the masked local part, masked subdomain, and top-level domain
-        return $maskedLocalPart.'@'.$maskedSubdomain.'.'.$tld;
     }
 }

34fcb89133957132f30412059d7257263eb4b69e
s.kazmi <s.kazmi@metro-markets.de>
2023-12-26 22:13:18 +0500

    empty line removed

--- routes/v3/api.php
+++ routes/v3/api.php
@@ -17,7 +17,6 @@ Route::prefix('auth')->group(function () {
 });
 
 Route::prefix('customer')->group(function () {
-
     Route::prefix('orders')->group(function () {
         Route::apiResource('payment-plans', PaymentPlanController::class)->only(['index', 'show']);
     });

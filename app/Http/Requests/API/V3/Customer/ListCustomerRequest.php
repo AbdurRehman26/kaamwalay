f2aace2b0a3c80363c9087197bdb64145deed348
AbdurRehman26 <AbdurRehman26@users.noreply.github.com>
2024-01-08 18:57:41 +0000

    Fix styling

--- app/Http/Requests/API/V3/Customer/ListCustomerRequest.php
+++ app/Http/Requests/API/V3/Customer/ListCustomerRequest.php
@@ -4,12 +4,12 @@ namespace App\Http\Requests\API\V3\Customer;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ListCustomerRequest  extends FormRequest
+class ListCustomerRequest extends FormRequest
 {
     public function rules(): array
     {
         return [
-            'filter.search' => ['required']
+            'filter.search' => ['required'],
         ];
     }
 }

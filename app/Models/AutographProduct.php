4bdec02f8c0bdaf3651764a1a0278d70e3c5b86d
Nabeel Ahmed <thisisnabeelahmed@gmail.com>
2024-01-11 02:02:52 +0500

    Feature: Autograph Authentication - Make searchable

--- app/Models/AutographProduct.php
+++ app/Models/AutographProduct.php
@@ -5,14 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Laravel\Scout\Searchable;
 
 class AutographProduct extends Model
 {
-    use HasFactory;
+    use HasFactory, Searchable;
 
     protected $casts = [
         'signed_at' => 'timestamp',
     ];
+
+    public function toSearchableArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'autograph_category' => $this->autographCategory->name,
+            'autograph_type' => $this->autographType->name,
+            'certificate_number' => $this->certificate_number,
+            'name' => $this->getLongName(),
+            'image_url' => $this->image_url,
+            'created_at_timestamp' => $this->created_at->unix(),
+        ];
+    }
 
     public function autographCategory(): BelongsTo
     {
@@ -22,5 +36,10 @@ class AutographProduct extends Model
     public function autographType(): BelongsTo
     {
         return $this->belongsTo(AutographType::class);
+    }
+
+    public function getLongName(): string
+    {
+        return $this->signed_by.' Signed '.$this->name;
     }
 }

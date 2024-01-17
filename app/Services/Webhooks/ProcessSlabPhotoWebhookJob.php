268d8820ac55d605f1b7e69b71004db86c9cfe41
Nabeel Ahmed <thisisnabeelahmed@gmail.com>
2024-01-17 19:26:47 +0500

    Feature: Made param optional in slab photos webhook

--- app/Services/Webhooks/ProcessSlabPhotoWebhookJob.php
+++ app/Services/Webhooks/ProcessSlabPhotoWebhookJob.php
@@ -15,12 +15,12 @@ class ProcessSlabPhotoWebhookJob extends ProcessWebhookJob
     {
         $payload = $this->webhookCall->getAttribute('payload');
 
-        if (empty($payload['certificate_number']) || empty($payload['front_slab_image']) || empty($payload['back_slab_image'])) {
+        if (empty($payload['certificate_number']) || (empty($payload['front_slab_image']) && empty($payload['back_slab_image']))) {
             throw new Exception('Invalid payload: '.json_encode($payload));
         }
 
         $userCard = UserCard::where('certificate_number', $payload['certificate_number'])->firstOrFail();
-        $userCard->slab_images = ['front' => $payload['front_slab_image'], 'back' => $payload['back_slab_image']];
+        $userCard->slab_images = ['front' => $payload['front_slab_image'] ?? null, 'back' => $payload['back_slab_image'] ?? null];
         $userCard->save();
 
         $userCard->searchable();

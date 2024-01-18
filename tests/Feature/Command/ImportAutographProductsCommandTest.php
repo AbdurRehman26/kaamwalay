2f28069758abbc21647e42d937d92f6197810301
Nabeel Ahmed <thisisnabeelahmed@gmail.com>
2024-01-19 04:14:37 +0500

    Feature: Autograph Authentication - Tests

--- tests/Feature/Command/ImportAutographProductsCommandTest.php
+++ tests/Feature/Command/ImportAutographProductsCommandTest.php
@@ -5,10 +5,20 @@ use App\Console\Commands\ImportAutographProductsCommand;
 it('imports autograph products', function () {
     Storage::fake();
     Storage::put('test.csv', '');
+
+    $this->artisan(ImportAutographProductsCommand::class)
+        ->expectsQuestion('What do you want to import?', 1)
+        ->expectsQuestion('What is file name?', 'test.csv')
+        ->assertExitCode(0);
+});
+
+it('imports autograph images', function () {
+    Storage::fake();
     Storage::makeDirectory('test');
 
     $this->artisan(ImportAutographProductsCommand::class)
-        ->expectsQuestion('Products Filename (storage/app/)', 'test.csv')
-        ->expectsQuestion('Images Directory (storage/app/)', 'test')
+        ->expectsQuestion('What do you want to import?', 2)
+        ->expectsQuestion('What is directory name?', 'test')
+        ->expectsQuestion('What is files prefix?', 'IMG-')
         ->assertExitCode(0);
 });

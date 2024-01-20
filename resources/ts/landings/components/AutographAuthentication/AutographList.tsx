4f722dfdbc16a71b039db37f27308135ef85900d
numair97 <numairsiddiquie98@gmail.com>
2024-01-20 15:50:34 +0500

    fixed mobile images issue

--- resources/ts/landings/components/AutographAuthentication/AutographList.tsx
+++ resources/ts/landings/components/AutographAuthentication/AutographList.tsx
@@ -28,6 +28,8 @@ const TableDiv = styled(TableContainer)({
         alignItems: 'normal',
     },
     '.TableInfoImage': {
+        maxWidth: '100%',
+        maxHeight: '100%',
         boxShadow: '0 1px 3px 0 rgba(0, 0, 0, 0.2), 0 2px 1px 0 rgba(0, 0, 0, 0.12),0 1px 1px 0 rgba(0, 0, 0, 0.14)',
     },
     '.TableInfoText': {

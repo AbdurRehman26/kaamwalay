0d8704f0812f3d662a7e080f4f0d61de466353bd
numair97 <numairsiddiquie98@gmail.com>
2024-01-21 19:19:48 +0500

    css fixes for detail page

--- resources/views/landings/authentication/partials/related-items.blade.php
+++ resources/views/landings/authentication/partials/related-items.blade.php
@@ -1,29 +1,23 @@
 <section class="authentication-view__related-items">
     <div class="container">
         <div class="bonus-earnings__heading-div">
-            <p class="bonus-earnings__heading-div__small-text authentication-view__related-items__heading" data-aos="fade">Related Items</p>
+            <p class="authentication-view__related-items__heading" data-aos="fade">Related Items</p>
         </div>
         <div class="bonus-earnings__images-div authentication-view__related-items__images-div" data-aos="fade">
-
             @foreach($related_items as $related_item)
-
-                <div class="authentication-view__related-items__image-div">
-                    <div class="authentication-view__related-items__image">
+            <a class="authentication-view__related-items__image-div" href="{{ route('authentication.view', [$related_item['certificate_number']]) }}">
+                <div>
+                    <div class="authentication-view__related-items__image image">
                         <div><span class="material-icons">verified_user</span></div>
-                        <a href="{{ route('authentication.view', [$related_item['certificate_number']]) }}">
-                            <img src="{{ $related_item['image_url'] }}" alt="bonus" class="bonus-earnings__image-div__image">
-                        </a>
+                        <img src="{{ $related_item['image_url'] }}" alt="bonus" class="bonus-earnings__image-div__image">
                     </div>
                     <div class="authentication-view__related-items__content">
-                        <a href="{{ route('authentication.view', [$related_item['certificate_number']]) }}">
-                            <h1>{{ $related_item['long_name'] }}</h1>
-                            <p class="authentication-view__related-items__certificate-number">{{ $related_item['certificate_number'] }}</p>
-                        </a>
+                        <h1 class="long-name">{{ $related_item['long_name'] }}</h1>
+                        <p class="authentication-view__related-items__certificate-number">{{ $related_item['certificate_number'] }}</p>
                     </div>
                 </div>
-
+            </a>
             @endforeach
-
         </div>
     </div>
 </section>
